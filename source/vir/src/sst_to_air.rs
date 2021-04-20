42f7f08871015b3b5240ac6c821d6e09b4c9914b
Chris Hawblitzel <Chris.Hawblitzel@microsoft.com>
2021-04-20 09:09:45 -0700

    Replace stm_to_stmt with stm_to_stmts

--- source/vir/src/sst_to_air.rs
+++ source/vir/src/sst_to_air.rs
@@ -74,13 +74,13 @@ pub(crate) fn exp_to_expr(exp: &Exp) -> Expr {
     }
 }
 
-pub fn stm_to_stmt(stm: &Stm, decls: &mut Vec<Decl>) -> Option<Stmt> {
+pub fn stm_to_stmts(stm: &Stm, decls: &mut Vec<Decl>) -> Vec<Stmt> {
     match &stm.x {
-        StmX::Assume(expr) => Some(Rc::new(StmtX::Assume(exp_to_expr(&expr)))),
+        StmX::Assume(expr) => vec![Rc::new(StmtX::Assume(exp_to_expr(&expr)))],
         StmX::Assert(expr) => {
             let air_expr = exp_to_expr(&expr);
             let option_span = Rc::new(Some(stm.span.clone()));
-            Some(Rc::new(StmtX::Assert(option_span, air_expr)))
+            vec![Rc::new(StmtX::Assert(option_span, air_expr))]
         }
         StmX::Decl { ident, typ, mutable } => {
             decls.push(if *mutable {
@@ -88,30 +88,35 @@ pub fn stm_to_stmt(stm: &Stm, decls: &mut Vec<Decl>) -> Option<Stmt> {
             } else {
                 Rc::new(DeclX::Const(suffix_local_id(&ident), typ_to_air(&typ)))
             });
-            None
+            vec![]
         }
         StmX::Assign(lhs, rhs) => {
             let ident = match &lhs.x {
                 ExpX::Var(ident) => ident,
                 _ => panic!("unexpected lhs {:?} in assign", lhs),
             };
-            Some(Rc::new(StmtX::Assign(suffix_local_id(&ident), exp_to_expr(rhs))))
+            vec![Rc::new(StmtX::Assign(suffix_local_id(&ident), exp_to_expr(rhs)))]
         }
         StmX::Fuel(x, fuel) => {
             if *fuel == 0 {
-                None
+                vec![]
             } else {
                 // (assume (fuel_bool fuel%f))
                 let id_fuel = prefix_fuel_id(&x);
                 let expr_fuel_bool = str_apply(&FUEL_BOOL, &vec![ident_var(&id_fuel)]);
-                Some(Rc::new(StmtX::Assume(expr_fuel_bool)))
+                vec![Rc::new(StmtX::Assume(expr_fuel_bool))]
             }
         }
         StmX::Block(stms) => {
-            let stmts = stms.iter().filter_map(|s| stm_to_stmt(s, decls)).collect::<Vec<_>>();
-            Some(Rc::new(StmtX::Block(Rc::new(stmts))))
+            let stmts = stms.iter().map(|s| stm_to_stmts(s, decls)).flatten().collect::<Vec<_>>();
+            vec![Rc::new(StmtX::Block(Rc::new(stmts)))]
         }
     }
+}
+
+pub fn stm_to_one_stmt(stm: &Stm, decls: &mut Vec<Decl>) -> Stmt {
+    let stmts = stm_to_stmts(stm, decls);
+    if stmts.len() == 1 { stmts[0].clone() } else { Rc::new(StmtX::Block(Rc::new(stmts))) }
 }
 
 fn set_fuel(local: &mut Vec<Decl>, hidden: &Vec<Ident>) {
@@ -146,13 +151,10 @@ fn set_fuel(local: &mut Vec<Decl>, hidden: &Vec<Ident>) {
 }
 
 pub fn stm_to_air(params: &Params, hidden: &Vec<Ident>, stm: &Stm) -> Commands {
-    let mut local: Vec<Decl> = params
-        .iter()
-        .map(|param| {
-            Rc::new(DeclX::Const(suffix_local_id(&param.x.name), typ_to_air(&param.x.typ)))
-        })
-        .collect();
-    let assertion = stm_to_stmt(&stm, &mut local).unwrap_or(Rc::new(StmtX::Block(Rc::new(vec![]))));
+    let mut local: Vec<Decl> = vec_map(params, |param| {
+        Rc::new(DeclX::Const(suffix_local_id(&param.x.name), typ_to_air(&param.x.typ)))
+    });
+    let assertion = stm_to_one_stmt(&stm, &mut local);
     set_fuel(&mut local, hidden);
     let query = Rc::new(QueryX { local: Rc::new(local), assertion });
     let command = Rc::new(CommandX::CheckValid(query));
