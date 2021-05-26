11f13a0c5766eb8bd30b6de9c942143ae94017c4
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-05-26 11:15:59 +0200

    Port adts example to new test harness

---
+++ source/rust_verify/tests/adts.rs
@@ -0,0 +1,85 @@
+#![feature(rustc_private)]
+#[macro_use]
+mod common;
+use common::*;
+
+const STRUCTS: &str = code_str! {
+    #[derive(PartialEq, Eq)]
+    struct Car {
+        four_doors: bool,
+        passengers: int,
+    }
+
+    #[derive(PartialEq, Eq)]
+    enum Vehicle {
+        Car(Car),
+        Train(bool),
+    }
+};
+
+test_verify_with_pervasive! {
+    #[test] test_struct_1 STRUCTS.to_string() + code_str! {
+        fn test_struct_1(p: int) {
+            assert((Car { four_doors: true, passengers: p }).passengers == p);
+            assert((Car { passengers: p, four_doors: true }).passengers == p); // fields intentionally out of order
+            assert((Car { four_doors: true, passengers: p }).passengers != p); // FAILS
+        }
+    } => Err(err) => assert_one_fails(err)
+}
+
+test_verify_with_pervasive! {
+    #[test] test_struct_2 STRUCTS.to_string() + code_str! {
+        fn test_struct_2(c: Car, p: int) {
+            assume(c.passengers == p);
+            assert(c.passengers == p);
+            assert(c.passengers != p); // FAILS
+        }
+    } => Err(err) => assert_one_fails(err)
+}
+
+test_verify_with_pervasive! {
+    #[test] test_struct_3 STRUCTS.to_string() + code_str! {
+        fn test_struct_3(p: int) {
+            let c = Car { passengers: p, four_doors: true };
+            assert(c.passengers == p);
+            assert(!c.four_doors); // FAILS
+        }
+    } => Err(err) => assert_one_fails(err)
+}
+
+test_verify_with_pervasive! {
+    #[test] test_struct_4 STRUCTS.to_string() + code_str! {
+        fn test_struct_4(passengers: int) {
+            assert((Car { passengers, four_doors: true }).passengers == passengers);
+        }
+    } => Ok(())
+}
+
+test_verify_with_pervasive! {
+    #[test] test_enum_1 STRUCTS.to_string() + code_str! {
+        fn test_enum_1(passengers: int) {
+            let t = Vehicle::Train(true);
+            let c1 = Vehicle::Car(Car { passengers, four_doors: true });
+            let c2 = Vehicle::Car(Car { passengers, four_doors: false });
+        }
+    } => Ok(())
+}
+
+test_verify_with_pervasive! {
+    #[test] test_neq STRUCTS.to_string() + code_str! {
+        fn test_neq(passengers: int) {
+            let c1 = Car { passengers, four_doors: true };
+            let c2 = Car { passengers, four_doors: false };
+            let c3 = Car { passengers, four_doors: true };
+
+            assert(c1 == c3);
+            assert(c1 != c2);
+
+            let t = Vehicle::Train(true);
+            let ca = Vehicle::Car(c1);
+
+            assert(t != ca);
+            assert(t == ca); // FAILS
+        }
+    } => Err(err) => assert_one_fails(err)
+}
