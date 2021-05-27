f51b01041877d73ee811bc4a2d71ce8994a276be
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-05-27 16:41:35 +0200

    Additional adt generics (un)boxing test

--- source/rust_verify/tests/adts_generics.rs
+++ source/rust_verify/tests/adts_generics.rs
@@ -30,3 +30,21 @@ test_verify_with_pervasive! {
         }
     } => Ok(())
 }
+
+test_verify_with_pervasive! {
+    #[test] test_generic_adt_eq code! {
+        #[derive(Eq, PartialEq)]
+        struct Thing<A> {
+            a: A,
+        }
+
+        fn one(v: int) {
+            let t1 = Thing { a: v };
+            let t2 = Thing { a: v };
+            let a1: int = t1.a;
+            let a2: int = t2.a;
+            assert(a1 == a2);
+            assert(a1 != a2); // FAILS
+        }
+    } => Err(err) => assert_one_fails(err)
+}
