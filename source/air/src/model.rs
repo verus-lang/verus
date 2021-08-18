f80814d173b84c4b2ab2362432d9eed06c0a8c5b
Bryan Parno <parno@cmu.edu>
2021-08-18 16:46:14 -0400

    Better document the AIR model

--- source/air/src/model.rs
+++ source/air/src/model.rs
@@ -1,3 +1,6 @@
+//! Provides an AIR-level interface to the model returned by the SMT solver
+//! when it reaches a SAT conclusion
+
 use crate::ast::{Decl, DeclX, Ident, SnapShots};
 use crate::context::Context;
 use crate::smt_util::new_const;
@@ -6,18 +9,31 @@ use std::fmt;
 use z3::ast::Dynamic;
 
 #[derive(Debug)]
+/// AIR-level model of a concrete counterexample
 pub struct Model<'a> {
+    /// Handle to the original Z3 model
     z3_model: z3::Model<'a>,
+    /// Internal mapping of snapshot IDs to snapshots that map AIR variables to usage counts.
+    /// Generated when converting mutable variables to Z3-level constants.
     id_snapshots: SnapShots,
+    /// Externally facing mapping from snapshot IDs to snapshots that map AIR variables
+    /// to their concrete values.
+    /// TODO: Upgrade to a semantics-preserving value type, instead of String.
     value_snapshots: HashMap<Ident, HashMap<Ident, String>>,
 }
 
 impl<'a> Model<'a> {
+    /// Returns an (unpopulated) AIR model object.  Must call [build()] to fully populate.
+    /// # Arguments
+    /// * `model` - The model that Z3 returns
+    /// * `snapshots` - Internal mapping of snapshot IDs to snapshots that map AIR variables to usage counts.
     pub fn new(model: z3::Model<'a>, snapshots: SnapShots) -> Model<'a> {
         // println!("Creating a new model with {} snapshots", snapshots.len());
         Model { z3_model: model, id_snapshots: snapshots, value_snapshots: HashMap::new() }
     }
 
+    /// Convert a Z3 AST variable `var_stmt` into a String value
+    /// Uses `var_name` only for error reporting.
     fn lookup_z3_var(&self, var_name: &String, var_smt: &Dynamic) -> String {
         if let Some(x) = self.z3_model.eval(var_smt) {
             if let Some(b) = x.as_bool() {
@@ -34,7 +50,10 @@ impl<'a> Model<'a> {
         }
     }
 
-    /// Reconstruct an AIR-level model based on the Z3 model
+    /// Populate the AIR-level model based on the Z3 model
+    /// `local_vars` should be a list of [DeclX::Const] values
+    /// representing the function's local non-mutable variables
+    /// (e.g., function parameters)
     pub fn build(&mut self, context: &mut Context, local_vars: Vec<Decl>) {
         println!("Building the AIR model");
         for (snap_id, id_snapshot) in &self.id_snapshots {
@@ -67,12 +86,14 @@ impl<'a> Model<'a> {
         }
     }
 
+    /// Look up the value of an AIR variable `name` in a given `snapshot`
     pub fn query_variable(&self, snapshot: Ident, name: Ident) -> Option<String> {
         Some(self.value_snapshots.get(&snapshot)?.get(&name)?.to_string())
     }
 }
 
 impl<'a> fmt::Display for Model<'a> {
+    /// Dump the contents of the AIR model for debugging purposes
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "\nDisplaying model with {} snapshots\n", self.value_snapshots.len())?;
         for (snap_id, value_snapshot) in &self.value_snapshots {
