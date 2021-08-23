2ed4058853b399b0350f0d44c92188ac8edfc89f
Bryan Parno <parno@cmu.edu>
2021-08-23 12:17:43 -0400

    If/else still seems to work

--- source/rust_verify/example/debug.rs
+++ source/rust_verify/example/debug.rs
@@ -17,6 +17,7 @@ fn test_mutation(i: int, n: nat, u: u8) {
     x = x + u;
     assert(x >= 5);
 }
+*/
 
 fn test_if_else(b:bool, z:int) {
     let mut x : int = 0;
@@ -31,8 +32,8 @@ fn test_if_else(b:bool, z:int) {
     }
     assert(x + y > 5); // 6_join
 }
-*/
 
+/*
 fn test_loop() {
     let mut i: u64 = 10;
     let mut b1: u8 = 20;
@@ -56,3 +57,4 @@ fn test_loop() {
 
     assert(true);   // 7_while_end
 }
+*/
