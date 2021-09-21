29cdd66ed9eccc229adccd5e186f7d1e3aa24ee4
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-09-21 11:52:29 +0200

    Add local z3 path to ./tools/rust-verify.sh

--- source/tools/rust-verify.sh
+++ source/tools/rust-verify.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-DYLD_LIBRARY_PATH=../install/lib/rustlib/x86_64-apple-darwin/lib LD_LIBRARY_PATH=../install/lib ../install/bin/rust_verify -L ../install/bin $@
+DUST_Z3_PATH="$(pwd)/z3" DYLD_LIBRARY_PATH=../install/lib/rustlib/x86_64-apple-darwin/lib LD_LIBRARY_PATH=../install/lib ../install/bin/rust_verify -L ../install/bin $@
