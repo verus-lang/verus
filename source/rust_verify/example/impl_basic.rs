b7323e64052fcd6390e808e5c4c4e565e274d496
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-10-25 13:15:06 +0200

    update example with new support for impl

--- source/rust_verify/example/impl_basic.rs
+++ source/rust_verify/example/impl_basic.rs
@@ -10,9 +10,14 @@ struct Car {
 }
 
 impl Car {
-    fn get_passengers(&self) -> int { // UNSUPPORTED: we need references first
+    fn get_passengers(&self) -> int {
+        ensures(|result: int| result == self.passengers);
         self.passengers
     }
 }
 
-fn main() { }
+fn main() {
+    let c = Car { four_doors: true, passengers: 3 };
+    let p = c.get_passengers();
+    assert(p < 4);
+}
