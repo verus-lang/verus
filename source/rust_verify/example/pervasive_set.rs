d5fb7267c8c091c1b0bb9015a9f3ba5df81497b9
Chris Hawblitzel <Chris.Hawblitzel@microsoft.com>
2021-11-10 11:28:44 -0800

    Add example/pervasive_set.rs

---
+++ source/rust_verify/example/pervasive_set.rs
@@ -0,0 +1,79 @@
+extern crate builtin;
+use builtin::*;
+use pervasive::*;
+
+/// set type for specifications
+#[verifier(no_verify)]
+pub struct Set<A> {
+    dummy: Vec<A>,
+}
+
+//TODO:
+//impl<A> Set<A> {
+//  ...
+//}
+
+#[spec]
+#[verifier(pub_abstract)]
+pub fn empty<A>() -> Set<A> {
+    arbitrary()
+}
+
+#[spec]
+#[verifier(pub_abstract)]
+pub fn contains<A>(s: Set<A>, a: A) -> bool {
+    arbitrary()
+}
+
+#[spec]
+pub fn subset_of<A>(s1: Set<A>, s2: Set<A>) -> bool {
+    forall(|a: A| imply(contains(s1, a), contains(s2, a)))
+}
+
+#[spec]
+pub fn ext_equal<A>(s1: Set<A>, s2: Set<A>) -> bool {
+    forall(|a: A| contains(s1, a) == contains(s2, a))
+}
+
+#[spec]
+#[verifier(pub_abstract)]
+pub fn insert<A>(s: Set<A>, a: A) -> Set<A> {
+    arbitrary()
+}
+
+#[spec]
+#[verifier(pub_abstract)]
+pub fn union<A>(s1: Set<A>, s2: Set<A>) -> Set<A> {
+    arbitrary()
+}
+
+#[spec]
+#[verifier(pub_abstract)]
+pub fn intersect<A>(s1: Set<A>, s2: Set<A>) -> Set<A> {
+    arbitrary()
+}
+
+#[spec]
+#[verifier(pub_abstract)]
+pub fn difference<A>(s1: Set<A>, s2: Set<A>) -> Set<A> {
+    arbitrary()
+}
+
+#[verifier(no_verify)]
+#[proof]
+pub fn set_axioms<A>() {
+    ensures([
+        forall(|a: A| !contains(empty(), a)),
+        forall(|s: Set<A>, a: A| contains(insert(s, a), a)),
+        forall(|s: Set<A>, a1: A, a2: A|
+            equal(a1, a2) || contains(insert(s, a1), a2) == contains(s, a2)),
+        forall(|s1: Set<A>, s2: Set<A>, a: A|
+            contains(union(s1, s2), a) == (contains(s1, a) || contains(s2, a))),
+        forall(|s1: Set<A>, s2: Set<A>, a: A|
+            contains(intersect(s1, s2), a) == (contains(s1, a) && contains(s2, a))),
+        forall(|s1: Set<A>, s2: Set<A>, a: A|
+            contains(difference(s1, s2), a) == (contains(s1, a) && !contains(s2, a))),
+        forall(|s1: Set<A>, s2: Set<A>|
+            ext_equal(s1, s2) == equal(s1, s2)),
+    ]);
+}
