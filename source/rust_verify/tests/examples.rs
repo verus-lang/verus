886cdb14fa3d12cd19e5fbf574d7d8bd91a40d8b
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-11-15 19:48:17 +0100

    cargo fmt tests/examples.rs

--- source/rust_verify/tests/examples.rs
+++ source/rust_verify/tests/examples.rs
@@ -35,7 +35,8 @@ fn run_examples() {
         path.extend(&entry.path());
         let path = path.to_str().expect("invalid example path");
 
-        let mut reader = std::io::BufReader::new(std::fs::File::open(relative_path).expect("cannot open file"));
+        let mut reader =
+            std::io::BufReader::new(std::fs::File::open(relative_path).expect("cannot open file"));
         let mut first_line = String::new();
         let first_line_elements: Vec<_> = {
             use std::io::BufRead;
@@ -51,48 +52,73 @@ fn run_examples() {
                 "expect-errors" => mode = Mode::ExpectErrors,
                 "expect-failures" => mode = Mode::ExpectFailures,
                 "ignore" => continue,
-                _ => panic!("invalid command for example file test: use one of 'expect-success', 'expect-errors', 'expect-failures' or 'ignore'"),
+                _ => panic!(
+                    "invalid command for example file test: use one of 'expect-success', 'expect-errors', 'expect-failures' or 'ignore'"
+                ),
             }
         }
 
         let script = if cfg!(target_os = "windows") {
-            format!("../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.dll --edition=2018 {}", &path)
+            format!(
+                "../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.dll --edition=2018 {}",
+                &path
+            )
         } else {
-            format!("DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/{}/lib LD_LIBRARY_PATH=../rust/install/lib ../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.{} --edition=2018 {}", RUST_LIB_TARGET, DYN_LIB_EXT, &path)
+            format!(
+                "DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/{}/lib LD_LIBRARY_PATH=../rust/install/lib ../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.{} --edition=2018 {}",
+                RUST_LIB_TARGET, DYN_LIB_EXT, &path
+            )
         };
-        
+
         let output = if cfg!(target_os = "windows") {
             Command::new("cmd")
-                    .current_dir("..")
-                    .args(&["/C", &script])
-                    .output()
-                    .expect("failed to execute process")
+                .current_dir("..")
+                .args(&["/C", &script])
+                .output()
+                .expect("failed to execute process")
         } else {
             Command::new("sh")
-                    .current_dir("..")
-                    .arg("-c")
-                    .arg(script)
-                    .output()
-                    .expect("failed to execute process")
+                .current_dir("..")
+                .arg("-c")
+                .arg(script)
+                .output()
+                .expect("failed to execute process")
         };
 
         use regex::Regex;
         let re = Regex::new(r"Verification results:: verified: (\d+) errors: (\d+)").unwrap();
         let stdout = std::str::from_utf8(&output.stdout).expect("invalid stdout encoding");
-        let verifier_output: Option<(u64, u64)> = re.captures_iter(stdout).next().map(|x| (x[1].parse().expect("invalid verifier output"), x[2].parse().expect("invalid verifier output")));
+        let verifier_output: Option<(u64, u64)> = re.captures_iter(stdout).next().map(|x| {
+            (
+                x[1].parse().expect("invalid verifier output"),
+                x[2].parse().expect("invalid verifier output"),
+            )
+        });
 
         let success = match mode {
-            Mode::ExpectSuccess  => output.status.success() && match verifier_output {
-                Some((_, 0)) => true, _ => false },
-            Mode::ExpectErrors   => !output.status.success(),
-            Mode::ExpectFailures => !output.status.success() && match verifier_output {
-                Some((_, failures)) if failures > 0 => true, _ => false
-            },
+            Mode::ExpectSuccess => {
+                output.status.success()
+                    && match verifier_output {
+                        Some((_, 0)) => true,
+                        _ => false,
+                    }
+            }
+            Mode::ExpectErrors => !output.status.success(),
+            Mode::ExpectFailures => {
+                !output.status.success()
+                    && match verifier_output {
+                        Some((_, failures)) if failures > 0 => true,
+                        _ => false,
+                    }
+            }
         };
 
         if !success {
             eprintln!("- example {} - mode: {:?} - failed -", &path, mode);
-            eprintln!("- stderr -\n{}\n", std::str::from_utf8(&output.stderr).expect("invalid stderr encoding"));
+            eprintln!(
+                "- stderr -\n{}\n",
+                std::str::from_utf8(&output.stderr).expect("invalid stderr encoding")
+            );
             eprintln!("- stdout -\n{}\n", stdout);
             assert!(false);
         }
