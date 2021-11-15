8b7c03bc61aacf8616f7d79a781b197da48dada2
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-11-15 19:46:27 +0100

    (Hopefully) correctly support linux in the examples test

--- source/rust_verify/tests/examples.rs
+++ source/rust_verify/tests/examples.rs
@@ -6,6 +6,12 @@ const DYN_LIB_EXT: &str = "dylib";
 #[cfg(target_os = "linux")]
 const DYN_LIB_EXT: &str = "so";
 
+#[cfg(target_os = "macos")]
+const RUST_LIB_TARGET: &str = "x86_64-unknown-linux";
+
+#[cfg(target_os = "linux")]
+const RUST_LIB_TARGET: &str = "x86_64-apple-darwin";
+
 #[derive(Debug)]
 enum Mode {
     ExpectSuccess,
@@ -52,7 +58,7 @@ fn run_examples() {
         let script = if cfg!(target_os = "windows") {
             format!("../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.$DYN_LIB_EXT --edition=2018 {}", &path)
         } else {
-            format!("DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/x86_64-apple-darwin/lib LD_LIBRARY_PATH=../rust/install/lib ../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.{} --edition=2018 {}", DYN_LIB_EXT, &path)
+            format!("DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/{}/lib LD_LIBRARY_PATH=../rust/install/lib ../rust/install/bin/rust_verify --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.{} --edition=2018 {}", RUST_LIB_TARGET, DYN_LIB_EXT, &path)
         };
         
         let output = if cfg!(target_os = "windows") {
