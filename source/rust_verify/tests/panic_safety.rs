c8a137d6bd541857d2cffb3de329ee6d4c954811
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2022-01-04 20:26:41 +0100

    Progress on summer school translation, add ignored test for panic safety TODO
    Co-Authored-By: Jon Howell <howell@vmware.com>

--- source/rust_verify/tests/panic_safety.rs
+++ source/rust_verify/tests/panic_safety.rs
@@ -5,10 +5,11 @@ use common::*;
 
 test_verify_one_file! {
     // TODO SOUNDNESS this code verifies, but panics in debug mode
+    // introduce under/overflow checks
     #[ignore] #[test] verified_code_should_not_panic code! {
         fn do_not_panic() {
             let mut a: u64 = 0;
-            a = a - 1;
+            a = a - 1; // TODO this should produce an error
         }
     } => Err(_)
 }
