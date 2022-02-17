eae261336347b9040ecdb5395b43a259f83cdbda
Chanhee Cho <channy1413@gmail.com>
2022-02-17 15:43:27 -0500

    Change to specific type for now

--- source/docs/bv_expriments/bitvec_auto.rs
+++ source/docs/bv_expriments/bitvec_auto.rs
@@ -3,29 +3,38 @@ mod pervasive;
 use pervasive::*;
 
 #[proof]
-#[verifier(external_body)]
-fn bit_and_property(){
+fn bit_and32_auto(){
     ensures([
-        forall(|a: int, b: int| #[trigger] (a&b) == b&a),
-        forall(|a: int, b: int, c:int| #[trigger] ((a&b)&c) == a&(b&c)),
-        forall(|a: int| #[trigger] (a&a) == a),
-        forall(|a: int| #[trigger] (a&0) == 0),
+        forall(|a: u32, b: u32| #[trigger] (a&b) == b&a),
+        forall(|a: u32, b: u32, c:u32| #[trigger] ((a&b)&c) == a&(b&c)),
+        forall(|a: u32| #[trigger] (a&a) == a),
+        forall(|a: u32| #[trigger] (a&0) == 0),
+        forall(|a: u32| #[trigger] (a& 0xffffffffu32) == a),
+        forall(|a: u32, b: u32| #[trigger] (a&b) >= 0),
+        forall(|a: u32, b: u32| #[trigger] (a&b) <= 0xffffffffu32),
     ]);
+    assert_bit_vector(forall(|a: u32, b: u32| #[trigger] (a&b) == b&a));
+    assert_bit_vector(forall(|a: u32, b: u32, c:u32| #[trigger] ((a&b)&c) == a&(b&c)));
+    assert_bit_vector(forall(|a: u32| #[trigger] (a&a) == a));
+    assert_bit_vector(forall(|a: u32| #[trigger] (a&0) == 0));
+    assert_bit_vector(forall(|a: u32| #[trigger] (a& 0xffffffffu32) == a));
+    assert_bit_vector(forall(|a: u32, b: u32| #[trigger] (a&b) >= 0));
+    assert_bit_vector(forall(|a: u32, b: u32| #[trigger] (a&b) <= 0xffffffffu32));
 }
 
 #[proof]
 fn test9(b1: u32, b2:u32, b3:u32) { 
-    bit_and_property();
+    bit_and32_auto();
+
     assert_bit_vector(b1 & 0xff < 0x100);
     assert(0xff & b1 < 0x100);
 
     assert(0 <= (b1 & b1) as int);
     assert((b1 & b1) as int <= 0xffffffffu32);
 
-    // assert( (b1&b2)&b3 == b1&(b2&b3) );
-    // assert(b1&0 == 0);
-    // assert(0&b2 == 0);
-
+    assert( (b1&b2)&b3 == b1&(b2&b3) );
+    assert(b1&0 == 0);
+    assert(0&b2 == 0);
 }
 
 fn main() {}
