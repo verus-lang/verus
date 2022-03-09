e509d244388e0085c151b5e05ddd89101784aef4
Travis Hance <tjhance7@gmail.com>
2022-03-08 21:44:09 -0500

    cells, ptr

--- source/pervasive/cell.rs
+++ source/pervasive/cell.rs
@@ -16,91 +16,135 @@ use std::mem::MaybeUninit;
 
 #[verifier(external_body)]
 pub struct PCell<#[verifier(strictly_positive)] V> {
-  ucell: UnsafeCell<MaybeUninit<V>>,
+    ucell: UnsafeCell<MaybeUninit<V>>,
 }
 
 #[proof]
 #[verifier(unforgeable)]
 pub struct Permission<V> {
-  #[spec] pub pcell: int,
-  #[spec] pub value: option::Option<V>,
+    #[spec] pub pcell: int,
+    #[spec] pub value: option::Option<V>,
 }
 
 impl<V> PCell<V> {
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn empty() -> (PCell<V>, Proof<Permission<V>>) {
-    ensures(|pt : (PCell<V>, Proof<Permission<V>>)|
-      equal(pt.1, Proof(Permission{ pcell: pt.0.view(), value: option::Option::None }))
-    );
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn empty() -> (PCell<V>, Proof<Permission<V>>) {
+        ensures(|pt : (PCell<V>, Proof<Permission<V>>)|
+            equal(pt.1, Proof(Permission{ pcell: pt.0.view(), value: option::Option::None }))
+        );
 
-    let p = PCell { ucell: UnsafeCell::new(MaybeUninit::uninit()) };
-    let Proof(t) = exec_proof_from_false();
-    (p, Proof(t))
-  }
-
-  fndecl!(pub fn view(&self) -> int);
-
-  //// Put
-
-  #[inline(always)]
-  #[verifier(external_body)]
-  fn put_external(&self, v: V) {
-    ensures(false);
-    opens_invariants_none();
-    unsafe {
-      *(self.ucell.get()) = MaybeUninit::new(v);
+        let p = PCell { ucell: UnsafeCell::new(MaybeUninit::uninit()) };
+        let Proof(t) = exec_proof_from_false();
+        (p, Proof(t))
     }
-  }
 
-  #[inline(always)]
-  pub fn put(&self, v: V, #[proof] perm: &mut Permission<V>) {
-    requires([
-        equal(self.view(), old(perm).pcell),
-        equal(old(perm).value, option::Option::None),
-    ]);
-    ensures(
-        equal(perm.pcell, old(perm).pcell) &&
-        equal(perm.value, option::Option::Some(v))
-    );
-    opens_invariants_none();
+    fndecl!(pub fn view(&self) -> int);
 
-    self.put_external(v);
-  }
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn put(&self, #[proof] perm: &mut Permission<V>, v: V) {
+        requires([
+            equal(self.view(), old(perm).pcell),
+            equal(old(perm).value, option::Option::None),
+        ]);
+        ensures(
+            equal(perm.pcell, old(perm).pcell) &&
+            equal(perm.value, option::Option::Some(v))
+        );
+        opens_invariants_none();
 
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn take(&self, #[proof] perm: &mut Permission<V>) -> V {
-    requires([
-        equal(self.view(), old(perm).pcell),
-        old(perm).value.is_Some(),
-    ]);
-    ensures(|v: V| [
-        equal(perm.pcell, old(perm).pcell),
-        equal(perm.value, option::Option::None),
-        equal(v, old(perm).value.get_Some_0()),
-    ]);
-    opens_invariants_none();
+        unsafe {
+            *(self.ucell.get()) = MaybeUninit::new(v);
+        }
+    }
 
-    unimplemented!();
-  }
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn take(&self, #[proof] perm: &mut Permission<V>) -> V {
+        requires([
+            equal(self.view(), old(perm).pcell),
+            old(perm).value.is_Some(),
+        ]);
+        ensures(|v: V| [
+            equal(perm.pcell, old(perm).pcell),
+            equal(perm.value, option::Option::None),
+            equal(v, old(perm).value.get_Some_0()),
+        ]);
+        opens_invariants_none();
 
+        unsafe {
+            let mut m = MaybeUninit::uninit();
+            std::mem::swap(&mut m, &mut *self.ucell.get());
+            m.assume_init()
+        }
+    }
 
-  /*
-  #[inline(always)]
-  #[verifier(no_verify)]
-  pub fn borrow(&self, perm: &'a Permission<V>) -> &'a V {
-    requires([
-        equal(self.view(), perm.view().pcell),
-        !equal(perm.view().value, None),
-    ]);
-    ensures(|p: Permission<V>|
-        equal(p.view().value, Some(v))
-    );
-    
-    self.write_external(v);
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn replace(&self, #[proof] perm: &mut Permission<V>, in_v: V) -> V {
+        requires([
+            equal(self.view(), old(perm).pcell),
+            old(perm).value.is_Some(),
+        ]);
+        ensures(|out_v: V| [
+            equal(perm.pcell, old(perm).pcell),
+            equal(perm.value, option::Option::Some(in_v)),
+            equal(out_v, old(perm).value.get_Some_0()),
+        ]);
+        opens_invariants_none();
 
-    perm
-  }
-  */
+        unsafe {
+            let mut m = MaybeUninit::new(in_v);
+            std::mem::swap(&mut m, &mut *self.ucell.get());
+            m.assume_init()
+        }
+    }
+
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn as_ref<'a>(&self, #[proof] perm: &'a Permission<V>) -> &'a V {
+        requires([
+            equal(self.view(), perm.pcell),
+            perm.value.is_Some(),
+        ]);
+        ensures(|v: V|
+            equal(v, perm.value.get_Some_0())
+        );
+        opens_invariants_none();
+
+        unsafe {
+            (*self.ucell.get()).assume_init_ref()
+        }
+    }
+
+    //////////////////////////////////
+    // Untrusted functions below here
+
+    #[inline(always)]
+    pub fn into_inner(self, #[proof] perm: Permission<V>) -> V {
+        requires([
+            equal(self.view(), perm.pcell),
+            perm.value.is_Some(),
+        ]);
+        ensures(|v|
+            equal(v, perm.value.get_Some_0())
+        );
+        opens_invariants_none();
+
+        #[proof] let mut perm = perm;
+        self.take(&mut perm)
+    }
+
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn new(v: V) -> (PCell<V>, Proof<Permission<V>>) {
+        ensures(|pt : (PCell<V>, Proof<Permission<V>>)|
+            equal(pt.1, Proof(Permission{ pcell: pt.0.view(), value: option::Option::Some(v) }))
+        );
+
+        let (p, Proof(mut t)) = Self::empty();
+        p.put(&mut t, v);
+        (p, Proof(t))
+    }
 }
--- source/pervasive/ptr.rs
+++ source/pervasive/ptr.rs
@@ -27,109 +27,162 @@ use std::alloc::{dealloc};
 
 #[verifier(external_body)]
 pub struct PPtr<#[verifier(strictly_positive)] V> {
-  uptr: NonNull<MaybeUninit<V>>,
+    uptr: NonNull<MaybeUninit<V>>,
 }
 
 #[proof]
 #[verifier(unforgeable)]
 pub struct Permission<V> {
-  #[spec] pub pptr: int,
-  #[spec] pub value: option::Option<V>,
+    #[spec] pub pptr: int,
+    #[spec] pub value: option::Option<V>,
 }
 
 impl<V> PPtr<V> {
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn empty() -> (PPtr<V>, Proof<Permission<V>>) {
-    ensures(|pt : (PPtr<V>, Proof<Permission<V>>)|
-      equal(pt.1, Proof(Permission{ pptr: pt.0.view(), value: option::Option::None }))
-    );
-    opens_invariants_none();
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn empty() -> (PPtr<V>, Proof<Permission<V>>) {
+        ensures(|pt : (PPtr<V>, Proof<Permission<V>>)|
+            equal(pt.1, Proof(Permission{ pptr: pt.0.view(), value: option::Option::None }))
+        );
+        opens_invariants_none();
 
-    let p = PPtr {
-      uptr: Box::leak(box MaybeUninit::uninit()).into(),
-    };
-    let Proof(t) = exec_proof_from_false();
-    (p, Proof(t))
-  }
-
-  fndecl!(pub fn view(&self) -> int);
-
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn clone(&self) -> PPtr<V> {
-      ensures(|pt: PPtr<V>| equal(pt.view(), self.view()));
-      opens_invariants_none();
-
-      PPtr { uptr: self.uptr }
-  }
-
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn put(&self, v: V, #[proof] perm: &mut Permission<V>) {
-    requires([
-        equal(self.view(), old(perm).pptr),
-        equal(old(perm).value, option::Option::None),
-    ]);
-    ensures([
-        equal(perm.pptr, old(perm).pptr),
-        equal(perm.value, option::Option::Some(v)),
-    ]);
-    opens_invariants_none();
-
-    unsafe {
-      *(self.uptr.as_ptr()) = MaybeUninit::new(v);
+        let p = PPtr {
+            uptr: Box::leak(box MaybeUninit::uninit()).into(),
+        };
+        let Proof(t) = exec_proof_from_false();
+        (p, Proof(t))
     }
-  }
 
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn take(&self, #[proof] perm: &mut Permission<V>) -> V {
-    requires([
-        equal(self.view(), old(perm).pptr),
-        old(perm).value.is_Some(),
-    ]);
-    ensures(|v: V| [
-        equal(perm.pptr, old(perm).pptr),
-        equal(perm.value, option::Option::None),
-        equal(v, old(perm).value.get_Some_0()),
-    ]);
-    opens_invariants_none();
+    fndecl!(pub fn view(&self) -> int);
 
-    unsafe {
-        let mut m = MaybeUninit::uninit();
-        std::mem::swap(&mut m, &mut *self.uptr.as_ptr());
-        m.assume_init()
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn clone(&self) -> PPtr<V> {
+        ensures(|pt: PPtr<V>| equal(pt.view(), self.view()));
+        opens_invariants_none();
+
+        PPtr { uptr: self.uptr }
     }
-  }
 
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn as_ref<'a>(&self, #[proof] perm: &'a Permission<V>) -> &'a V {
-    requires([
-        equal(self.view(), perm.pptr),
-        perm.value.is_Some(),
-    ]);
-    ensures(|v: V|
-        equal(v, perm.value.get_Some_0())
-    );
-    opens_invariants_none();
-    
-    unsafe {
-        self.uptr.as_ref().assume_init_ref()
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn put(&self, #[proof] perm: &mut Permission<V>, v: V) {
+        requires([
+            equal(self.view(), old(perm).pptr),
+            equal(old(perm).value, option::Option::None),
+        ]);
+        ensures([
+            equal(perm.pptr, old(perm).pptr),
+            equal(perm.value, option::Option::Some(v)),
+        ]);
+        opens_invariants_none();
+
+        unsafe {
+            *(self.uptr.as_ptr()) = MaybeUninit::new(v);
+        }
     }
-  }
 
-  #[inline(always)]
-  #[verifier(external_body)]
-  pub fn dispose(&self, #[proof] perm: Permission<V>) {
-    requires([
-        equal(self.view(), perm.pptr),
-        equal(perm.value, option::Option::None),
-    ]);
-    opens_invariants_none();
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn take(&self, #[proof] perm: &mut Permission<V>) -> V {
+        requires([
+            equal(self.view(), old(perm).pptr),
+            old(perm).value.is_Some(),
+        ]);
+        ensures(|v: V| [
+            equal(perm.pptr, old(perm).pptr),
+            equal(perm.value, option::Option::None),
+            equal(v, old(perm).value.get_Some_0()),
+        ]);
+        opens_invariants_none();
 
-    // based on the implementation of Rc::drop
-    dealloc(self.uptr.cast().as_ptr(), Layout::for_value(self.uptr.as_ref()));
-  }
+        unsafe {
+            let mut m = MaybeUninit::uninit();
+            std::mem::swap(&mut m, &mut *self.uptr.as_ptr());
+            m.assume_init()
+        }
+    }
+
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn replace(&self, #[proof] perm: &mut Permission<V>, in_v: V) -> V {
+        requires([
+            equal(self.view(), old(perm).pptr),
+            old(perm).value.is_Some(),
+        ]);
+        ensures(|out_v: V| [
+            equal(perm.pptr, old(perm).pptr),
+            equal(perm.value, option::Option::Some(in_v)),
+            equal(out_v, old(perm).value.get_Some_0()),
+        ]);
+        opens_invariants_none();
+
+        unsafe {
+            let mut m = MaybeUninit::new(in_v);
+            std::mem::swap(&mut m, &mut *self.uptr.as_ptr());
+            m.assume_init()
+        }
+    }
+
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn as_ref<'a>(&self, #[proof] perm: &'a Permission<V>) -> &'a V {
+        requires([
+            equal(self.view(), perm.pptr),
+            perm.value.is_Some(),
+        ]);
+        ensures(|v: V|
+            equal(v, perm.value.get_Some_0())
+        );
+        opens_invariants_none();
+        
+        unsafe {
+            self.uptr.as_ref().assume_init_ref()
+        }
+    }
+
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn dispose(&self, #[proof] perm: Permission<V>) {
+        requires([
+            equal(self.view(), perm.pptr),
+            equal(perm.value, option::Option::None),
+        ]);
+        opens_invariants_none();
+
+        dealloc(self.uptr.cast().as_ptr(), Layout::for_value(self.uptr.as_ref()));
+    }
+
+    //////////////////////////////////
+    // Untrusted functions below here
+
+    #[inline(always)]
+    pub fn into_inner(self, #[proof] perm: Permission<V>) -> V {
+        requires([
+            equal(self.view(), perm.pptr),
+            perm.value.is_Some(),
+        ]);
+        ensures(|v|
+            equal(v, perm.value.get_Some_0())
+        );
+        opens_invariants_none();
+
+        #[proof] let mut perm = perm;
+        let v = self.take(&mut perm);
+        self.dispose(perm);
+        v
+    }
+
+    #[inline(always)]
+    #[verifier(external_body)]
+    pub fn new(v: V) -> (PPtr<V>, Proof<Permission<V>>) {
+        ensures(|pt : (PPtr<V>, Proof<Permission<V>>)|
+            equal(pt.1, Proof(Permission{ pptr: pt.0.view(), value: option::Option::Some(v) }))
+        );
+
+        let (p, Proof(mut t)) = Self::empty();
+        p.put(&mut t, v);
+        (p, Proof(t))
+    }
+
 }
