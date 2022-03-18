b2f887986f67a5ccdb964cceb78b9d01d1faef18
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2022-03-18 11:28:28 +0100

    Use the rust-verify.sh script for build-run-log.sh too

--- source/tools/build-run-log.sh
+++ source/tools/build-run-log.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-if [ `uname` == "Darwin" ]; then
-    DYN_LIB_EXT=dylib
-elif [ `uname` == "Linux" ]; then
-    DYN_LIB_EXT=so
-fi
-
 color_blue="\x1B[1;94m"
 color_reset="\x1B[0m"
 
@@ -23,7 +17,7 @@ rs_file_basename=`basename "$rs_file"`
 mkdir -p $rs_file_dir/log
 
 RUSTC=../rust/install/bin/rustc ../rust/install/bin/cargo build && \
-        VERUS_Z3_PATH="$(pwd)/z3" DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/x86_64-apple-darwin/lib LD_LIBRARY_PATH=../rust/install/lib ../rust/install/bin/rust_verify --edition=2018 --pervasive-path pervasive --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.$DYN_LIB_EXT $rs_file \
+        ./tools/rust-verify.sh $rs_file \
         --log-air $rs_file_dir/log/$rs_file_basename.air --log-vir $rs_file_dir/log/$rs_file_basename.vir --log-vir-simple $rs_file_dir/log/$rs_file_bbasename.vir-simple --log-smt $rs_file_dir/log/$rs_file_basename.smt
 result=$?
 
