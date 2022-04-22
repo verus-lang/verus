c8184556c909d2f5a5b690c118289551d336442f
Chanhee Cho <channy1413@gmail.com>
2022-04-21 21:54:19 -0400

    Add assert_by_nonlinear testcases

--- source/rust_verify/tests/non_linear.rs
+++ source/rust_verify/tests/non_linear.rs
@@ -7,6 +7,7 @@ use common::*;
 
 // TODO: add testcases for assert_by_nonlinear
 // TODO: make sure testcases do not timeout
+// Test #[verifier(non_linear)]
 test_verify_one_file! {
     #[test] test1 code! {
         #[verifier(non_linear)]
@@ -65,6 +66,70 @@ test_verify_one_file! {
     } => Ok(())
 }
 
+// Test `assert_by_nonlinear
+test_verify_one_file! {
+    #[test] test5 code! {
+        #[proof]
+        fn test5_bound_checking(x: u32, y: u32, z:u32) {
+            requires([
+                x <= 0xffff,
+                y <= 0xffff,
+                z <= 0xffff,
+            ]);
+
+            assert_by_nonlinear( (x as int) * (z as int) == ((x*z) as int), {
+                requires([
+                    x <= 0xffff,
+                    z <= 0xffff,
+                ]);
+                assert(0 <= (x as int) * (z as int));
+                assert((x as int) * (z as int) <= 0xffff*0xffff);
+            });
+            assert((x as int) * (z as int) == ((x*z) as int));
+
+            assert_by_nonlinear((y as int) * (z as int) == ( (y*z) as int), {
+                requires([
+                    y <= 0xffff,
+                    z <= 0xffff,
+                ]);
+                assert(0 <= (y as int) * (z as int));
+                assert((y as int) * (z as int) <= 0xffff*0xffff);
+            });
+            assert((y as int) * (z as int) == ((y*z) as int));
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] test6 code! {
+        #[proof]
+        fn test6(x: u32, y: u32, z:u32) {
+            requires(x < 0xfff);
+
+            assert_by_nonlinear(x*x + x == x * (x + 1), {
+                requires(x < 0xfff);
+            });
+            assert(x*x + x == x * (x + 1));
+
+            assert_by_nonlinear(x*y == y*x, {});
+            assert(x*y == y*x);
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] test7 code! {
+        #[proof]
+        fn test6(x: int, y: int, z:int) {
+            assert_by_nonlinear( (x+y)*z == x*z + y*z, {});
+            assert( (x+y)*z == x*z + y*z);
+
+            assert_by_nonlinear( (x-y)*z == x*z - y*z, {});
+            assert( (x-y)*z == x*z - y*z);
+        }
+    } => Ok(())
+}
+
 test_verify_one_file! {
     #[test] test1_fails code! {
         #[verifier(non_linear)]
@@ -89,6 +154,24 @@ test_verify_one_file! {
                 3 <= z,
             ]);
             ensures (y*z > x); // FAILS
+        }
+    } => Err(e) => assert_one_fails(e)
+}
+
+test_verify_one_file! {
+    #[test] test3_fails code! {
+        #[proof]
+        fn test3_fails(x: int, y: int, z: int) {
+            assert_by_nonlinear(x*y == y*z,{}) // FAILS
+        }
+    } => Err(e) => assert_one_fails(e)
+}
+
+test_verify_one_file! {
+    #[test] test4_fails code! {
+        #[proof]
+        fn test4_fails(x: u32, y: u32, z: u32) {
+            assert_by_nonlinear( (x as int) * (z as int) == ((x*z) as int), {}); //FAILS
         }
     } => Err(e) => assert_one_fails(e)
 }
