83009747f7032075b295a2891825379dd32418a8
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2022-05-04 11:41:53 +0200

    Add multiplication commutativity to prelude

--- source/vir/src/prelude.rs
+++ source/vir/src/prelude.rs
@@ -274,6 +274,10 @@ pub(crate) fn prelude_nodes() -> Vec<Node> {
             :pattern (([Mul] x y))
         )))
         (axiom (forall ((x Int) (y Int)) (!
+            (= ([Mul] x y) ([Mul] y x))
+            :pattern (([Mul] x y))
+        )))
+        (axiom (forall ((x Int) (y Int)) (!
             (= ([EucDiv] x y) (div x y))
             :pattern (([EucDiv] x y))
         )))
