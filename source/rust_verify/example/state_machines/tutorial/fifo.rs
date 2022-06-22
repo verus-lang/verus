b560c4e914a4c3a9edb6f8c6634af8acae665c5d
Travis Hance <tjhance7@gmail.com>
2022-06-22 14:35:18 -0400

    fix tutorial/fifo.rs for z3-4.8.17

--- source/rust_verify/example/state_machines/tutorial/fifo.rs
+++ source/rust_verify/example/state_machines/tutorial/fifo.rs
@@ -577,6 +577,10 @@ pub fn new_queue<T>(len: usize) -> (Producer<T>, Consumer<T>) {
         backing_cells_vec.push(cell);
 
         perms.proof_insert(i, cell_perm);
+
+        assert(perms.dom().contains(i as nat));
+        assert(equal(backing_cells_vec.index(i as nat).id(), perms.index(i as nat).pcell));
+        assert(perms.index(i as nat).value.is_None());
     }
 
     // Vector for ids
