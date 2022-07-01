a4ccb09be20b824021c940475046298beb5de42a
Bryan Parno <parno@cmu.edu>
2022-07-01 13:57:28 -0400

    Notes on a bug

--- source/rust_verify/example/assert_by_compute.rs
+++ source/rust_verify/example/assert_by_compute.rs
@@ -7,15 +7,18 @@ fn main() {}
 fn compute_arith(x:u64) {
     assert_by_compute((7 + 7 * 2 > 20) && (22 - 5 <= 10*10)); // true
     assert_by_compute(x * 0 == 0);  // 0 == 0
-    // TODO: This currently produces: uClip(64, x) == x
+    // TODO: This currently produces: uClip(64, x) == x,
+    // due to the same issue mentioned below
     assert_by_compute(x * 1 == x);  // x == x
 }
 
 fn compute_ite() {
     assert_by_compute(9 == if 7 > 3 { 9 } else { 5 });  // 9 == 9
-    // TODO: This fails the expr_to_pure_exp check, due to the overflow checks that are inserted
-    // 1. Why doesn't this apply to compute_arith?
-    // 2. Why isn't the mode inferred to be spec?
-    // 3. Should we set the view_as_spec field in State to be true before calling expr_to_pure_exp?
+    // TODO: The example below fails the expr_to_pure_exp check,
+    // due to the overflow checks that are inserted.
+    // They are inserted because the mode checker treats constants as Exec,
+    // which leads to the arith being marked as Exec, and the mode checker
+    // confirms that an Exec expression can be passed as a Spec arg,
+    // but it doesn't "upgrade" the expression to Spec
     //assert_by_compute(9 == if (7 + 7 * 2 > 20) { 7 + 2 } else { 22 - 5 + 10*10 });
 }
