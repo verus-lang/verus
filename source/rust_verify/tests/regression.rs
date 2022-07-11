9e6696c278495f482ae3ba06e1cfade7aa9915b8
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2022-07-11 13:54:04 -0700

    Update regression tests for #189

--- source/rust_verify/tests/regression.rs
+++ source/rust_verify/tests/regression.rs
@@ -21,16 +21,13 @@ test_verify_one_file! {
     #[test] test_189b verus_code! {
         use set::*;
 
-        spec fn set_trigger<A>(a: A) -> bool { true }
+        spec fn different_set<V>(s: Set<V>) -> Set<V> { s }
 
         proof fn test_sets_1() {
             let s1: Set<i32> = Set::empty().insert(1);
 
-            // TODO allow triggers on `equal`?
-            assert (exists|s3: Set<i32>| set_trigger(s3) && s3 !== s1) by {
-                assert(!Set::empty().contains(1));
-                assert(Set::empty() !== s1);
-                assert(set_trigger(Set::<i32>::empty()));
+            assert (exists|s3: Set<i32>| different_set(s3) !== s1) by {
+                assert(!different_set(Set::empty()).contains(1));
             }
         }
     } => Ok(())
