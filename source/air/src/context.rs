227fbd3a194f0c8f0daf269489cd7cb421917586
Chanhee Cho <channy1413@gmail.com>
2022-07-26 14:06:46 -0400

    as bv fails more often in the newer version, adopt more tailored options

--- source/air/src/context.rs
+++ source/air/src/context.rs
@@ -240,12 +240,24 @@ impl Context {
             self.log_set_z3_param(option, &s);
         }
     }
+    
+    pub(crate) fn set_z3_param_str(&mut self, option: &str, value: &str, write_to_logs: bool) {
+        if option == "tactic.default_tactic"  {
+            if write_to_logs {
+                self.log_set_z3_param(option, &value.to_string());
+            }
+        } else {            
+            panic!("encountered unexpected z3 option: {} : {}", option, value);
+        }
+    }
 
     pub fn set_z3_param(&mut self, option: &str, value: &str) {
         if value == "true" {
             self.set_z3_param_bool(option, true, true);
         } else if value == "false" {
             self.set_z3_param_bool(option, false, true);
+        } else if value == "sat" {
+            self.set_z3_param_str(option, value, true);
         } else if value.contains(".") {
             let v = value.parse::<f64>().expect(&format!("could not parse option value {}", value));
             self.set_z3_param_f64(option, v, true);
