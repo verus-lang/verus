eb0aff63924fdb202fb938f40371ea790fe899ec
Andrea Lattuada <andrea@lattuada.me>
2022-08-01 17:18:40 +0200

    Remove some unnecessary env variables and some brittleness from scripts and code that are os and arch dependent

--- source/tools/docs.sh
+++ source/tools/docs.sh
@@ -13,13 +13,21 @@ case $(uname -m) in
     ;;
 esac
 
+if [ `uname` == "Darwin" ]; then
+    DYN_LIB_EXT=dylib
+    LIB_PATH="DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/${ARCH}-apple-darwin/lib"
+elif [ `uname` == "Linux" ]; then
+    DYN_LIB_EXT=so
+    LIB_PATH="LD_LIBRARY_PATH=../rust/install/lib/rustlib/${ARCH}-unknown-linux-gnu/lib"
+fi
+
 TEMPD=$(mktemp -d)
 
 cp -r pervasive $TEMPD
 echo "#[allow(rustdoc::invalid_rust_codeblocks)] pub mod pervasive;" >> $TEMPD/lib.rs
 
 echo "Running rustdoc..."
-VERUSDOC=1 VERUS_Z3_PATH="$(pwd)/z3" DYLD_LIBRARY_PATH=../rust/install/lib/rustlib/${ARCH}-apple-darwin/lib LD_LIBRARY_PATH=../rust/install/lib/rustlib/${ARCH}-unknown-linux-gnu/lib ../rust/install/bin/rustdoc --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.dylib --extern state_machines_macros=../rust/install/bin/libstate_machines_macros.dylib --edition=2018 -Zenable_feature=stmt_expr_attributes -Zenable_feature=box_syntax -Zenable_feature=box_patterns -Zenable_feature=negative_impls -Zproc-macro-backtrace $TEMPD/lib.rs
+eval ""VERUSDOC=1 VERUS_Z3_PATH="$(pwd)/z3" $LIB_PATH ../rust/install/bin/rustdoc --extern builtin=../rust/install/bin/libbuiltin.rlib --extern builtin_macros=../rust/install/bin/libbuiltin_macros.$DYN_LIB_EXT --extern state_machines_macros=../rust/install/bin/libstate_machines_macros.$DYN_LIB_EXT --edition=2018 -Zenable_feature=stmt_expr_attributes -Zenable_feature=box_syntax -Zenable_feature=box_patterns -Zenable_feature=negative_impls -Zproc-macro-backtrace $TEMPD/lib.rs""
 
 rm -rf $TEMPD
 
