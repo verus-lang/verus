edb49a0204106d0dbf43906e97971d4cb0493a0a
Travis Hance <tjhance7@gmail.com>
2022-09-02 00:57:45 -0400

    support arm64 for macs in get-z3.sh

--- source/tools/get-z3.sh
+++ source/tools/get-z3.sh
@@ -3,17 +3,17 @@
 z3_version="4.10.1"
 
 if [ `uname` == "Darwin" ]; then
-    filename=z3-$z3_version-x64-osx-10.16
-    wget https://github.com/Z3Prover/z3/releases/download/z3-$z3_version/$filename.zip
-    unzip $filename.zip
-    cp $filename/bin/z3 .
-    rm -r $filename
-    rm $filename.zip
+    if [[ $(uname -m) == 'arm64' ]]; then
+        filename=z3-$z3_version-arm64-osx-11.0
+    else
+        filename=z3-$z3_version-x64-osx-10.16
+    fi
 elif [ `uname` == "Linux" ]; then
     filename=z3-$z3_version-x64-glibc-2.31
-    wget https://github.com/Z3Prover/z3/releases/download/z3-$z3_version/$filename.zip
-    unzip $filename.zip
-    cp $filename/bin/z3 .
-    rm -r $filename
-    rm $filename.zip
 fi
+
+wget https://github.com/Z3Prover/z3/releases/download/z3-$z3_version/$filename.zip
+unzip $filename.zip
+cp $filename/bin/z3 .
+rm -r $filename
+rm $filename.zip
