dad819c849f85da7388885a802a3943b633c96b3
Chanhee Cho <channy1413@gmail.com>
2023-01-12 17:04:08 +0900

    add _regression_315 in the testcase name

--- source/rust_verify/tests/recursion.rs
+++ source/rust_verify/tests/recursion.rs
@@ -1217,7 +1217,7 @@ test_verify_one_file! {
 }
 
 test_verify_one_file! {
-    #[test] decreases_on_non_recursive_generic_datatype verus_code! {
+    #[test] decreases_on_non_recursive_generic_datatype_regression_315 verus_code! {
         // https://github.com/verus-lang/verus/issues/315
         use pervasive::option::*;
         spec fn max(a: nat, b: nat) -> nat {
