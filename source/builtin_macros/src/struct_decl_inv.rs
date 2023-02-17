c96530b78908505ef17885fede84b3f603b1f3cc
Travis Hance <tjhance7@gmail.com>
2023-02-17 18:55:43 -0500

    fix struct_decl_inv, add open

--- source/builtin_macros/src/struct_decl_inv.rs
+++ source/builtin_macros/src/struct_decl_inv.rs
@@ -698,7 +698,7 @@ fn output_invariant(
                 stream.extend(quote_spanned! { predicate.span() =>
                     #vis struct #predname { }
                     impl<#type_params> crate::pervasive::atomic_ghost::AtomicInvariantPredicate<#k_type, #v_type, #g_type> for #predname #where_clause {
-                        spec fn atomic_inv(#tmp_k: #k_type, #tmp_v: #v_type, #tmp_g: #g_type) -> bool {
+                        open spec fn atomic_inv(#tmp_k: #k_type, #tmp_v: #v_type, #tmp_g: #g_type) -> bool {
                             let #k_pat = #tmp_k;
                             let #v_pat = #tmp_v;
                             let #g_pat = #tmp_g;
@@ -716,7 +716,7 @@ fn output_invariant(
                 stream.extend(quote_spanned! { predicate.span() =>
                     #vis struct #predname { }
                     impl<#type_params> crate::pervasive::invariant::InvariantPredicate<#k_type, #v_type> for #predname #where_clause {
-                        spec fn inv(#tmp_k: #k_type, #tmp_v: #v_type) -> bool {
+                        open spec fn inv(#tmp_k: #k_type, #tmp_v: #v_type) -> bool {
                             let #k_pat = #tmp_k;
                             let #v_pat = #tmp_v;
                             #predicate
