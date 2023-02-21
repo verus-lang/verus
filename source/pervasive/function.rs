a4d40dd807bd6feb17e46455213c499bc7f8c1fe
Jay Bosamiya <jaybosamiya@gmail.com>
2023-02-21 01:10:01 +0000

    Extensionality axioms for n-argument functions

--- source/pervasive/function.rs
+++ source/pervasive/function.rs
@@ -25,3 +25,30 @@ verus! {
     ensures f1 == f2
   {}
 }
+
+/// A macro to conveniently generate similar functional extensionality axioms for functions that
+/// take `n` arguments.
+#[doc(hidden)]
+macro_rules! gen_fun_ext_n {
+  ($fun_ext:ident, $dummy_trigger:ident, $O:ident, $($x:ident : $I:ident),*) => {
+
+    verus! {
+      /// See [`dummy_trigger`]
+      pub closed spec fn $dummy_trigger < $($I),* >($($x : $I),*);
+
+      /// See [`fun_ext`]
+      #[verifier(external_body)]
+      pub proof fn $fun_ext<$($I),*, $O>(f1: FnSpec($($I),*,) -> $O, f2: FnSpec($($I),*,) -> $O)
+        requires forall |$($x: $I),*| #![trigger $dummy_trigger($($x),*)] f1($($x),*) == f2($($x),*)
+        ensures f1 == f2
+      {}
+    }
+  }
+
+}
+
+// Note: We start at 1 just for consistency; it is exactly equivalent to `fun_ext`
+gen_fun_ext_n! { fun_ext_1, dummy_trigger_1, B, x1: A1 }
+gen_fun_ext_n! { fun_ext_2, dummy_trigger_2, B, x1: A1, x2: A2 }
+gen_fun_ext_n! { fun_ext_3, dummy_trigger_3, B, x1: A1, x2: A2, x3: A3 }
+gen_fun_ext_n! { fun_ext_4, dummy_trigger_4, B, x1: A1, x2: A2, x3: A3, x4: A4 }
