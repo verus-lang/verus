f745ebc2adbfb9b2a53cf97472b886101bacaf5d
Jay Bosamiya <jaybosamiya@gmail.com>
2023-02-28 02:12:07 +0000

    Add some test case minimizers

---
+++ source/tools/minimizers/rlimit_exceeded.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -eo pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+VERIFY="${DIR}/../rust-verify.sh"
+
+timeout 10 "${VERIFY}" ./foo.rs 2>stderr >/dev/null || true
+
+if grep 'Resource limit (rlimit) exceeded' stderr >/dev/null; then
+    exit 0
+else
+    exit 1
+fi
