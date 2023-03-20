70a25e69204c48a2e4a51e6da02880666b03cf69
Travis Hance <tjhance7@gmail.com>
2023-03-20 14:03:56 -0400

    fix tests

--- source/pervasive/atomic.rs
+++ source/pervasive/atomic.rs
@@ -486,7 +486,7 @@ macro_rules! atomic_bool_methods {
             #[cfg(not(verus_macro_erase_ghost))]
             opens_invariants_none();
 
-            return self.ato.fetch_or(n, Ordering::SeqCst);
+            return self.ato.fetch_xor(n, Ordering::SeqCst);
         }
 
         #[inline(always)]
