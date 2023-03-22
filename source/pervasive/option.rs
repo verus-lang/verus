bfabf45e0947ded04a1d1823bcbe5925ad8a629f
Reto Achermann <achreto@gmail.com>
2023-03-22 14:28:10 -0700

    pervasive: adding exec methods for is_some()/is_none()
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- source/pervasive/option.rs
+++ source/pervasive/option.rs
@@ -35,6 +35,24 @@ impl<A> Option<A> {
         }
     }
 
+    pub const fn is_some(&self) -> (res: bool)
+        ensures res <==> self.is_Some(),
+    {
+        match self {
+            Option::Some(_) => true,
+            Option::None => false,
+        }
+    }
+
+    pub const fn is_none(&self) -> (res: bool)
+        ensures res <==> self.is_None(),
+    {
+        match self {
+            Option::Some(_) => false,
+            Option::None => true,
+        }
+    }
+
     pub fn as_ref(&self) -> (a: Option<&A>)
         ensures
           a.is_Some() <==> self.is_Some(),
