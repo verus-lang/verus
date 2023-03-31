776eace34a97e13375d35f83aff5e29182396f3e
nobody <nobody>
2023-03-31 22:52:32 +0200

    import rustc_driver and rustc_interface from rust 1.68.0

---
+++ dependencies/rustc_interface/src/lib.rs
@@ -0,0 +1,29 @@
+#![feature(box_patterns)]
+#![feature(decl_macro)]
+#![feature(internal_output_capture)]
+#![feature(thread_spawn_unchecked)]
+#![feature(once_cell)]
+#![feature(try_blocks)]
+#![recursion_limit = "256"]
+#![allow(rustc::potential_query_instability)]
+#![deny(rustc::untranslatable_diagnostic)]
+#![deny(rustc::diagnostic_outside_of_impl)]
+
+#[macro_use]
+extern crate tracing;
+
+mod callbacks;
+mod errors;
+pub mod interface;
+mod passes;
+mod proc_macro_decls;
+mod queries;
+pub mod util;
+
+pub use callbacks::setup_callbacks;
+pub use interface::{run_compiler, Config};
+pub use passes::{DEFAULT_EXTERN_QUERY_PROVIDERS, DEFAULT_QUERY_PROVIDERS};
+pub use queries::Queries;
+
+#[cfg(test)]
+mod tests;
