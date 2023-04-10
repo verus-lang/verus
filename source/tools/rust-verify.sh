eb8def57088876321b923cf001e26d215b1713d8
Jay Bosamiya <jaybosamiya@gmail.com>
2023-04-10 15:28:49 -0400

    Allow rust-verify.sh to be symlinked to

--- source/tools/rust-verify.sh
+++ source/tools/rust-verify.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname $(readlink -f "${BASH_SOURCE[0]}") )" >/dev/null 2>&1 && pwd )"
 SOURCE="$DIR/.."
 
 # default to release, if it is compiled
