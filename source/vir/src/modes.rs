verus-lang/verus

commit 750df5fc3f3b3dcb8263e9e65d83ed8cc4c24564

disallow get_variant functions in exec mode, fix #341

source/vir/src/modes.rs
@@ -372,7 +372,10 @@ fn get_var_loc_mode(
             }
             *to_mode
         }
-        ExprX::UnaryOpr(UnaryOpr::Field(FieldOpr { datatype, variant: _, field }), rcvr) => {
+        ExprX::UnaryOpr(
+            UnaryOpr::Field(FieldOpr { datatype, variant: _, field, get_variant }),
+            rcvr,
+        ) => {
             let rcvr_mode =
                 get_var_loc_mode(typing, outer_mode, expr_inner_mode, rcvr, init_not_mut)?;
             let datatype = &typing.datatypes[datatype].x;
@@ -383,7 +386,8 @@ fn get_var_loc_mode(
                 .find(|x| x.name == *field)
                 .expect("datatype field valid")
                 .a;
-            mode_join(rcvr_mode, *field_mode)
+            let call_mode = if *get_variant { Mode::Spec } else { rcvr_mode };
+            mode_join(call_mode, *field_mode)
         }
         ExprX::Block(stmts, Some(e1)) if stmts.len() == 0 => {
             // For now, only support the special case for Tracked::borrow_mut.
@@ -663,7 +667,13 @@ fn check_expr_handle_mut_arg(
         ExprX::UnaryOpr(UnaryOpr::TupleField { .. }, e1) => {
             return check_expr_handle_mut_arg(typing, outer_mode, erasure_mode, e1);
         }
-        ExprX::UnaryOpr(UnaryOpr::Field(FieldOpr { datatype, variant, field }), e1) => {
+        ExprX::UnaryOpr(
+            UnaryOpr::Field(FieldOpr { datatype, variant, field, get_variant }),
+            e1,
+        ) => {
+            if *get_variant && typing.check_ghost_blocks && typing.block_ghostness == Ghost::Exec {
+                return err_str(&expr.span, "cannot get variant in exec mode");
+            }
             let (e1_mode_read, e1_mode_write) =
                 check_expr_handle_mut_arg(typing, outer_mode, erasure_mode, e1)?;
             let datatype = &typing.datatypes[datatype];

source/builtin_macros/src/is_variant.rs
@@ -30,11 +30,12 @@ pub fn attribute_is_variant(
                         );
 
                         quote! {
-                            #[verifier::spec]
-                            #[allow(non_snake_case)]
-                            #[verifier::get_variant(#variant_ident, #field_ident)] /* vattr */
-                            pub fn #get_ident(self) -> #field_ty {
-                                unimplemented!()
+                            verus! {
+                                #[allow(non_snake_case)]
+                                #[verifier::get_variant(#variant_ident, #field_ident)] /* vattr */
+                                pub open spec fn #get_ident(self) -> #field_ty {
+                                    unimplemented!()
+                                }
                             }
                         }
                     })
@@ -54,11 +55,12 @@ pub fn attribute_is_variant(
                         );
 
                         quote! {
-                            #[verifier::spec]
-                            #[allow(non_snake_case)]
-                            #[verifier::get_variant(#variant_ident_str, #field_lit)] /* vattr */
-                            pub fn #get_ident(self) -> #field_ty {
-                                unimplemented!()
+                            verus! {
+                                #[allow(non_snake_case)]
+                                #[verifier::get_variant(#variant_ident_str, #field_lit)] /* vattr */
+                                pub open spec fn #get_ident(self) -> #field_ty {
+                                    unimplemented!()
+                                }
                             }
                         }
                     })
@@ -67,12 +69,13 @@ pub fn attribute_is_variant(
             };
 
             quote! {
-                #[verifier::spec]
-                #[verifier::is_variant(#variant_ident_str)] /* vattr */
-                #[allow(non_snake_case)]
-                pub fn #fun_ident(&self) -> bool { unimplemented!() }
+                verus! {
+                    #[verifier::is_variant(#variant_ident_str)] /* vattr */
+                    #[allow(non_snake_case)]
+                    pub open spec fn #fun_ident(&self) -> bool { unimplemented!() }
 
-                #get_fns
+                    #get_fns
+                }
             }
         })
         .collect::<proc_macro2::TokenStream>();

source/rust_verify/src/rust_to_vir_expr.rs
@@ -1254,6 +1254,7 @@ fn fn_call_to_vir<'tcx>(
                         GetVariantField::Named(n) => str_ident(&n),
                         GetVariantField::Unnamed(i) => positional_field_ident(i),
                     },
+                    get_variant: true,
                 }),
                 vir_args.into_iter().next().expect("missing arg for is_variant"),
             ));
@@ -2721,6 +2722,7 @@ pub(crate) fn expr_to_vir_innermost<'tcx>(
                         datatype,
                         variant: variant_name,
                         field: field_name,
+                        get_variant: false,
                     }),
                     vir_lhs,
                 ),

source/rust_verify_test/tests/regression.rs
@@ -263,6 +263,25 @@ test_verify_one_file! {
 }
 
 test_verify_one_file! {
+    #[test] is_variant_in_exec_issue_341 verus_code! {
+        pub struct Lock {}
+
+        #[is_variant]
+        pub enum OptionX<T> {
+            NoneX,
+            SomeX(T)
+        }
+
+        pub fn what_is_wrong() -> bool
+        {
+            let opt_lock = OptionX::SomeX(Lock{});
+            let lock = opt_lock.get_SomeX_0();   // This line triggers panic
+            true
+        }
+    } => Err(err) => assert_vir_error_msg(err, "cannot get variant in exec mode")
+}
+
+test_verify_one_file! {
     #[ignore] #[test] trait_argument_names_issue278 verus_code! {
         trait T {
             fn f(&self, a: usize) -> (res: usize)

source/vir/src/ast.rs
@@ -187,6 +187,7 @@ pub struct FieldOpr {
     pub datatype: Path,
     pub variant: Ident,
     pub field: Ident,
+    pub get_variant: bool,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash, PartialOrd, Ord, ToDebugSNode)]

source/vir/src/ast_simplify.rs
@@ -166,6 +166,7 @@ fn pattern_to_exprs_rec(
                     datatype: path.clone(),
                     variant: variant.clone(),
                     field: prefix_tuple_field(i),
+                    get_variant: false,
                 });
                 let field_exp = pattern_field_expr(&pattern.span, expr, &pat.typ, field_op);
                 let pattern_test = pattern_to_exprs_rec(ctx, state, &field_exp, pat, decls)?;
@@ -184,6 +185,7 @@ fn pattern_to_exprs_rec(
                     datatype: path.clone(),
                     variant: variant.clone(),
                     field: binder.name.clone(),
+                    get_variant: false,
                 });
                 let field_exp = pattern_field_expr(&pattern.span, expr, &binder.a.typ, field_op);
                 let pattern_test = pattern_to_exprs_rec(ctx, state, &field_exp, &binder.a, decls)?;
@@ -296,6 +298,7 @@ fn simplify_one_expr(ctx: &GlobalCtx, state: &mut State, expr: &Expr) -> Result<
                         datatype: path.clone(),
                         variant: variant.clone(),
                         field: field.name.clone(),
+                        get_variant: false,
                     });
                     let exprx = ExprX::UnaryOpr(op, update.clone());
                     let field_exp = SpannedTyped::new(&expr.span, &field.a.0, exprx);
@@ -434,7 +437,7 @@ fn tuple_get_field_expr(
     let datatype = state.tuple_type_name(tuple_arity);
     let variant = prefix_tuple_variant(tuple_arity);
     let field = prefix_tuple_field(field);
-    let op = UnaryOpr::Field(FieldOpr { datatype, variant, field });
+    let op = UnaryOpr::Field(FieldOpr { datatype, variant, field, get_variant: false });
     let field_expr = SpannedTyped::new(span, typ, ExprX::UnaryOpr(op, tuple_expr.clone()));
     field_expr
 }

source/vir/src/poly.rs
@@ -376,7 +376,7 @@ fn poly_expr(ctx: &Ctx, state: &mut State, expr: &Expr) -> Expr {
                     let exprx = ExprX::UnaryOpr(op.clone(), e1.clone());
                     SpannedTyped::new(&e1.span, &e1.typ, exprx)
                 }
-                UnaryOpr::Field(FieldOpr { datatype, variant, field }) => {
+                UnaryOpr::Field(FieldOpr { datatype, variant, field, get_variant: _ }) => {
                     let fields = &ctx.datatype_map[datatype].x.get_variant(variant).a;
                     let field = crate::ast_util::get_field(fields, field);
 

source/vir/src/sst_to_air.rs
@@ -797,7 +797,7 @@ pub(crate) fn exp_to_expr(ctx: &Ctx, exp: &Exp, expr_ctxt: &ExprCtxt) -> Result<
                 let name = suffix_global_id(&fun_to_air_ident(&height()));
                 Arc::new(ExprX::Apply(name, Arc::new(vec![expr])))
             }
-            UnaryOpr::Field(FieldOpr { datatype, variant, field }) => {
+            UnaryOpr::Field(FieldOpr { datatype, variant, field, get_variant: _ }) => {
                 let expr = exp_to_expr(ctx, exp, expr_ctxt)?;
                 Arc::new(ExprX::Apply(
                     variant_field_ident(datatype, variant, field),
@@ -1306,7 +1306,7 @@ fn assume_other_fields_unchanged_inner(
         [f] if f.len() == 0 => Ok(vec![]),
         _ => {
             let mut updated_fields: BTreeMap<_, Vec<_>> = BTreeMap::new();
-            let FieldOpr { datatype, variant, field: _ } = &updates[0][0];
+            let FieldOpr { datatype, variant, field: _, get_variant: _ } = &updates[0][0];
             for u in updates {
                 assert!(u[0].datatype == *datatype && u[0].variant == *variant);
                 updated_fields.entry(&u[0].field).or_insert(Vec::new()).push(u[1..].to_vec());
@@ -1322,6 +1322,7 @@ fn assume_other_fields_unchanged_inner(
                                 datatype: datatype.clone(),
                                 variant: variant.clone(),
                                 field: field.name.clone(),
+                                get_variant: false,
                             }),
                             base.clone(),
                         ),

commit c1175a28dcab6bd79d8e6a73145b39a0b5b5851d

fix error introduced by 750df5fc

source/vir/src/modes.rs
@@ -672,7 +672,7 @@ fn check_expr_handle_mut_arg(
             e1,
         ) => {
             if *get_variant && typing.check_ghost_blocks && typing.block_ghostness == Ghost::Exec {
-                return err_str(&expr.span, "cannot get variant in exec mode");
+                return error(&expr.span, "cannot get variant in exec mode");
             }
             let (e1_mode_read, e1_mode_write) =
                 check_expr_handle_mut_arg(typing, outer_mode, erasure_mode, e1)?;