4272bb6c0dab67f029535b276ff87e6073818b1e
Reto Achermann <achreto@gmail.com>
2023-05-31 09:28:28 -0700

    rust_verify: replace RefCell with Mutex to allow sending between threads
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- source/rust_verify/src/spans.rs
+++ source/rust_verify/src/spans.rs
@@ -4,7 +4,7 @@ use rustc_span::source_map::SourceMap;
 use rustc_span::{BytePos, ExternalSource, FileName, RealFileName, Span, SpanData};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use vir::ast::{SpannedTyped, Typ};
 use vir::def::Spanned;
 
@@ -37,7 +37,7 @@ enum ExternSourceInfo {
 struct ExternSourceFile {
     original_start_pos: BytePos,
     original_end_pos: BytePos,
-    info: Arc<std::cell::RefCell<ExternSourceInfo>>,
+    info: Arc<Mutex<ExternSourceInfo>>,
 }
 
 #[derive(Debug)]
@@ -103,7 +103,7 @@ impl SpanContextX {
                         let file = ExternSourceFile {
                             original_start_pos: BytePos(original.start_pos),
                             original_end_pos: BytePos(original.end_pos),
-                            info: Arc::new(std::cell::RefCell::new(info)),
+                            info: Arc::new(Mutex::new(info)),
                         };
                         if !imported_crates.contains_key(&imported_crate) {
                             imported_crates.insert(imported_crate, CrateInfo { files: Vec::new() });
@@ -126,7 +126,7 @@ impl SpanContextX {
                 let file = ExternSourceFile {
                     original_start_pos: BytePos(original.start_pos),
                     original_end_pos: BytePos(original.end_pos),
-                    info: Arc::new(std::cell::RefCell::new(info)),
+                    info: Arc::new(Mutex::new(info)),
                 };
                 imported_crates.get_mut(&imported_crate).unwrap().files.push(file);
             }
@@ -173,7 +173,7 @@ impl SpanContextX {
         if let Some(source_map) = source_map {
             // If rustc didn't originally load the file into the source_map,
             // we can try to request that it load the file on demand.
-            let mut info = info.borrow_mut();
+            let mut info = info.lock().unwrap();
             let filename = if let ExternSourceInfo::Delayed { filename, hash } = &*info {
                 Some((filename.clone(), hash.clone()))
             } else {
@@ -190,7 +190,7 @@ impl SpanContextX {
                 }
             }
         }
-        let locs = match &*info.borrow() {
+        let locs = match &*info.lock().unwrap() {
             ExternSourceInfo::Loaded { start_pos, end_pos } => {
                 Some((original_start_pos, original_end_pos, *start_pos, *end_pos))
             }
