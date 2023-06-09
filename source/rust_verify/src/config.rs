5195f8f6a0c3df31cbf96b809d636420f6e86d00
ahuoguo <ahuoguo@gmail.com>
2023-06-09 11:04:46 -0400

    version flag

--- source/rust_verify/src/config.rs
+++ source/rust_verify/src/config.rs
@@ -150,8 +150,10 @@ pub fn parse_args(program: &String, args: impl Iterator<Item = String>) -> (Args
     const OPT_PROFILE_ALL: &str = "profile-all";
     const OPT_COMPILE: &str = "compile";
     const OPT_NO_SOLVER_VERSION_CHECK: &str = "no-solver-version-check";
+    const OPT_VERSION: &str = "version";
 
     let mut opts = Options::new();
+    opts.optflag("", OPT_VERSION, "print version information");
     opts.optopt("", OPT_PERVASIVE_PATH, "Path of the pervasive module", "PATH");
     opts.optopt("", OPT_EXPORT, "Export Verus metadata for library crate", "CRATENAME=PATH");
     opts.optmulti("", OPT_IMPORT, "Import Verus metadata from library crate", "CRATENAME=PATH");
