6431890d8e4ace4093b9284e62041720b38123d8
Liz Austell <elaustell@davidson.edu>
2023-06-09 08:52:40 -0400

    fixed toml formatting. Verus version unfinished

--- source/error_report/src/main.rs
+++ source/error_report/src/main.rs
@@ -1,8 +1,14 @@
 use std::env;
 use std::fs::File;
+use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use std::process::{Command, Stdio};
+use toml::{map::Map, Value};
+use std::str;
+// use toml::ser;
+// use toml::Value;
+ // 0.5.1
 
 // TODO
 // use toml::{map::Map, Value};
@@ -29,22 +35,6 @@ use std::process::{Command, Stdio};
 const REL_Z3_PATH: &str = "../../../target-verus/release/z3";
 const REL_VERUS_PATH: &str = "../../../target-verus/release/verus";
 
-fn d_to_vec(file_name: String) -> Vec<String> {
-    let file = File::open(file_name).expect("Couldn't open file!");
-    let mut reader = BufReader::new(file);
-    let mut dependencies = String::new();
-    reader.read_line(&mut dependencies).expect("Could not read the first line");
-
-    let mut deps = Vec::new();
-    for dep in dependencies.split_whitespace() {
-        if dep.ends_with(".d") {
-            continue;
-        }
-        deps.push(dep.to_string());
-    }
-    deps
-}
-
 fn main() {
     // path where this piece of code is (then you can talk abt the rel path to z3/verus)
     let mut exe_dir = env::current_exe().expect("invalid directory");
@@ -63,7 +53,7 @@ fn main() {
     let z3_path = exe_dir.join(REL_Z3_PATH);
     // exe_dir.push(REL_Z3_PATH);
 
-    let msg1 = Command::new(z3_path).arg("--version").output().expect("failed to execute process");
+    let z3_version_output = Command::new(z3_path).arg("--version").output().expect("failed to execute process");
 
     let msg: &str = file_path.trim();
 
@@ -78,34 +68,67 @@ fn main() {
         .spawn()
         .expect("failed to execute process");
 
-    let msg2: std::process::Output = child.wait_with_output().expect("Failed to read stdout");
+    let verus_output: std::process::Output = child.wait_with_output().expect("Failed to read stdout");
 
     // no color information now (because we are writing in the file)
-    println!("{}", String::from_utf8_lossy(&msg2.stderr));
-    println!("{}", String::from_utf8_lossy(&msg2.stdout));
+    println!("{}", String::from_utf8_lossy(&verus_output.stderr));
+    println!("{}", String::from_utf8_lossy(&verus_output.stdout));
 
     // TODO: see above
     // probably change to file name?
-    write_toml(msg1, msg2);
+    write_toml(z3_version_output, verus_output);
 
     let d_file_name = create_zip(file_path);
 
-    // TODO: delete .d file and .toml file
     clean_up(d_file_name);
 }
 
-fn write_toml(msg1: std::process::Output, msg2: std::process::Output)
+fn create_toml(z3_version: String, verus_version: String, stdout: String, stderr: String) -> Value {
+
+    let mut versions = Map::new();
+    versions.insert("z3-version".to_string(), Value::String(z3_version));
+    versions.insert("verus-version".to_string(), Value::String(verus_version));
+
+    
+    let mut output = Map::new();
+    output.insert("stdout".to_string(), Value::String(stdout));
+    output.insert("stderr".to_string(),Value::String(stderr));
+
+    let mut map = Map::new();
+    map.insert("title".to_string(), Value::String("Error report file - details and dependencies".to_string()));
+    map.insert("Versions".into(), Value::Table(versions));
+    map.insert("Verus-output".into(), Value::Table(output));
+
+    Value::Table(map)
+}
+
+fn write_toml(z3_version_output: std::process::Output, verus_output: std::process::Output)
 {
     let mut file = File::create("error_report.toml").expect("Unable to create file");
     
-    write!(file, "{}", "Z3 version output:\n").unwrap();
-    file.write(&msg1.stdout).unwrap();
+    let mut z3_version = String::new();
+    z3_version.push_str(match str::from_utf8(&z3_version_output.stdout) {
+        Ok(val) => val,
+        Err(_) => panic!("got non UTF-8 data from git"),
+    });
 
-    write!(file, "{}", "\nverus stdout:\n").unwrap();
-    file.write(&msg2.stdout).unwrap();
+    let mut verus_version = "TODO".to_string();
 
-    write!(file, "{}", "\nverus stderr:\n").unwrap();
-    file.write(&msg2.stderr).unwrap();
+    let mut stdout = String::new();
+    stdout.push_str(match str::from_utf8(&verus_output.stdout) {
+        Ok(val) => val,
+        Err(_) => panic!("got non UTF-8 data from git"),
+    });
+    
+    let mut stderr = String::new();
+    stderr.push_str(match str::from_utf8(&verus_output.stderr) {
+        Ok(val) => val,
+        Err(_) => panic!("got non UTF-8 data from git"),
+    });
+
+    let toml_string = toml::to_string(&create_toml(z3_version, verus_version, stdout, stderr)).expect("Could not encode TOML value");
+    fs::write("error_report.toml", toml_string).expect("Could not write to file!");
+
 }
 
 pub fn create_zip(file_path: String) -> String
@@ -145,8 +168,23 @@ pub fn create_zip(file_path: String) -> String
     d_file_name
 }
 
-fn clean_up(d_file_name: String) {
-    Command::new("rm").arg("error_report.toml").output().expect("failed to delete toml file");
+fn d_to_vec(file_name: String) -> Vec<String> {
+    let file = File::open(file_name).expect("Couldn't open file!");
+    let mut reader = BufReader::new(file);
+    let mut dependencies = String::new();
+    reader.read_line(&mut dependencies).expect("Could not read the first line");
 
-    Command::new("rm").arg(d_file_name).output().expect("failed to delete .d file");
+    let mut deps = Vec::new();
+    for dep in dependencies.split_whitespace() {
+        if dep.ends_with(".d") {
+            continue;
+        }
+        deps.push(dep.to_string());
+    }
+    deps
+}
+
+fn clean_up(d_file_name: String) {
+    fs::remove_file("error_report.toml").expect("failed to delete toml file\n");
+    fs::remove_file(d_file_name).expect("failed to delete .d file\n");
 }
