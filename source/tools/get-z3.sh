e3b5b149b6bdfec85e1a5f9eb208b8508a08a70f
Travis Hance <tjhance7@gmail.com>
2023-06-14 20:24:55 -0400

    in get-z3 script, delete the z3 binary before moving the new one

--- source/tools/get-z3.sh
+++ source/tools/get-z3.sh
@@ -14,6 +14,10 @@ fi
 
 wget https://github.com/Z3Prover/z3/releases/download/z3-$z3_version/$filename.zip
 unzip $filename.zip
+
+# delete the existing z3 because of caching issue on macs
+rm -f z3
+
 cp $filename/bin/z3 .
 rm -r $filename
 rm $filename.zip
