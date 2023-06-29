77df8ff76638216eb2a243d234486600d40ab0ce
hayley-leblanc <hleblanc@utexas.edu>
2023-06-29 17:12:28 -0400

    Some specs for u32 and i32 methods

---
+++ source/pervasive/std_specs/num.rs
@@ -0,0 +1,141 @@
+#![allow(unused_imports)]
+use builtin::*;
+use builtin_macros::*;
+use crate::prelude::*;
+
+verus!{
+
+// == u32 methods ==
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_add(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        lhs + rhs > u32::MAX ==> result.is_None(),
+        lhs + rhs <= u32::MAX ==> {
+            match result {
+                Some(result) => result == lhs + rhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_add(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_add_signed(lhs: u32, rhs: i32) -> (result: Option<u32>)
+    ensures
+        lhs + rhs > u32::MAX || lhs + rhs < 0 ==> result.is_None(),
+        0 <= lhs + rhs <= u32::MAX ==> {
+            match result {
+                Some(result) => result == lhs + rhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_add_signed(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_sub(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        lhs - rhs < 0 ==> result.is_None(),
+        lhs - rhs >= 0 ==> {
+            match result {
+                Some(result) => result == lhs - rhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_sub(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_mul(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        lhs * rhs > u64::MAX ==> result.is_None(),
+        lhs * rhs <= u64::MAX ==> {
+            match result {
+                Some(result) => result == lhs * rhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_mul(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_div(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        rhs == 0 ==> result.is_None(),
+        rhs != 0 ==> {
+            match result {
+                Some(result) => result == lhs / rhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_div(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_div_euclid(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        rhs == 0 ==> result.is_None(),
+        rhs != 0 ==> {
+            match result {
+                Some(result) => result == lhs / rhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_div_euclid(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_rem(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        rhs == 0 ==> result.is_None(),
+        rhs != 0 ==> {
+            match result {
+                Some(result) => result == lhs % rhs,
+                None => false 
+            }
+        }
+{
+    lhs.checked_rem(rhs)
+}
+
+#[verifier::external_fn_specification]
+pub fn ex_u32_checked_rem_euclid(lhs: u32, rhs: u32) -> (result: Option<u32>)
+    ensures 
+        rhs == 0 ==> result.is_None(),
+        rhs != 0 ==> {
+            match result {
+                Some(result) => result == lhs % rhs,
+                None => false 
+            }
+        }
+{
+    lhs.checked_rem_euclid(rhs)
+}
+
+// == i32 methods ==
+
+#[verifier::external_fn_specification]
+pub fn ex_i32_checked_add(lhs: i32, rhs: i32) -> (result: Option<i32>)
+    ensures 
+        rhs + lhs > i32::MAX || rhs + lhs < i32::MIN ==> result.is_None(),
+        i32::MIN <= rhs + lhs <= i32::MAX ==> {
+            match result {
+                Some(result) => result == rhs + lhs,
+                None => false
+            }
+        }
+{
+    lhs.checked_add(rhs)
+}
+
+// spec checked euclidean operations for signed ints as well. 
+// test well with prime numbers to make sure you don't mess it up
+
+} // verus!
---
+++ source/rust_verify/example/std_test/num.rs
@@ -0,0 +1,79 @@
+#![allow(unused_imports)]
+use builtin::*;
+use builtin_macros::*;
+use vstd::prelude::*;
+use vstd::pervasive::runtime_assert;
+
+verus!{
+
+fn test_u32_checked_add() {
+    runtime_assert(u32::MAX.checked_add(1).is_none());
+    runtime_assert((u32::MAX - 1).checked_add(1).unwrap() == u32::MAX);
+    runtime_assert(5u32.checked_add(10).unwrap() == 15);
+}
+
+fn test_u32_checked_add_signed() {
+    runtime_assert(1u32.checked_add_signed(-2).is_none());
+    runtime_assert(1u32.checked_add_signed(-1).unwrap() == 0);
+    runtime_assert(5u32.checked_add_signed(10).unwrap() == 15);
+}
+
+fn test_u32_checked_sub() {
+    runtime_assert(1u32.checked_sub(2).is_none());
+    runtime_assert(1u32.checked_sub(1).unwrap() == 0);
+    runtime_assert(u32::MAX.checked_sub(u32::MAX).unwrap() == 0);
+    runtime_assert(10u32.checked_sub(5).unwrap() == 5);
+}
+
+fn test_u32_checked_mul() {
+    runtime_assert(u32::MAX.checked_mul(2).is_none());
+    runtime_assert(u32::MAX.checked_mul(1).unwrap() == u32::MAX);
+    runtime_assert(u32::MAX.checked_mul(0).unwrap() == 0);
+    runtime_assert((u32::MAX / 2).checked_mul(4).is_none());
+    runtime_assert(5u32.checked_mul(10).unwrap() == 50);
+}
+
+fn test_u32_checked_div() {
+    runtime_assert(u32::MAX.checked_div(0).is_none());
+    runtime_assert(u32::MAX.checked_div(1).unwrap() == u32::MAX);
+    runtime_assert(10u32.checked_div(5).unwrap() == 2);
+}
+
+fn test_u32_checked_div_euclid() {
+    runtime_assert(u32::MAX.checked_div_euclid(0).is_none());
+    runtime_assert(u32::MAX.checked_div_euclid(1).unwrap() == u32::MAX);
+    runtime_assert(10u32.checked_div_euclid(5).unwrap() == 2);
+}
+
+fn test_u32_checked_rem() {
+    runtime_assert(u32::MAX.checked_rem(0).is_none());
+    runtime_assert(0u32.checked_rem(0).is_none());
+    runtime_assert(0u32.checked_rem(1).unwrap() == 0);
+    runtime_assert(7u32.checked_rem(2).unwrap() == 1);
+}
+
+fn test_u32_checked_rem_euclid() {
+    runtime_assert(u32::MAX.checked_rem_euclid(0).is_none());
+    runtime_assert(0u32.checked_rem_euclid(0).is_none());
+    runtime_assert(0u32.checked_rem_euclid(1).unwrap() == 0);
+    runtime_assert(7u32.checked_rem_euclid(2).unwrap() == 1);
+}
+
+fn test_i32_checked_add() {
+    let neg_one: i32 = -1;
+    let neg_five: i32 = -5;
+    let neg_ten: i32 = -10;
+    let neg_fifteen: i32 = -15;
+    runtime_assert(i32::MAX.checked_add(1).is_none());
+    runtime_assert((i32::MAX - 2).checked_add(1).unwrap() == i32::MAX - 1);
+    runtime_assert(i32::MIN.checked_add(neg_one).is_none());
+    runtime_assert(i32::MIN.checked_add(1).unwrap() == i32::MIN + 1);
+    runtime_assert(neg_ten.checked_add(5).unwrap() == neg_five);
+    runtime_assert(10i32.checked_add(5).unwrap() == 15);
+    runtime_assert(10i32.checked_add(neg_five).unwrap() == 5);
+    runtime_assert(neg_ten.checked_add(neg_five).unwrap() == neg_fifteen);
+}
+
+fn main() {}
+
+}
