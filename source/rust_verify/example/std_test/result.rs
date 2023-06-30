619b88bf3f2dc2f827694970a29ed356e7dc303d
Jay Lorch <lorch@microsoft.com>
2023-06-30 08:46:07 -0400

    Fix Result tests

--- source/rust_verify/example/std_test/result.rs
+++ source/rust_verify/example/std_test/result.rs
@@ -5,33 +5,21 @@ verus! {
     
 fn is_ok_test() {
     let r1: Result<i32, i32> = Ok(7);
-    runtime_assert(r1.is_Ok() == true);
+    runtime_assert(r1.is_ok() == true);
     let r2: Result<i32, i32> = Err(7);
-    runtime_assert(r2.is_Ok() == false);
+    runtime_assert(r2.is_ok() == false);
     let r3: Result<i32, bool> = Err(true);
-    runtime_assert(r3.is_Ok() == false);
-}
-
-fn map_test_helper(x: i32) -> (y: i32)
-    requires
-        x < 100000
-    ensures
-        y > x
-{
-    x + 1
+    runtime_assert(r3.is_ok() == false);
 }
 
 fn map_test() {
     let r1: Result<i32, i32> = Ok(7);
-    let r2; Result<i32, i32> = r1.map(|i| i+1);
-    runtime_assert(r2.is_ok());
-    runtime_assert(r2.get_Ok_0() == 8);
-    let r3 = r1.map(map_test_helper);
-    runtime_assert(r3.is_ok());
-    runtime_assert(r3.get_Ok_0() > 7);
-    let r4: Result<i32, i32> = Err(9);
-    runtime_assert(r4.map(map_test_helper) == Err(9));
-    runtime_assert(r4.map(|i| i+1) == Err(9));
+    let op = |x: i32| -> (y: i32) requires x < 100000 ensures y > x { x + 1 };
+    let r2 = r1.map(op);
+    runtime_assert(r2.unwrap() > 7);
+    let r3: Result<i32, i32> = Err(9);
+    let r4: Result<i32, i32> = r3.map(op);
+    runtime_assert(r4.unwrap_err() == 9);
 }
     
 } // verus!
