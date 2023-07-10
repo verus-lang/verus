005fe23984af46bb00f722ae08e969a4c19d9200
Andrea Lattuada <lattuada@vmware.com>
2023-07-10 15:16:33 +0200

    add support for `cargo update` in vargo, minor refactoring, prevent inadvertently using cargo directly
    (when the vargo environment is active)

--- tools/activate.fish
+++ tools/activate.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+functions --erase cargo
+
 # Get the script's directory
 set SCRIPT_DIR (realpath (dirname (status -f)))
 
@@ -9,3 +11,8 @@ cargo build --release
 popd
 
 set -x PATH $SCRIPT_DIR/vargo/target/release $PATH
+
+function cargo
+  echo "when working on Verus do not use cargo directly, use vargo instead"
+  echo "if you need to, you can still access cargo directly by starting a new shell without running the activate command"
+end
