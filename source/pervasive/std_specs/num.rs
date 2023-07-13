2daf0d59a84c74dcf8fc1d0e4408523139e221b6
hayley-leblanc <hleblanc@utexas.edu>
2023-07-13 11:42:45 -0700

    Refactor most ensures clauses

--- source/pervasive/std_specs/num.rs
+++ source/pervasive/std_specs/num.rs
@@ -11,12 +11,7 @@ verus!{
 pub fn ex_u32_checked_add(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
         lhs + rhs > u32::MAX ==> result.is_None(),
-        lhs + rhs <= u32::MAX ==> {
-            match result {
-                Some(result) => result == lhs + rhs,
-                None => false
-            }
-        }
+        lhs + rhs <= u32::MAX ==> result == Some((lhs + rhs) as u32)
 {
     lhs.checked_add(rhs)
 }
@@ -25,12 +20,7 @@ pub fn ex_u32_checked_add(lhs: u32, rhs: u32) -> (result: Option<u32>)
 pub fn ex_u32_checked_add_signed(lhs: u32, rhs: i32) -> (result: Option<u32>)
     ensures
         lhs + rhs > u32::MAX || lhs + rhs < 0 ==> result.is_None(),
-        0 <= lhs + rhs <= u32::MAX ==> {
-            match result {
-                Some(result) => result == lhs + rhs,
-                None => false
-            }
-        }
+        lhs + rhs <= u32::MAX ==> result == Some((lhs + rhs) as u32)
 {
     lhs.checked_add_signed(rhs)
 }
@@ -39,12 +29,7 @@ pub fn ex_u32_checked_add_signed(lhs: u32, rhs: i32) -> (result: Option<u32>)
 pub fn ex_u32_checked_sub(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
         lhs - rhs < 0 ==> result.is_None(),
-        lhs - rhs >= 0 ==> {
-            match result {
-                Some(result) => result == lhs - rhs,
-                None => false
-            }
-        }
+        lhs - rhs >= 0 ==> result == Some((lhs - rhs) as u32)
 {
     lhs.checked_sub(rhs)
 }
@@ -52,13 +37,8 @@ pub fn ex_u32_checked_sub(lhs: u32, rhs: u32) -> (result: Option<u32>)
 #[verifier::external_fn_specification]
 pub fn ex_u32_checked_mul(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
-        lhs * rhs > u64::MAX ==> result.is_None(),
-        lhs * rhs <= u64::MAX ==> {
-            match result {
-                Some(result) => result == lhs * rhs,
-                None => false
-            }
-        }
+        lhs * rhs > u32::MAX ==> result.is_None(),
+        lhs * rhs <= u32::MAX ==> result == Some((lhs * rhs) as u32)
 {
     lhs.checked_mul(rhs)
 }
@@ -67,12 +47,7 @@ pub fn ex_u32_checked_mul(lhs: u32, rhs: u32) -> (result: Option<u32>)
 pub fn ex_u32_checked_div(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
         rhs == 0 ==> result.is_None(),
-        rhs != 0 ==> {
-            match result {
-                Some(result) => result == lhs / rhs,
-                None => false
-            }
-        }
+        rhs != 0 ==> result == Some((lhs / rhs) as u32)
 {
     lhs.checked_div(rhs)
 }
@@ -81,12 +56,7 @@ pub fn ex_u32_checked_div(lhs: u32, rhs: u32) -> (result: Option<u32>)
 pub fn ex_u32_checked_div_euclid(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
         rhs == 0 ==> result.is_None(),
-        rhs != 0 ==> {
-            match result {
-                Some(result) => result == lhs / rhs,
-                None => false
-            }
-        }
+        rhs != 0 ==> result == Some((lhs / rhs) as u32)
 {
     lhs.checked_div_euclid(rhs)
 }
@@ -95,12 +65,7 @@ pub fn ex_u32_checked_div_euclid(lhs: u32, rhs: u32) -> (result: Option<u32>)
 pub fn ex_u32_checked_rem(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
         rhs == 0 ==> result.is_None(),
-        rhs != 0 ==> {
-            match result {
-                Some(result) => result == lhs % rhs,
-                None => false 
-            }
-        }
+        rhs != 0 ==> result == Some((lhs % rhs) as u32)
 {
     lhs.checked_rem(rhs)
 }
@@ -109,12 +74,7 @@ pub fn ex_u32_checked_rem(lhs: u32, rhs: u32) -> (result: Option<u32>)
 pub fn ex_u32_checked_rem_euclid(lhs: u32, rhs: u32) -> (result: Option<u32>)
     ensures 
         rhs == 0 ==> result.is_None(),
-        rhs != 0 ==> {
-            match result {
-                Some(result) => result == lhs % rhs,
-                None => false 
-            }
-        }
+        rhs != 0 ==> result == Some((lhs % rhs) as u32)
 {
     lhs.checked_rem_euclid(rhs)
 }
@@ -125,12 +85,7 @@ pub fn ex_u32_checked_rem_euclid(lhs: u32, rhs: u32) -> (result: Option<u32>)
 pub fn ex_i32_checked_add(lhs: i32, rhs: i32) -> (result: Option<i32>)
     ensures 
         lhs + rhs > i32::MAX || lhs + rhs < i32::MIN ==> result.is_None(),
-        i32::MIN <= lhs + rhs <= i32::MAX ==> {
-            match result {
-                Some(result) => result == lhs + rhs,
-                None => false
-            }
-        }
+        i32::MIN <= lhs + rhs <= i32::MAX ==> result == Some((lhs + rhs) as i32)
 {
     lhs.checked_add(rhs)
 }
@@ -139,12 +94,7 @@ pub fn ex_i32_checked_add(lhs: i32, rhs: i32) -> (result: Option<i32>)
 pub fn ex_i32_checked_add_unsigned(lhs: i32, rhs: u32) -> (result: Option<i32>)
     ensures 
         lhs + rhs > i32::MAX ==> result.is_None(),
-        lhs + rhs <= i32::MAX ==> {
-            match result {
-                Some(result) => result == lhs + rhs,
-                None => false
-            }
-        }
+        lhs + rhs <= i32::MAX ==> result == Some((lhs + rhs) as i32)
 {
     lhs.checked_add_unsigned(rhs)
 }
@@ -153,11 +103,7 @@ pub fn ex_i32_checked_add_unsigned(lhs: i32, rhs: u32) -> (result: Option<i32>)
 pub fn ex_i32_checked_sub(lhs: i32, rhs: i32) -> (result: Option<i32>)
     ensures 
         lhs - rhs > i32::MAX || lhs - rhs < i32::MIN ==> result.is_None(),
-        i32::MIN <= lhs - rhs <= i32::MAX ==>
-            match result {
-                Some(result) => result == lhs - rhs,
-                None => false
-            }
+        i32::MIN <= lhs - rhs <= i32::MAX ==> result == Some((lhs - rhs) as i32)
 {
     lhs.checked_sub(rhs)
 }
@@ -166,11 +112,7 @@ pub fn ex_i32_checked_sub(lhs: i32, rhs: i32) -> (result: Option<i32>)
 pub fn ex_i32_checked_sub_unsigned(lhs: i32, rhs: u32) -> (result: Option<i32>)
     ensures
         lhs - rhs < i32::MIN ==> result.is_None(),
-        i32::MIN <= lhs - rhs ==> 
-            match result {
-                Some(result) => result == lhs - rhs,
-                None => false
-            }
+        i32::MIN <= lhs - rhs ==> result == Some((lhs - rhs) as i32)
 {
     lhs.checked_sub_unsigned(rhs)
 }
@@ -179,11 +121,7 @@ pub fn ex_i32_checked_sub_unsigned(lhs: i32, rhs: u32) -> (result: Option<i32>)
 pub fn ex_i32_checked_mul(lhs: i32, rhs: i32) -> (result: Option<i32>)
     ensures 
         lhs * rhs < i32::MIN || lhs * rhs > i32::MAX ==> result.is_None(),
-        i32::MIN <= lhs * rhs <= i32::MAX ==>
-            match result {
-                Some(result) => result == lhs * rhs,
-                None => false 
-            }
+        i32::MIN <= lhs * rhs <= i32::MAX ==> result == Some((lhs * rhs) as i32)
 {
     lhs.checked_mul(rhs)
 }
@@ -209,10 +147,7 @@ pub fn ex_i32_checked_div(lhs: i32, rhs: i32) -> (result: Option<i32>)
             if output < i32::MIN || output > i32::MAX {
                 result.is_None()
             } else {
-                match result {
-                    Some(result) => result == output,
-                    None => false
-                }
+                result == Some(output as i32)
             }
         })
 {
@@ -224,11 +159,7 @@ pub fn ex_i32_checked_div_euclid(lhs: i32, rhs: i32) -> (result: Option<i32>)
     ensures
         rhs == 0 ==> result.is_None(), 
         lhs / rhs < i32::MIN || lhs / rhs > i32::MAX ==> result.is_None(),
-        i32::MIN <= lhs / rhs <= i32::MAX ==> 
-            match result {
-                Some(result) => result == lhs / rhs,
-                None => false
-            }
+        i32::MIN <= lhs / rhs <= i32::MAX ==> result == Some((lhs / rhs) as i32)
 {
     lhs.checked_div_euclid(rhs)
 }
@@ -254,10 +185,7 @@ pub fn ex_i32_checked_rem(lhs: i32, rhs: i32) -> (result: Option<i32>)
             if output < i32::MIN || output > i32::MAX {
                 result.is_None()
             } else {
-                match result {
-                    Some(result) => result == output,
-                    None => false
-                }
+                result == Some(output as i32)
             }
         })
 {
@@ -269,11 +197,7 @@ pub fn ex_i32_checked_rem_euclid(lhs: i32, rhs: i32) -> (result: Option<i32>)
     ensures 
         rhs == 0 ==> result.is_None(),
         lhs % rhs < i32::MIN || lhs % rhs > i32::MAX ==> result.is_None(),
-        i32::MIN <= lhs % rhs <= i32::MAX ==> 
-            match result {
-                Some(result) => result == lhs % rhs,
-                None => false
-            }
+        i32::MIN <= lhs % rhs <= i32::MAX ==> result == Some((lhs % rhs) as i32)
 {
     lhs.checked_rem_euclid(rhs)
 }
