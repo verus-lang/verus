9492843251a156ef30c2f23f6eebbd6fdb795792
Liz Austell <elaustell@davidson.edu>
2023-07-27 17:28:34 -0400

    some set lemmas to associated functions

--- source/pervasive/set_lib.rs
+++ source/pervasive/set_lib.rs
@@ -31,7 +31,7 @@ impl<A> Set<A> {
             b <==> self =~= Set::empty(),
     {
         if self.finite() && self.len() == 0 {
-            lemma_len0_is_empty::<A>(self);
+            self.lemma_len0_is_empty();
         }
         self =~= Set::empty()
     }
@@ -135,7 +135,6 @@ impl<A> Set<A> {
         }
     }
 
-
     /// Any totally-ordered set contains a unique maximal (equivalently, greatest) element.
     /// Returns an arbitrary value if r is not a total ordering
     pub open spec fn find_unique_maximal(self, r: FnSpec(A,A) -> bool) -> A 
@@ -175,55 +174,147 @@ impl<A> Set<A> {
             Multiset::<A>::empty().insert(self.choose()).add(self.remove(self.choose()).to_multiset())
         }
     }
-}
 
-/// A finite set with length 0 is equivalent to the empty set.
-pub proof fn lemma_len0_is_empty<A>(s: Set<A>)
-    requires
-        s.finite(),
-        s.len() == 0,
-    ensures
-        s == Set::<A>::empty(),
-{
-    if exists|a: A| s.contains(a) {
-        // derive contradiction:
-        assert(s.remove(s.choose()).len() + 1 == 0);
+    /// A finite set with length 0 is equivalent to the empty set.
+    pub proof fn lemma_len0_is_empty(self)
+        requires
+            self.finite(),
+            self.len() == 0,
+        ensures
+            self == Set::<A>::empty(),
+    {
+        if exists|a: A| self.contains(a) {
+            // derive contradiction:
+            assert(self.remove(self.choose()).len() + 1 == 0);
+        }
+        assert(self =~= Set::empty());
     }
-    assert(s =~= Set::empty());
-}
-
-/// A singleton set has length 1.
-pub proof fn lemma_singleton_size<A>(s: Set<A>)
-    requires
-        s.is_singleton(),
-    ensures
-        s.len() == 1
-{
-    lemma_set_properties::<A>();
-    let elt = choose |elt: A| s.contains(elt);
-    assert(s.remove(elt).insert(elt) =~= s);
-    assert(s.remove(elt) =~= Set::empty());
     
-}
-
-/// A set has exactly one element, if and only if, it has at least one element and any two elements are equal.
-pub proof fn lemma_is_singleton<A>(s: Set<A>)
-    requires
-        s.finite(),
-    ensures
-        s.is_singleton() == (s.len() == 1)
-{
-    if s.is_singleton() {
-        lemma_singleton_size(s);
+    /// A singleton set has length 1.
+    pub proof fn lemma_singleton_size(self)
+        requires
+            self.is_singleton(),
+        ensures
+            self.len() == 1
+    {
+        lemma_set_properties::<A>();
+        let elt = choose |elt: A| self.contains(elt);
+        assert(self.remove(elt).insert(elt) =~= self);
+        assert(self.remove(elt) =~= Set::empty());
+        
     }
-    if s.len() == 1 {
-        assert forall |x: A, y: A| s.contains(x) && s.contains(y) implies x==y by {
-            let x = choose |x: A| s.contains(x);
-            lemma_set_properties::<A>();
-            assert(s.remove(x).len() == 0);
-            assert(s.insert(x) =~= s);
+
+    /// A set has exactly one element, if and only if, it has at least one element and any two elements are equal.
+    pub proof fn lemma_is_singleton(s: Set<A>)
+        requires
+            s.finite(),
+        ensures
+            s.is_singleton() == (s.len() == 1)
+    {
+        if s.is_singleton() {
+            s.lemma_singleton_size();
+        }
+        if s.len() == 1 {
+            assert forall |x: A, y: A| s.contains(x) && s.contains(y) implies x==y by {
+                let x = choose |x: A| s.contains(x);
+                lemma_set_properties::<A>();
+                assert(s.remove(x).len() == 0);
+                assert(s.insert(x) =~= s);
+            }
         }
     }
+
+    /// The result of filtering a finite set is finite and has size less than or equal to the original set.
+    pub proof fn lemma_len_filter(self, f: FnSpec(A) -> bool)
+        requires
+            self.finite(),
+        ensures
+            self.filter(f).finite(),
+            self.filter(f).len() <= self.len(),
+        decreases
+            self.len(),
+    {
+        lemma_len_intersect::<A>(self, Set::new(f));
+    }
+
+    /// In a pre-ordered set, a greatest element is necessarily maximal.
+    pub proof fn lemma_greatest_implies_maximal(self, r: FnSpec(A,A) -> bool, max: A)
+        requires pre_ordering(r),
+        ensures is_greatest(r, max, self) ==> is_maximal(r, max, self),
+    {}
+
+    /// In a pre-ordered set, a least element is necessarily minimal.
+    pub proof fn lemma_least_implies_minimal(self, r: FnSpec(A,A) -> bool, min: A)
+        requires pre_ordering(r),
+        ensures is_least(r, min, self) ==> is_minimal(r, min, self),
+    {}
+
+    /// In a totally-ordered set, an element is maximal if and only if it is a greatest element.
+    pub proof fn lemma_maximal_equivalent_greatest(self, r: FnSpec(A,A) -> bool, max: A)
+        requires total_ordering(r),
+        ensures is_greatest(r, max, self) <==> is_maximal(r, max, self),
+    {
+        assert(is_maximal(r, max, self) ==> forall |x:A| !self.contains(x) || !r(max,x) || r(x,max));
+    }
+
+    /// In a totally-ordered set, an element is maximal if and only if it is a greatest element.
+    pub proof fn lemma_minimal_equivalent_least(self, r: FnSpec(A,A) -> bool, min: A)
+        requires total_ordering(r),
+        ensures is_least(r, min, self) <==> is_minimal(r, min, self),
+    {
+        assert(is_minimal(r, min, self) ==> forall |x:A| !self.contains(x) || !r(x,min) || r(min,x));
+    }
+
+    /// In a partially-ordered set, there exists at most one least element.
+    pub proof fn lemma_least_is_unique(self, r: FnSpec(A,A) -> bool)
+        requires partial_ordering(r),
+        ensures forall |min: A, min_prime: A| is_least(r, min, self) && is_least(r, min_prime, self) ==> min == min_prime,
+    {
+        assert forall |min: A, min_prime: A| is_least(r, min, self) && is_least(r, min_prime, self) implies min == min_prime by {
+            assert(r(min, min_prime));
+            assert(r(min_prime, min));
+        }
+    }
+
+    /// In a partially-ordered set, there exists at most one greatest element.
+    pub proof fn lemma_greatest_is_unique(self, r: FnSpec(A,A) -> bool)
+        requires partial_ordering(r),
+        ensures forall |max: A, max_prime: A| is_greatest(r, max, self) && is_greatest(r, max_prime, self) ==> max == max_prime,
+    {
+        assert forall |max: A, max_prime: A| is_greatest(r, max, self) && is_greatest(r, max_prime, self) implies max == max_prime by {
+            assert(r(max_prime, max));
+            assert(r(max, max_prime));
+        }
+    }
+
+    /// In a totally-ordered set, there exists at most one minimal element.
+    pub proof fn lemma_minimal_is_unique(self, r: FnSpec(A,A) -> bool)
+        requires
+            total_ordering(r),
+        ensures
+            forall |min: A, min_prime: A| is_minimal(r, min, self) && is_minimal(r, min_prime, self) ==> min == min_prime,
+    {
+        assert forall |min: A, min_prime: A| is_minimal(r, min, self) && is_minimal(r, min_prime, self) implies min == min_prime by {
+            self.lemma_minimal_equivalent_least(r,min);
+            self.lemma_minimal_equivalent_least(r,min_prime);
+            self.lemma_least_is_unique(r);
+        }
+    }
+
+    /// In a totally-ordered set, there exists at most one maximal element.
+    pub proof fn lemma_maximal_is_unique(self, r: FnSpec(A,A) -> bool)
+        requires
+            self.finite(),
+            total_ordering(r),
+        ensures
+            forall |max: A, max_prime: A| is_maximal(r, max, self) && is_maximal(r, max_prime, self) ==> max == max_prime,
+    {
+        assert forall |max: A, max_prime: A| is_maximal(r, max, self) && is_maximal(r, max_prime, self) implies max == max_prime by {
+            self.lemma_maximal_equivalent_greatest(r,max);
+            self.lemma_maximal_equivalent_greatest(r,max_prime);
+            self.lemma_greatest_is_unique(r);
+        }
+    }  
 }
 
 /// The size of a union of two sets is less than or equal to the size of
@@ -327,19 +418,6 @@ pub proof fn lemma_len_difference<A>(s1: Set<A>, s2: Set<A>)
     }
 }
 
-/// The result of filtering a finite set is finite and has size less than or equal to the original set.
-pub proof fn lemma_len_filter<A>(s: Set<A>, f: FnSpec(A) -> bool)
-    requires
-        s.finite(),
-    ensures
-        s.filter(f).finite(),
-        s.filter(f).len() <= s.len(),
-    decreases
-        s.len(),
-{
-    lemma_len_intersect::<A>(s, Set::new(f));
-}
-
 /// Creates a finite set of integers in the range [lo, hi).
 pub open spec fn set_int_range(lo: int, hi: int) -> Set<int> {
     Set::new(|i: int| lo <= i && i < hi)
@@ -405,85 +483,6 @@ pub proof fn lemma_map_size<A,B>(x: Set<A>, y: Set<B>, f: FnSpec(A) -> B)
         lemma_map_size(x.remove(a), y.remove(f(a)), f);
     }
 }
-
-/// In a pre-ordered set, a greatest element is necessarily maximal.
-pub proof fn lemma_greatest_implies_maximal<A>(r: FnSpec(A,A) -> bool, max: A, s: Set<A>)
-    requires pre_ordering(r),
-    ensures is_greatest(r, max, s) ==> is_maximal(r, max, s),
-{}
-
-/// In a pre-ordered set, a least element is necessarily minimal.
-pub proof fn lemma_least_implies_minimal<A>(r: FnSpec(A,A) -> bool, min: A, s: Set<A>)
-    requires pre_ordering(r),
-    ensures is_least(r, min, s) ==> is_minimal(r, min, s),
-{}
-
-/// In a totally-ordered set, an element is maximal if and only if it is a greatest element.
-pub proof fn lemma_maximal_equivalent_greatest<A>(r: FnSpec(A,A) -> bool, max: A, s: Set<A>)
-    requires total_ordering(r),
-    ensures is_greatest(r, max, s) <==> is_maximal(r, max, s),
-{
-    assert(is_maximal(r, max, s) ==> forall |x:A| !s.contains(x) || !r(max,x) || r(x,max));
-}
-
-/// In a totally-ordered set, an element is maximal if and only if it is a greatest element.
-pub proof fn lemma_minimal_equivalent_least<A>(r: FnSpec(A,A) -> bool, min: A, s: Set<A>)
-    requires total_ordering(r),
-    ensures is_least(r, min, s) <==> is_minimal(r, min, s),
-{
-    assert(is_minimal(r, min, s) ==> forall |x:A| !s.contains(x) || !r(x,min) || r(min,x));
-}
-
-/// In a partially-ordered set, there exists at most one least element.
-pub proof fn lemma_least_is_unique<A>(r: FnSpec(A,A) -> bool, s: Set<A>)
-    requires partial_ordering(r),
-    ensures forall |min: A, min_prime: A| is_least(r, min, s) && is_least(r, min_prime, s) ==> min == min_prime,
-{
-   assert forall |min: A, min_prime: A| is_least(r, min, s) && is_least(r, min_prime, s) implies min == min_prime by {
-        assert(r(min, min_prime));
-        assert(r(min_prime, min));
-   }
-}
-
-/// In a partially-ordered set, there exists at most one greatest element.
-pub proof fn lemma_greatest_is_unique<A>(r: FnSpec(A,A) -> bool, s: Set<A>)
-    requires partial_ordering(r),
-    ensures forall |max: A, max_prime: A| is_greatest(r, max, s) && is_greatest(r, max_prime, s) ==> max == max_prime,
-{
-   assert forall |max: A, max_prime: A| is_greatest(r, max, s) && is_greatest(r, max_prime, s) implies max == max_prime by {
-        assert(r(max_prime, max));
-        assert(r(max, max_prime));
-   }
-}
-
-/// In a totally-ordered set, there exists at most one minimal element.
-pub proof fn lemma_minimal_is_unique<A>(r: FnSpec(A,A) -> bool, s: Set<A>)
-    requires
-        total_ordering(r),
-    ensures
-        forall |min: A, min_prime: A| is_minimal(r, min, s) && is_minimal(r, min_prime, s) ==> min == min_prime,
-{
-    assert forall |min: A, min_prime: A| is_minimal(r, min, s) && is_minimal(r, min_prime, s) implies min == min_prime by {
-        lemma_minimal_equivalent_least(r,min,s);
-        lemma_minimal_equivalent_least(r,min_prime,s);
-        lemma_least_is_unique(r,s);
-   }
-}
-
-/// In a totally-ordered set, there exists at most one maximal element.
-pub proof fn lemma_maximal_is_unique<A>(r: FnSpec(A,A) -> bool, s: Set<A>)
-    requires
-        s.finite(),
-        total_ordering(r),
-    ensures
-        forall |max: A, max_prime: A| is_maximal(r, max, s) && is_maximal(r, max_prime, s) ==> max == max_prime,
-{
-    assert forall |max: A, max_prime: A| is_maximal(r, max, s) && is_maximal(r, max_prime, s) implies max == max_prime by {
-        lemma_maximal_equivalent_greatest(r,max,s);
-        lemma_maximal_equivalent_greatest(r,max_prime,s);
-        lemma_greatest_is_unique(r,s);
-   }
-}  
 
 /// Proof of correctness and expected behavior for `Set::find_unique_minimal`.
 pub proof fn find_unique_minimal_ensures<A>(s: Set<A>, r: FnSpec(A,A) -> bool)
