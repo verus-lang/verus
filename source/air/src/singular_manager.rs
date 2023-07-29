526f31b6cbdd722bcb020389142e906c0c3a8062
Yi Zhou <yizhou7@outlook.com>
2023-07-28 21:06:58 -0400

    use a different callback for singular writer, remove buggy echo write from it

--- source/air/src/singular_manager.rs
+++ source/air/src/singular_manager.rs
@@ -1,7 +1,6 @@
-use crate::smt_process::writer_thread;
 use std::io::{BufRead, BufReader, Write};
-use std::process::ChildStdout;
-use std::sync::mpsc::{channel, Sender};
+use std::process::{ChildStdin, ChildStdout};
+use std::sync::mpsc::{channel, Receiver, Sender};
 
 pub struct SingularManager {
     pub singular_executable_name: String,
@@ -21,17 +20,35 @@ impl SingularManager {
     }
     pub fn launch(&self) -> SingularProcess {
         let mut child = std::process::Command::new(&self.singular_executable_name)
+            // .arg("--no-tty")
             .arg("--quiet")
+            // .arg("--no-shell")
             .stdin(std::process::Stdio::piped())
             .stdout(std::process::Stdio::piped())
             .spawn()
             .expect("could not execute Singular process");
-        let singular_pipe_stdout = BufReader::new(child.stdout.take().expect("take stdout"));
+
+        let child_stdout = child.stdout.take().expect("take stdout");
+        let singular_pipe_stdout = BufReader::new(child_stdout);
+
         let child_stdin = child.stdin.take().expect("take stdin");
         let (sender, receiver) = channel();
-        std::thread::spawn(move || writer_thread(receiver, child_stdin));
+        std::thread::spawn(move || singular_writer_thread(receiver, child_stdin));
         SingularProcess { requests: sender, singular_pipe_stdout }
     }
+}
+
+fn singular_writer_thread(requests: Receiver<Vec<u8>>, mut singular_pipe_stdin: ChildStdin) {
+    while let Ok(req) = requests.recv() {
+        singular_pipe_stdin
+            .write_all(&req)
+            .and_then(|_| writeln!(&singular_pipe_stdin))
+            // .and_then(|_| writeln!(&singular_pipe_stdin, "print("ok");", DONE))
+            .and_then(|_| singular_pipe_stdin.flush())
+            // The Singular process could die unexpectedly. In that case, we die too:
+            .expect("IO error: failure when sending data to Singular process across pipe");
+    }
+    // Exit when the other side closes the channel
 }
 
 impl SingularProcess {
