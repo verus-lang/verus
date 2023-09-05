verus-lang/verus

commit e92f1af8df2a3632d0e29f34e352824214aa34eb

Removing settings.json + replacing with template, fixing .exe issue in mod.rs

source/rust_verify_test/tests/common/mod.rs
@@ -319,8 +319,12 @@ pub fn run_verus(
     }
 
     let mut child = std::process::Command::new(bin);
-    #[cfg(not(target_os = "windows"))]
-    let child = child.env("VERUS_Z3_PATH", "../z3");
+    if let Err(_) = std::env::var("VERUS_Z3_PATH") {
+        #[cfg(not(target_os = "windows"))]
+        child.env("VERUS_Z3_PATH", "../z3");
+        #[cfg(target_os = "windows")]
+        child.env("VERUS_Z3_PATH", "../z3.exe");
+    }
     let child = child
         .args(&verus_args[..])
         .stdout(std::process::Stdio::piped())

.gitignore
@@ -2,3 +2,4 @@
 *.swp
 *.exe
 *.pdb
+.vscode/settings.json

.vscode/settings.json.template
@@ -1,3 +1,4 @@
+// Copy to .vscode/settings.json and adjust as necessary
 {
     "rust-analyzer.cargo.buildScripts.enable": true,
     "rust-analyzer.cargo.extraEnv": {
@@ -7,7 +8,8 @@
     "rust-analyzer.runnableEnv": {
         "RUSTC_BOOTSTRAP": "1"
     },
-    "rust-analyzer.runnables.command": "../tools/vargo/target/release/vargo",
+    // Use .exe extension for Windows, otherwise remove
+    "rust-analyzer.runnables.command": "../tools/vargo/target/release/vargo[.exe]",
     "rust-analyzer.procMacro.enable": true,
     "rust-analyzer.rustc.source": "discover",
     "rust-analyzer.workspace.symbol.search.scope": "workspace_and_dependencies",

commit 47532c994cdd1bba83308721437062ef3473ae93

fix z3 search path issue introduced by e92f1af8

source/rust_verify_test/tests/common/mod.rs
@@ -319,12 +319,22 @@ pub fn run_verus(
     }
 
     let mut child = std::process::Command::new(bin);
-    if let Err(_) = std::env::var("VERUS_Z3_PATH") {
-        #[cfg(not(target_os = "windows"))]
-        child.env("VERUS_Z3_PATH", "../z3");
-        #[cfg(target_os = "windows")]
-        child.env("VERUS_Z3_PATH", "../z3.exe");
-    }
+    child.env(
+        "VERUS_Z3_PATH",
+        std::env::var("VERUS_Z3_PATH")
+            .map(|p| {
+                let p = std::path::PathBuf::from(p);
+                (if p.is_relative() { std::path::PathBuf::from("..").join(p) } else { p })
+                    .into_os_string()
+            })
+            .unwrap_or({
+                if cfg!(target_os = "windows") {
+                    std::ffi::OsString::from("../z3")
+                } else {
+                    std::ffi::OsString::from("..\\z3.exe")
+                }
+            }),
+    );
     let child = child
         .args(&verus_args[..])
         .stdout(std::process::Stdio::piped())