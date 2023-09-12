4c215ef19b6d7a84f0bb03198c70045f9f661b46
Zhaofeng Li <hello@zhaofeng.li>
2023-09-12 10:40:21 -0600

    vargo: Allow disabling std and alloc in vstd

--- tools/vargo/src/main.rs
+++ tools/vargo/src/main.rs
@@ -173,6 +173,18 @@ fn run() -> Result<(), String> {
         .map(|p| args.remove(p))
         .is_some();
 
+    let vstd_no_std = args
+        .iter()
+        .position(|x| x.as_str() == "--vstd-no-std")
+        .map(|p| args.remove(p))
+        .is_some();
+
+    let vstd_no_alloc = args
+        .iter()
+        .position(|x| x.as_str() == "--vstd-no-alloc")
+        .map(|p| args.remove(p))
+        .is_some();
+
     let mut args_bucket = args.clone();
     let in_nextest = std::env::var("VARGO_IN_NEXTEST").is_ok();
 
@@ -814,6 +826,12 @@ fn run() -> Result<(), String> {
                         }
                         if vstd_no_verify {
                             vstd_build = vstd_build.arg("--no-verify");
+                        }
+                        if vstd_no_std {
+                            vstd_build = vstd_build.arg("--no-std");
+                        }
+                        if vstd_no_alloc {
+                            vstd_build = vstd_build.arg("--no-alloc");
                         }
                         if verbose {
                             vstd_build = vstd_build.arg("--verbose");
