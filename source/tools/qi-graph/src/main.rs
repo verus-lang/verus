0c40a7d2467e688ddf52f9bc2b76dec169628428
Andrea Lattuada <lattuada@vmware.com>
2023-09-22 18:23:45 +0200

    group by instantiation

--- source/tools/qi-graph/src/main.rs
+++ source/tools/qi-graph/src/main.rs
@@ -1,7 +1,11 @@
-use std::{collections::{HashMap, HashSet}, path::Path, process::exit};
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+    process::exit,
+};
 
 use getopts::Options;
-use qi_graph::{Graph, InstantiationGraph, Quantifier, QuantifierKind};
+use qi_graph::{Graph, Instantiation, InstantiationGraph, QuantifierKind};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -47,37 +51,37 @@ fn main() {
 /// for those that don't collapse, remove the node, and stitch all parents to point at its children
 /// recursively
 fn prune_by_predicate(
-    simple_graph: &HashMap<Quantifier, HashMap<Quantifier, u64>>,
-    predicate: &dyn Fn(&Quantifier) -> bool,
-) -> HashMap<Quantifier, HashMap<Quantifier, u64>> {
-     // remove the nodes in the graph relating to internal nodes
-    let mut pruned_graph: HashMap<Quantifier, HashMap<Quantifier, u64>> = HashMap::new();
-    for (src, dsts) in simple_graph {
+    input_graph: &HashMap<Instantiation, HashMap<Instantiation, u64>>,
+    predicate: &dyn Fn(&Instantiation) -> bool,
+) -> HashMap<Instantiation, HashMap<Instantiation, u64>> {
+    // remove the nodes in the graph relating to internal nodes
+    let mut pruned_graph: HashMap<Instantiation, HashMap<Instantiation, u64>> = HashMap::new();
+    for (src, dsts) in input_graph {
         // only add back in the nodes satisfying predicate as sources
         if predicate(src) {
             // function to traverse down the non-internal dsts of a source
             fn compute_final_edges(
-                visited : &mut HashSet<Quantifier>,
-                graph : &HashMap<Quantifier, HashMap<Quantifier, u64>>, 
-                predicate: &dyn Fn(&Quantifier) -> bool,
-                dsts : &HashMap<Quantifier, u64>) -> HashMap<Quantifier, u64> {
-                let mut pruned_edges : HashMap<Quantifier, u64> = HashMap::new();
+                visited: &mut HashSet<Instantiation>,
+                graph: &HashMap<Instantiation, HashMap<Instantiation, u64>>,
+                predicate: &dyn Fn(&Instantiation) -> bool,
+                dsts: &HashMap<Instantiation, u64>,
+            ) -> HashMap<Instantiation, u64> {
+                let mut pruned_edges: HashMap<Instantiation, u64> = HashMap::new();
                 // Ex: #1 -> {#2, #3, #4} , #4 internal
                 for (dst, count) in dsts {
                     if predicate(dst) {
                         // #2 and #3 directly get added to pruned
-                        *pruned_edges.entry(dst.clone()).or_insert(0) += count;                    
+                        *pruned_edges.entry(dst.clone()).or_insert(0) += count;
                     } else {
                         // don't repeatedly add the same sources in case of cycle
                         if !visited.contains(dst) {
                             visited.insert(dst.clone());
                             // #4 -> {#5, #6}
-                            let res = 
-                                if let Some(new_dsts) = graph.get(dst) {
-                                    compute_final_edges(visited, graph, predicate, new_dsts)
-                                } else {
-                                    HashMap::new()
-                                };
+                            let res = if let Some(new_dsts) = graph.get(dst) {
+                                compute_final_edges(visited, graph, predicate, new_dsts)
+                            } else {
+                                HashMap::new()
+                            };
                             // final result: #1 -> {#2, #3, #5, #6}
                             for (new_dst, new_count) in res {
                                 assert!(predicate(&new_dst));
@@ -88,12 +92,12 @@ fn prune_by_predicate(
                 }
                 pruned_edges
             }
-            let res = compute_final_edges(&mut HashSet::new(), simple_graph, predicate, &dsts) ;
+            let res = compute_final_edges(&mut HashSet::new(), input_graph, predicate, &dsts);
             // clean the hashmaps of the destinations
             pruned_graph.insert(src.clone(), res);
         }
     }
-    pruned_graph   
+    pruned_graph
 }
 
 fn run(input_path: &str) -> Result<(), String> {
@@ -103,17 +107,28 @@ fn run(input_path: &str) -> Result<(), String> {
         bincode::deserialize(&bytes[..]).map_err(|_| format!("input {input_path} is malformed"))?;
     dbg!(graph.graph.0.len());
 
-    let mut simple_graph: HashMap<Quantifier, HashMap<Quantifier, u64>> = HashMap::new();
-    for (src, dsts) in graph.graph.0 {
-        let new_src = simple_graph.entry(src.quantifier.clone()).or_insert(HashMap::new());
-        for (_edge, dst) in dsts {
-            *new_src.entry(dst.quantifier.clone()).or_insert(0) += 1;
-        }
-    }
+    // let mut simple_graph: HashMap<Quantifier, HashMap<Quantifier, u64>> = HashMap::new();
+    // for (src, dsts) in graph.graph.0 {
+    //     let new_src = simple_graph.entry(src.quantifier.clone()).or_insert(HashMap::new());
+    //     for (_edge, dst) in dsts {
+    //         *new_src.entry(dst.quantifier.clone()).or_insert(0) += 1;
+    //     }
+    // }
 
-    let pruned_graph = prune_by_predicate(&simple_graph, &|src : &Quantifier| {src.kind != QuantifierKind::Internal});
+    let input_graph = graph
+        .graph
+        .0
+        .into_iter()
+        .map(|(src, dsts)| {
+            (src, dsts.into_iter().map(|((), inst)| (inst, 1)).collect::<HashMap<_, _>>())
+        })
+        .collect();
 
-    let simple_graph: Graph<Quantifier, u64> = Graph(
+    let pruned_graph = prune_by_predicate(&input_graph, &|src: &Instantiation| {
+        src.quantifier.kind != QuantifierKind::Internal
+    });
+
+    let simple_graph: Graph<Instantiation, u64> = Graph(
         pruned_graph
             .into_iter()
             .map(|(src, edges)| {
@@ -123,7 +138,7 @@ fn run(input_path: &str) -> Result<(), String> {
     );
     simple_graph.to_dot_file(
         &Path::new(input_path).with_extension("dot"),
-        |n| n.qid.clone(),
+        |n| format!("{} ({}, {})", n.quantifier.qid, n.id.0, n.id.1),
         |e| Some(format!("{}", e)),
     )?;
 
