ba838011c7de24bc561a0b5e729e5b6000417f95
Andrea Lattuada <lattuada@vmware.com>
2023-10-27 14:36:00 +0100

    wip scaffolding

--- source/vir/src/alternation_check.rs
+++ source/vir/src/alternation_check.rs
@@ -1,10 +1,11 @@
 use air::scope_map::ScopeMap;
+use ast_visitor::VisitorControlFlow;
 
 use crate::{
     ast::{FunctionX, Krate, Mode, VirErr},
     ast_visitor::{self, expr_visitor_dfs},
     context::Ctx,
-    scc::Graph,
+    scc::Graph, messages::error,
 };
 
 pub fn alternation_check(ctx: &Ctx, krate: &Krate) -> Result<(), VirErr> {
@@ -13,14 +14,48 @@ pub fn alternation_check(ctx: &Ctx, krate: &Krate) -> Result<(), VirErr> {
         let FunctionX { require, ensure, decrease, body, broadcast_forall, attrs, .. } = &f.x;
         let Some(body) = body else { return Ok(()) };
         for expr in ensure.iter() {
-            expr_visitor_dfs(expr, &mut ScopeMap::new(), &mut |scope_map, expr| match expr.x {
-                crate::ast::ExprX::Call(_, _) => todo!(),
-                crate::ast::ExprX::Unary(_, _) => todo!(),
-                crate::ast::ExprX::Binary(_, _, _) => todo!(),
-                crate::ast::ExprX::BinaryOpr(_, _, _) => todo!(),
-                crate::ast::ExprX::Quant(_, _, _) => todo!(),
-                crate::ast::ExprX::Closure(_, _) => todo!(),
-                crate::ast::ExprX::Choose { params, cond, body } => todo!(),
+            use crate::ast::ExprX::*;
+            expr_visitor_dfs::<VirErr, _>(expr, &mut ScopeMap::new(), &mut |scope_map, expr| match &expr.x {
+                Call(_, _) => todo!(),
+                Unary(_, _) => todo!(),
+                UnaryOpr(_, _) => todo!(),
+                Binary(_, _, _) => todo!(),
+                BinaryOpr(_, _, _) => todo!(),
+                Quant(_, _, _) => todo!(),
+                Closure(_, _) => todo!(),
+                Choose { params, cond, body } => todo!(),
+
+                Loc(..) |
+                Tuple(..) |
+                Ctor(..) |
+                Multi(..) |
+                WithTriggers { .. } |
+                Assign { .. } |
+                AssertAssume { .. } |
+                If(..) |
+                Match(..) |
+                Loop { .. } |
+                Return(..) |
+                Ghost { .. } |
+                Block(..) => VisitorControlFlow::Recurse,
+
+                Const(_) |
+                Var(_) |
+                VarLoc(_) |
+                VarAt(_, _) |
+                ConstVar(_, _) |
+                StaticVar(_) |
+                Fuel(_, _) |
+                Header(_) |
+                BreakOrContinue { .. } => VisitorControlFlow::Return,
+
+                NullaryOpr(..) |
+                ExecClosure { .. } |
+                AssertBy { .. } |
+                AssertQuery { .. } |
+                AssertCompute(..) |
+                RevealString(..) |
+                OpenInvariant(..) => VisitorControlFlow::Stop(error(&expr.span, "unsupported in EPR fragment")),
             });
         }
     }
