ccc1cf83b68828654b0284ebfac71c6e711ce655
GenericMonkey <pransrin@umich.edu>
2023-11-03 12:29:08 -0400

    handle assert/assume, add tests

--- source/rust_verify_test/tests/epr.rs
+++ source/rust_verify_test/tests/epr.rs
@@ -67,7 +67,7 @@ test_verify_one_file! {
 
             // in EPR, as we negate our ensures clauses
             // so this should pass
-            proof fn lemma1()
+            proof fn test()
                 ensures
                     forall|y : S2| ((s2_s3(y) == S3Val) || (exists|x: S1| s1_s2(x) == y)),
             {
@@ -84,7 +84,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should also pass if the argument is passed in
-            proof fn lemma1_mod(y : S2)
+            proof fn test(y : S2)
                 ensures
                     ((s2_s3(y) == S3Val) || (exists|x: S1| s1_s2(x) == y))
             {
@@ -102,7 +102,7 @@ test_verify_one_file! {
 
             // in EPR, as we negate our ensures clauses
             // so this should pass
-            proof fn lemma1()
+            proof fn lemma()
                 ensures
                     forall|y : S2| ((s2_s3(y) == S3Val) || (exists|x: S1| s1_s2(x) == y)),
             {
@@ -111,11 +111,11 @@ test_verify_one_file! {
 
             // not in EPR when lemma1 is called
             // should fail
-            proof fn lemma1_call()
+            proof fn test()
                 ensures
                     forall|x : S1| s1_s2(x) == S2Val,
             {
-                lemma1();
+                lemma();
                 assume(false);
             }
         }
@@ -129,7 +129,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should also pass if the argument is passed in
-            proof fn lemma1_mod(y : S2)
+            proof fn lemma(y : S2)
                 ensures
                     ((s2_s3(y) == S3Val) || (exists|x: S1| s1_s2(x) == y))
             {
@@ -138,11 +138,11 @@ test_verify_one_file! {
 
             // should be in EPR when lemma1_mod is called (arg is explicit)
             // should pass
-            proof fn lemma1_mod_call()
+            proof fn test()
                 ensures
                     forall|x : S1| s1_s2(x) == S2Val,
             {
-                lemma1_mod(S2Val);
+                lemma(S2Val);
                 assume(false);
             }
         }
@@ -160,7 +160,7 @@ test_verify_one_file! {
                 exists|x : S1| s1_s2(x) == y
             }
 
-            proof fn lemma2()
+            proof fn test()
                 ensures exists|y : S2| positive(y),
             {
                 assume(false);
@@ -181,7 +181,7 @@ test_verify_one_file! {
             }
 
             // should fail
-            proof fn lemma3()
+            proof fn test()
                 ensures exists|y : S2| negative(y),
             {
                 assume(false);
@@ -197,7 +197,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should fail
-            proof fn lemma4()
+            proof fn test()
                 ensures
                     exists|y : S2| #![trigger s2_s3(y)] forall|x : S1| s1_s2(x) == y || s2_s3(y) == S3Val,
             {
@@ -211,7 +211,7 @@ test_verify_one_file! {
     #[test] lemma_call_ensures_pass TYPES_TEST_2.to_string() + verus_code_str! {
         mod Lemma4Mod {
             use crate::TypesTest2::*;
-            pub proof fn lemma4()
+            pub proof fn lemma()
                 ensures
                     exists|y : S2| #![trigger s2_s3(y)] forall|x : S1| s1_s2(x) == y || s2_s3(y) == S3Val,
             {
@@ -221,14 +221,14 @@ test_verify_one_file! {
         #[verifier::epr_check]
         mod EPRProofTest2 {
             use crate::TypesTest2::*;
-            use crate::Lemma4Mod::lemma4;
+            use crate::Lemma4Mod::lemma;
 
-            proof fn lemma4_call()
+            proof fn test()
                 ensures exists|x : S1| s1_s2(x) == S2Val,
             {
-                // invoking lemma4, which isn't in EPR, is fine as it
+                // invoking lemma, which isn't in EPR, is fine as it
                 // is used in opposite polarity
-                lemma4();
+                lemma();
                 assume(false);
             }
         }
@@ -242,7 +242,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // this should pass, as the forall is double negated
-            proof fn lemma5()
+            proof fn test()
                 ensures
                     exists|y : S2| (forall|x : S1| s1_s2(x) == y || s2_s3(y) == S3Val) ==> s2_s3(y) == S3Val,
             {
@@ -259,7 +259,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // this should fail due to second forall from the second part of implication
-            proof fn lemma6()
+            proof fn test()
                 ensures
                     exists|y : S2| #![trigger s2_s3(y)] (forall|x : S1| s1_s2(x) == y || s2_s3(y) == S3Val) ==> (forall|z : S1| #![trigger s1_s2(z)] s1_s2(z) == y),
             {
@@ -276,7 +276,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // this should pass, as negation flips polarity
-            proof fn lemma7()
+            proof fn test()
                 ensures
                     exists|y : S2| #![trigger s2_s3(y)] !(forall|x : S1| s1_s2(x) == y || s2_s3(y) == S3Val),
             {
@@ -293,7 +293,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should fail for function cycle
-            proof fn lemma8()
+            proof fn test()
                 ensures
                     forall|x : S1, y : S2, z : S3| s1_s2(x) == y || s2_s3(y) == z || s3_s1(z) == x
             {
@@ -310,7 +310,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should fail
-            proof fn lemma10()
+            proof fn test()
                 requires
                     forall|y : S2| (exists|x : S1| s1_s2(x) == y) || s2_s3(y) == S3Val
                 ensures
@@ -329,7 +329,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should pass
-            proof fn lemma11(y : S2)
+            proof fn test(y : S2)
                 ensures
                     forall|x : S1| s1_s2(x) == y
             {
@@ -346,7 +346,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should fail (testing duplicate openings/closings)
-            proof fn lemma12()
+            proof fn test()
                 requires
                     forall|x : S1| s1_s2(x) == S2Val || (forall|y : S1| s1_s2(y) == s1_s2(x)) || exists|y : S2| s2_s1(y) == x
                 ensures
@@ -365,7 +365,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should fail
-            proof fn lemma13()
+            proof fn test()
                 requires
                     forall|x : S2| (forall|y : S1| x == S2Val || s1_pred(y)) == ((s1_s2(S1Val) == x) || s2_pred(x))
                 ensures
@@ -385,7 +385,7 @@ test_verify_one_file! {
             use crate::TypesTest2::*;
 
             // should fail
-            proof fn lemma14()
+            proof fn test()
                 requires
                     forall|x : S2| (forall|y : S1| x == S2Val || s1_pred(y)) <==> ((s1_s2(S1Val) == x) || s2_pred(x))
                 ensures
@@ -409,11 +409,88 @@ test_verify_one_file! {
             }
 
             // should fail (self loop S1 -> S1)
-            proof fn lemma15()
+            proof fn test()
                 ensures
                     forall| x : S1 | self_loop(x) == x
             {
                 assume(false);
+            }
+        }
+    } => Err(err) => assert_vir_error_msg(err, "Function not in EPR, quantifier alternation graph contains cycle")
+}
+
+
+test_verify_one_file! {
+    #[test] assume_positive_only_fail TYPES_TEST_2.to_string() + verus_code_str! {
+        #[verifier::epr_check]
+        mod EPRProofTest2 {
+            use crate::TypesTest2::*;
+
+
+            // should fail (forall exists in assume)
+            proof fn test()
+                ensures
+                    s1_s2(S1Val) == S2Val
+            {
+                assume(forall|x : S2| s2_pred(x) && exists| y : S1| s1_s2(y) == x);
+                assume(false);
+            }
+        }
+    } => Err(err) => assert_vir_error_msg(err, "Function not in EPR, quantifier alternation graph contains cycle")
+}
+
+test_verify_one_file! {
+    #[test] assume_positive_only_pass TYPES_TEST_2.to_string() + verus_code_str! {
+        #[verifier::epr_check]
+        mod EPRProofTest2 {
+            use crate::TypesTest2::*;
+
+
+            // should pass (exists forall in assume)
+            proof fn test()
+                ensures
+                    s1_s2(S1Val) == S2Val
+            {
+                assume(exists|x : S2| s2_pred(x) && forall| y : S1| s1_s2(y) == x);
+                assume(false);
+            }
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] assert_positive_fail TYPES_TEST_2.to_string() + verus_code_str! {
+        #[verifier::epr_check]
+        mod EPRProofTest2 {
+            use crate::TypesTest2::*;
+
+
+            // should fail (forall exists in assert)
+            proof fn test()
+                ensures
+                    s1_s2(S1Val) == S2Val
+            {
+                assume(false);
+                assert(forall|x : S2| s2_pred(x) && exists| y : S1| s1_s2(y) == x);
+            }
+        }
+    } => Err(err) => assert_vir_error_msg(err, "Function not in EPR, quantifier alternation graph contains cycle")
+}
+
+test_verify_one_file! {
+    #[test] assert_negative_fail TYPES_TEST_2.to_string() + verus_code_str! {
+        #[verifier::epr_check]
+        mod EPRProofTest2 {
+            use crate::TypesTest2::*;
+
+
+            // should fail (exists forall in assert (negative))
+            proof fn test()
+                ensures
+                    s1_s2(S1Val) == S2Val
+            {
+                assume(false);
+                assert(exists|x : S2| s2_pred(x) && forall| y : S1| s1_s2(y) == x);
             }
         }
     } => Err(err) => assert_vir_error_msg(err, "Function not in EPR, quantifier alternation graph contains cycle")
