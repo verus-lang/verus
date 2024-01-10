679051eb2745571cc06c15de63f9e146c836071f
Chris Hawblitzel <Chris.Hawblitzel@microsoft.com>
2024-01-09 16:57:44 -0800

    Limit axiom_spec_range_next to specific types

--- source/pervasive/std_specs/range.rs
+++ source/pervasive/std_specs/range.rs
@@ -17,22 +17,6 @@ pub trait StepSpec where Self: Sized {
 
 pub spec fn spec_range_next<A>(a: Range<A>) -> (Range<A>, Option<A>);
 
-#[verifier::external_body]
-#[verifier::broadcast_forall]
-pub proof fn axiom_spec_range_next<A: StepSpec>(range: Range<A>)
-    ensures
-        range.start.spec_is_lt(range.end) ==>
-            // TODO (not important): use new "matches ==>" syntax here
-            (if let Some(n) = range.start.spec_forward_checked(1) {
-                spec_range_next(range) == (Range { start: n, ..range }, Some(range.start))
-            } else {
-                true
-            }),
-        !range.start.spec_is_lt(range.end) ==>
-            #[trigger] spec_range_next(range) == (range, None::<A>),
-{
-}
-
 #[verifier::external_fn_specification]
 pub fn ex_range_next<A: core::iter::Step>(range: &mut Range<A>) -> (r: Option<A>)
     ensures (*range, r) == spec_range_next(*old(range))
@@ -43,7 +27,7 @@ pub fn ex_range_next<A: core::iter::Step>(range: &mut Range<A>) -> (r: Option<A>
 } // verus!
 
 macro_rules! step_specs {
-    ($t: ty) => {
+    ($t: ty, $axiom: ident) => {
         verus! {
         impl StepSpec for $t {
             open spec fn spec_is_lt(self, other: Self) -> bool {
@@ -71,20 +55,37 @@ macro_rules! step_specs {
                     None
                 }
             }
-        } // verus!
         }
+        // TODO: we might be able to make this generic over A: StepSpec
+        // once we settle on a way to connect std traits like Step with spec traits like StepSpec.
+        #[verifier::external_body]
+        #[verifier::broadcast_forall]
+        pub proof fn $axiom(range: Range<$t>)
+            ensures
+                range.start.spec_is_lt(range.end) ==>
+                    // TODO (not important): use new "matches ==>" syntax here
+                    (if let Some(n) = range.start.spec_forward_checked(1) {
+                        spec_range_next(range) == (Range { start: n, ..range }, Some(range.start))
+                    } else {
+                        true
+                    }),
+                !range.start.spec_is_lt(range.end) ==>
+                    #[trigger] spec_range_next(range) == (range, None::<$t>),
+        {
+        }
+        } // verus!
     }
 }
 
-step_specs!(u8);
-step_specs!(u16);
-step_specs!(u32);
-step_specs!(u64);
-step_specs!(u128);
-step_specs!(usize);
-step_specs!(i8);
-step_specs!(i16);
-step_specs!(i32);
-step_specs!(i64);
-step_specs!(i128);
-step_specs!(isize);
+step_specs!(u8, axiom_spec_range_next_u8);
+step_specs!(u16, axiom_spec_range_next_u16);
+step_specs!(u32, axiom_spec_range_next_u32);
+step_specs!(u64, axiom_spec_range_next_u64);
+step_specs!(u128, axiom_spec_range_next_u128);
+step_specs!(usize, axiom_spec_range_next_usize);
+step_specs!(i8, axiom_spec_range_next_i8);
+step_specs!(i16, axiom_spec_range_next_i16);
+step_specs!(i32, axiom_spec_range_next_i32);
+step_specs!(i64, axiom_spec_range_next_i64);
+step_specs!(i128, axiom_spec_range_next_i128);
+step_specs!(isize, axiom_spec_range_next_isize);
