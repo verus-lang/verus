f1040e17a5a5e5ab819083fb673e7e8abd37c00a
Jay Lorch <jaylorch@gmail.com>
2024-02-14 15:01:20 -0800

    Add external specs for Result::{ok,err} (#985)

--- source/rust_verify/example/std_test/result.rs
+++ source/rust_verify/example/std_test/result.rs
@@ -22,4 +22,30 @@ fn map_test() {
     runtime_assert(r4.unwrap_err() == 9);
 }
     
+fn ok_test() {
+    let r1: Result<i32, i32> = Ok(7);
+    runtime_assert(r1.ok().is_some());
+    runtime_assert(r1.ok().unwrap() == 7);
+    let r2: Result<i32, i32> = Err(7);
+    runtime_assert(r2.ok().is_none());
+    let r3: Result<i32, bool> = Err(true);
+    runtime_assert(r3.ok().is_none());
+    let r4: Result<bool, i32> = Ok(false);
+    runtime_assert(r4.ok().is_some());
+    runtime_assert(r4.ok().unwrap() == false);
+}
+    
+fn err_test() {
+    let r1: Result<i32, i32> = Ok(7);
+    runtime_assert(r1.err().is_none());
+    let r2: Result<i32, i32> = Err(7);
+    runtime_assert(r2.err().is_some());
+    runtime_assert(r2.err().unwrap() == 7);
+    let r3: Result<i32, bool> = Err(true);
+    runtime_assert(r3.err().is_some());
+    runtime_assert(r3.err().unwrap() == true);
+    let r4: Result<bool, i32> = Ok(true);
+    runtime_assert(r4.err().is_none());
+}
+    
 } // verus!
