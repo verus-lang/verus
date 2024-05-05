55b24eea93ad3dc907a4219e8b58a2f5f3f43afb
Travis Hance <tjhance7@gmail.com>
2024-05-05 13:41:15 -0400

    fix variance for AtomicInvariant/LocalInvariant and add tests

--- source/rust_verify_test/tests/marker_traits.rs
+++ source/rust_verify_test/tests/marker_traits.rs
@@ -13,6 +13,11 @@ const COMMON: &str = code_str! {
         impl<A, B> vstd::invariant::InvariantPredicate<A, B> for Pred<A, B> {
             open spec fn inv(k: A, v: B) -> bool { true }
         }
+
+        struct Pred2;
+        impl<'a> vstd::invariant::InvariantPredicate<(), &'a u8> for Pred2 {
+            open spec fn inv(k: (), v: &'a u8) -> bool { true }
+        }
     }
 };
 
@@ -106,6 +111,70 @@ macro_rules! check_none {
     };
 }
 
+#[macro_export]
+macro_rules! check_covariant {
+    ($name:ident, $name2: ident, $tparams:expr, $t:expr) => {
+        test_verify_one_file! {
+            #[test] $name COMMON.to_string() + &"
+            struct J { }
+            impl<'a> J {
+                fn check_covariance$tparams(x: $WITH_STATIC) -> $WITH_A {
+                    x   
+                }
+            }
+            ".replace("$tparams", $tparams)
+            .replace("$WITH_STATIC", &($t).replace("$P", "&'static u8"))
+            .replace("$WITH_A", &($t).replace("$P", "&'a u8"))
+            => Ok(())
+        }
+        test_verify_one_file! {
+            #[test] $name2 COMMON.to_string() + &"
+            struct J { }
+            impl<'a> J {
+                fn check_contravariance$tparams(x: $WITH_A) -> $WITH_STATIC {
+                    x   
+                }
+            }
+            ".replace("$tparams", $tparams)
+            .replace("$WITH_STATIC", &($t).replace("$P", "&'static u8"))
+            .replace("$WITH_A", &($t).replace("$P", "&'a u8"))
+            => Err(err) => assert_vir_error_msg(err, "lifetime may not live long enough")
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! check_invariant {
+    ($name:ident, $name2: ident, $tparams:expr, $t:expr) => {
+        test_verify_one_file! {
+            #[test] $name COMMON.to_string() + &"
+            struct J { }
+            impl<'a> J {
+                fn check_covariance$tparams(x: $WITH_STATIC) -> $WITH_A {
+                    x   
+                }
+            }
+            ".replace("$tparams", $tparams)
+            .replace("$WITH_STATIC", &($t).replace("$P", "&'static u8"))
+            .replace("$WITH_A", &($t).replace("$P", "&'a u8"))
+            => Err(err) => assert_vir_error_msg(err, "lifetime may not live long enough")
+        }
+        test_verify_one_file! {
+            #[test] $name2 COMMON.to_string() + &"
+            struct J { }
+            impl<'a> J {
+                fn check_contravariance$tparams(x: $WITH_A) -> $WITH_STATIC {
+                    x   
+                }
+            }
+            ".replace("$tparams", $tparams)
+            .replace("$WITH_STATIC", &($t).replace("$P", "&'static u8"))
+            .replace("$WITH_A", &($t).replace("$P", "&'a u8"))
+            => Err(err) => assert_vir_error_msg(err, "lifetime may not live long enough")
+        }
+    }
+}
+
 // raw ptrs
 
 check_send_sync!(raw_ptr_points_to_send_sync, "<T: Send + Sync>", "vstd::raw_ptr::PointsTo<T>");
@@ -123,12 +192,21 @@ check_sync!(
 );
 check_none!(raw_ptr_points_none, raw_ptr_points_none2, "<T>", "vstd::raw_ptr::PointsTo<T>");
 
+check_covariant!(
+    raw_ptr_points_to_covariant,
+    raw_ptr_points_to_covariant2,
+    "",
+    "vstd::raw_ptr::PointsTo<$P>"
+);
+
 // PPtr
 
 check_send_sync!(ptr_points_to_send_sync, "<T: Send + Sync>", "vstd::ptr::PointsTo<T>");
 check_send!(ptr_points_to_send, ptr_points_to_send2, "<T: Send>", "vstd::ptr::PointsTo<T>");
 check_sync!(ptr_points_to_sync, ptr_points_to_sync2, "<T: Sync>", "vstd::ptr::PointsTo<T>");
 check_none!(ptr_points_none, ptr_points_none2, "<T>", "vstd::ptr::PointsTo<T>");
+
+check_covariant!(ptr_points_to_covariant, ptr_points_to_covariant2, "", "vstd::ptr::PointsTo<$P>");
 
 check_send_sync!(points_to_raw_send_sync, "", "vstd::ptr::PointsToRaw");
 
@@ -140,6 +218,13 @@ check_sync!(cell_points_to_sync, cell_points_to_sync2, "<T: Sync>", "vstd::cell:
 check_none!(cell_points_none, cell_points_none2, "<T>", "vstd::cell::PointsTo<T>");
 
 check_send_sync!(pcell, "<T: Send + Sync>", "vstd::cell::PCell<T>");
+
+check_covariant!(
+    cell_points_to_covariant,
+    cell_ptr_points_to_covariant2,
+    "",
+    "vstd::cell::PointsTo<$P>"
+);
 
 // LocalInvariant
 
@@ -163,6 +248,13 @@ check_none!(
 );
 check_none!(local_none, local_none2, "<T>", "vstd::invariant::LocalInvariant<(), T, Pred<(), T>>");
 
+check_invariant!(
+    local_invariant,
+    local_invariant2,
+    "",
+    "vstd::invariant::LocalInvariant<(), $P, Pred2>"
+);
+
 // AtomicInvariant
 
 check_send_sync!(
@@ -182,4 +274,11 @@ check_none!(
     atomic_none2,
     "<T>",
     "vstd::invariant::AtomicInvariant<(), T, Pred<(), T>>"
+);
+
+check_invariant!(
+    atomic_invariant,
+    atomic_invariant2,
+    "",
+    "vstd::invariant::AtomicInvariant<(), $P, Pred2>"
 );
