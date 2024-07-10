aab953d515624c66232da1066586f0b9441273e1
Chris Hawblitzel (Microsoft) <Chris.Hawblitzel@microsoft.com>
2024-07-10 13:03:13 -0700

    Add trait_conflicts.rs to detect conflicting trait implementations (#1172)
    at the VIR/AST level

--- source/rust_verify/src/lifetime.rs
+++ source/rust_verify/src/lifetime.rs
@@ -173,6 +173,12 @@ pub(crate) fn check<'tcx>(queries: &'tcx rustc_interface::Queries<'tcx>) {
     queries.global_ctxt().expect("global_ctxt").enter(|tcx| {
         let hir = tcx.hir();
         let krate = hir.krate();
+        match rustc_hir_analysis::check_crate(tcx) {
+            Ok(()) => {}
+            Err(_) => {
+                return;
+            }
+        }
         for owner in &krate.owners {
             if let MaybeOwner::Owner(owner) = owner {
                 match owner.node() {
@@ -251,18 +257,23 @@ fn open_atomic_invariant_begin<'a, X, V>(_inv: &'a X) -> (InvariantBlockGuard, V
 fn open_local_invariant_begin<'a, X, V>(_inv: &'a X) -> (InvariantBlockGuard, V) { panic!(); }
 fn open_invariant_end<V>(_guard: InvariantBlockGuard, _v: V) { panic!() }
 fn index<'a, V, Idx, Output>(v: &'a V, index: Idx) -> &'a Output { panic!() }
+struct C<const N: usize, A: ?Sized>(Box<A>);
+struct Arr<A: ?Sized, const N: usize>(Box<A>);
+fn main() {}
 ";
 
 fn emit_check_tracked_lifetimes<'tcx>(
     cmd_line_args: crate::config::Args,
     tcx: TyCtxt<'tcx>,
     verus_items: std::sync::Arc<VerusItems>,
+    spans: &SpanContext,
     krate: &'tcx Crate<'tcx>,
     emit_state: &mut EmitState,
     erasure_hints: &ErasureHints,
     item_to_module_map: &crate::rust_to_vir::ItemToModuleMap,
+    vir_crate: &vir::ast::Krate,
 ) -> State {
-    let gen_state = crate::lifetime_generate::gen_check_tracked_lifetimes(
+    let mut gen_state = crate::lifetime_generate::gen_check_tracked_lifetimes(
         cmd_line_args,
         tcx,
         verus_items,
@@ -270,6 +281,8 @@ fn emit_check_tracked_lifetimes<'tcx>(
         erasure_hints,
         item_to_module_map,
     );
+    crate::trait_conflicts::gen_check_trait_impl_conflicts(spans, vir_crate, &mut gen_state);
+
     for line in PRELUDE.split('\n') {
         emit_state.writeln(line.replace("\r", ""));
     }
@@ -361,6 +374,7 @@ pub(crate) fn check_tracked_lifetimes<'tcx>(
     spans: &SpanContext,
     erasure_hints: &ErasureHints,
     item_to_module_map: &crate::rust_to_vir::ItemToModuleMap,
+    vir_crate: &vir::ast::Krate,
     lifetime_log_file: Option<File>,
 ) -> Result<Vec<Message>, VirErr> {
     let krate = tcx.hir().krate();
@@ -369,10 +383,12 @@ pub(crate) fn check_tracked_lifetimes<'tcx>(
         cmd_line_args,
         tcx,
         verus_items,
+        spans,
         krate,
         &mut emit_state,
         erasure_hints,
         item_to_module_map,
+        vir_crate,
     );
     let mut rust_code: String = String::new();
     for line in &emit_state.lines {
