fab4100899e4b9b577d12d00eec0e25512e0f391
Anouk Duyster <aduyster@mpi-sws.org>
2024-07-21 17:19:27 +0200

    add vargo-application, list of missing parts, added singular

--- flake.nix
+++ flake.nix
@@ -1,16 +1,26 @@
 # TODO
 # * [x] deal with hardcoded path
 # * [x] the dependency z3
-# * [ ] verus as a package (not doable yet, since verus calls rustup and that means internet access during the build phase, which is not possible in nix)
+# * [ ] remove duplicates
+# * [ ] verus as an app (which checks whether verus was build beforehand, otherwise instructs user to call the vargo app)
+# * [ ] think about command names
 # * [x] vargo as a package (technically doable with a cargo)
-# * [ ] pick right z3 dependency automagically (not super simple when all we have the url in the script...)
 
+# MAYBE LATER
+# * [ ] pick right z3 dependency automagically (not super simple when all we have the url in the script...)
+# * [ ] verus as a package (not doable yet, since verus calls rustup and that means internet access during the build phase, which is not possible in nix)
 
 # How to use (if you have the nix package manager installed. These days, I recommend [Lix](https://lix.systems).)
-# 1. `nix develop` This will drop you in a shell environment where all dependencies are (should be) available
-# afterwards, when running this the first time, continue the installation:
-# 2. in ./source/: `vargo build --release` (building verus)
-# 3. run `PATH=$(realpath .)/source/target-verus/release:$PATH (this adds verus to the path)
+# ===============================
+#
+# How to use the devshell (meant for developping on vargo or verus itself)
+# `nix develop` This will drop you in a shell environment where all dependencies are (should be) available
+#
+# How to call vargo:
+# `cd src && nix run .#vargo-br`
+#
+# How to call verus;
+# TODO
 
 {
   inputs = {
@@ -67,28 +77,86 @@
 
         packages = [
           pkgs.z3_4_12
+          pkgs.cvc5
+          pkgs.singular
+
           pkgs.clang
           pkgs.llvmPackages.bintools
           pkgs.rustup
-          pkgs.cvc5
           (import ./vargo.nix { inherit pkgs; })
         ];
         shellHook = ''
           export PATH=$PATH:''${CARGO_HOME}/bin
           export PATH=$PATH:''${RUSTUP_HOME}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          export PATH=$(realpath .)/source/target-verus/release:$PATH
           export VERUS_Z3_PATH=$(which z3)
           export VERUS_CVC5_PATH=$(which cvc5)
+          export VERUS_SINGULAR_PATH=$(which Singular) # what exactly does it need on the path?
         '';
       };
     });
 
     # this only works if you have rustup installed system wide
     # invoke as `nix run .#vargo`
-    apps = forAllSystems (system: {
-      vargo = {
-        type = "app";
-        program = "${self.packages.${system}.vargo}/bin/vargo";
+    apps = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      libPath = with pkgs; lib.makeLibraryPath [
+        # load external libraries that you need in your rust project here
+      ];
+      # todo put these vars inside
+      RUSTC_VERSION = overrides.toolchain.channel; # TODO das hier wird noch gar nicht genutzt so
+#         https://github.com/rust-lang/rust-bindgen#environment-variables
+      LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+
+#         Add precompiled library to rustc search path
+      RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+#           add libraries here (e.g. pkgs.libvmi)
+      ]);
+      LD_LIBRARY_PATH = libPath;
+#       Add glibc, clang, glib, and other headers to bindgen search path
+      BINDGEN_EXTRA_CLANG_ARGS =
+#         Includes normal include path
+        (builtins.map (a: ''-I"${a}/include"'') [
+#         add dev libraries here (e.g. pkgs.libvmi.dev)
+        pkgs.glibc.dev
+      ])
+#         Includes with special directory paths
+      ++ [
+        ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+        ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+          ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+      ];
+      vargoWithRustup = pkgs.writeShellApplication {
+        name = "vargo";
+        runtimeInputs = [
+          pkgs.z3_4_12
+          pkgs.cvc5
+          pkgs.singular
+
+          pkgs.clang
+          pkgs.llvmPackages.bintools
+          pkgs.rustup
+          (import ./vargo.nix { inherit pkgs; })
+        ];
+
+
+        text = ''
+          VERUS_Z3_PATH=$(which z3)
+          export VERUS_Z3_PATH
+
+
+          export PATH=$PATH:~/.cargo/bin
+          export PATH=$PATH:~/.rutup/toolchains/${RUSTC_VERSION}-x86_64-unknown-linux-gnu/bin/
+
+
+          vargo build --release
+        '';
       };
+      in {
+        vargo-br = {
+          type = "app";
+          program = "${vargoWithRustup}/bin/vargo";
+        };
     });
   };
 }
