e0c7fef6371a9f46ac0dc5d5dbe4bbf98125b0d7
Travis Hance <tjhance7@gmail.com>
2024-08-14 10:30:11 -0400

    treat PhantomData like unit

--- source/rust_verify_test/tests/std.rs
+++ source/rust_verify_test/tests/std.rs
@@ -406,3 +406,15 @@ test_verify_one_file! {
         }
     } => Err(err) => assert_one_fails(err)
 }
+
+test_verify_one_file! {
+    #[test] phantom_data_is_unit verus_code! {
+        use core::marker::PhantomData;
+        use vstd::prelude::*;
+
+        proof fn stuff(a: PhantomData<u64>, b: PhantomData<u64>) {
+            assert(a == b);
+            assert(a == PhantomData::<u64>);
+        }
+    } => Ok(())
+}
