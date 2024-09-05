6a9c17ee2f6567c546da513d6f6f8b20d9c1a546
Andrea Lattuada <andrea@lattuada.me>
2024-09-05 16:54:29 +0200

    fixes for rust-analyzer in VScode (fixes #1253)
    * one latent issue that caused a non-blocking warning that was
      misinterpreted as a failure (#1253)
    * stop rust-analyzer from trying to extract source code for tests,
      it fails and it doesn't need to do so

--- source/rust_verify_test_macros/src/rust_code.rs
+++ source/rust_verify_test_macros/src/rust_code.rs
@@ -9,18 +9,21 @@ pub(crate) fn rust_code_core(input: TokenStream) -> String {
         span = span.join(token.span()).unwrap();
     }
 
-    let src = span.source_text().unwrap();
-    let n = span.start().column() - 1;
-    let original_src = src;
-    let mut src = String::new();
-    let mut lines = original_src.lines();
-    src += lines.next().unwrap();
-    src += "\n";
-    for line in lines {
-        let (indent, line) = line.split_at(n.min(line.len()));
-        assert!(!indent.contains(|c| c != ' '), "Invalid indentation");
-        src += line;
+    if let Some(src) = span.source_text() {
+        let n = span.start().column() - 1;
+        let original_src = src;
+        let mut src = String::new();
+        let mut lines = original_src.lines();
+        src += lines.next().unwrap();
         src += "\n";
+        for line in lines {
+            let (indent, line) = line.split_at(n.min(line.len()));
+            assert!(!indent.contains(|c| c != ' '), "Invalid indentation");
+            src += line;
+            src += "\n";
+        }
+        src
+    } else {
+        "panic!(\"source_text() returned None\")".to_string()
     }
-    src
 }
