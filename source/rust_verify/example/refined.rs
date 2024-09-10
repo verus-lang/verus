a48ee7d56cebcc4118a8f95be623e5ac1fb7fb0d
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-09-09 17:17:47 -0700

    generalize refined types to ghost values (but in a tracked wrapper)

--- source/rust_verify/example/refined.rs
+++ source/rust_verify/example/refined.rs
@@ -15,7 +15,7 @@ verus! {
         }
     }
 
-    pub fn test(Tracked(rx): Tracked<Refined::<T, TPred>>) -> (res: Tracked<Refined::<T, TPred>>) {
+    pub fn test_t(Tracked(rx): Tracked<RefinedT::<T, TPred>>) -> (res: Tracked<RefinedT::<T, TPred>>) {
         proof {
             rx.validate();
         };
@@ -32,15 +32,45 @@ verus! {
                 } else {
                     T{ a: 0u8 }
                 };
-            res = Refined::alloc(newv)
+            res = RefinedT::alloc(newv)
+        };
+        Tracked(res)
+    }
+
+    pub fn test_g(Tracked(rx): Tracked<RefinedG::<T, TPred>>) -> (res: Tracked<RefinedG::<T, TPred>>) {
+        proof {
+            rx.validate();
+        };
+        assert(rx@.a < 3);
+        assert(rx@.a != 1);
+        let tracked mut res = rx;
+        proof {
+            let v = res.take();
+            let mut newv =
+                if v.a == 0 {
+                    T{ a: 2u8 }
+                } else if v.a == 1 {
+                    T{ a: 100u8 }
+                } else {
+                    T{ a: 0u8 }
+                };
+            res = RefinedG::alloc(newv)
         };
         Tracked(res)
     }
 
     pub fn main() {
         let tracked x = T{a: 0u8};
-        let tracked rx = Refined::<T, TPred>::alloc(x);
-        let Tracked(rx) = test(Tracked(rx));
+        let tracked rx = RefinedT::<T, TPred>::alloc(x);
+        let Tracked(rx) = test_t(Tracked(rx));
+        proof {
+            rx.validate();
+        };
+        assert(rx@.a < 3);
+
+        let x = T{a: 0u8};
+        let tracked rx = RefinedG::<T, TPred>::alloc(x);
+        let Tracked(rx) = test_g(Tracked(rx));
         proof {
             rx.validate();
         };
--- source/vstd/refined.rs
+++ source/vstd/refined.rs
@@ -2,12 +2,27 @@ use super::prelude::*;
 
 verus! {
 
-    // This file implements refinement types for tracked values.
-    // In particular, Refined<T, P> is a tracked T for which the
-    // P predicate holds.
+    // This file implements refinement types using tracked values.
+    // In particular, RefinedT<T, P> and RefinedG<T, P> are values
+    // of type T for which the P predicate holds, for tracked and
+    // ghost values, respectively.  RefinedG must be tracked, since
+    // otherwise the choose operator could be used to make up a
+    // value of type RefinedG<T, P> with a P that never holds.
+    //
+    // The implementations of RefinedT and RefinedG are trusted:
+    // they rely on the fact that only the Refined{T,G}::alloc()
+    // functions construct or modify values of type Refined{T,G}<>,
+    // and they require that the predicate hold of the value used.
+    // In particular, the Refined{T,G}<>::validate() methods are
+    // assumed correct using #[verifier::external_body].
 
-    pub tracked struct Refined<T, P> {
+    pub tracked struct RefinedT<T, P> {
         tracked v: T,
+        ghost m: std::marker::PhantomData<P>,
+    }
+
+    pub tracked struct RefinedG<T, P> {
+        ghost v: T,
         ghost m: std::marker::PhantomData<P>,
     }
 
@@ -15,14 +30,14 @@ verus! {
         spec fn valid(v: T) -> bool;
     }
 
-    impl<T, P> Refined<T, P> where P: RefinedPred<T> {
-        pub proof fn alloc(tracked v: T) -> (tracked r: Refined<T, P>)
+    impl<T, P> RefinedT<T, P> where P: RefinedPred<T> {
+        pub proof fn alloc(tracked v: T) -> (tracked r: RefinedT<T, P>)
             requires
                 P::valid(v)
             ensures
                 r@ == v
         {
-            Refined::<T, P>{
+            RefinedT::<T, P>{
                 v: v,
                 m: std::marker::PhantomData,
             }
@@ -39,9 +54,44 @@ verus! {
         {
         }
 
-        pub proof fn take(tracked self) -> (tracked r: T)
+        pub proof fn take(tracked self) -> (result: T)
             ensures
-                P::valid(r)
+                result == self@,
+                P::valid(result),
+        {
+            self.validate();
+            self.v
+        }
+    }
+
+    impl<T, P> RefinedG<T, P> where P: RefinedPred<T> {
+        pub proof fn alloc(v: T) -> (tracked r: RefinedG<T, P>)
+            requires
+                P::valid(v)
+            ensures
+                r@ == v
+        {
+            RefinedG::<T, P>{
+                v: v,
+                m: std::marker::PhantomData,
+            }
+        }
+
+        pub closed spec fn view(self) -> T {
+            self.v
+        }
+
+        #[verifier::external_body]
+        pub proof fn validate(tracked &self)
+            ensures
+                P::valid(self@)
+        {
+        }
+
+        pub proof fn take(tracked self) -> (result: T)
+            ensures
+                result == self@,
+                P::valid(result),
         {
             self.validate();
             self.v
