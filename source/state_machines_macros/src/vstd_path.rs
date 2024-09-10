8e49e6cdf7fe8ef1c1a8ad5a6b9875b83497c1e5
Travis Hance <tjhance7@gmail.com>
2024-09-10 13:27:52 -0400

    fix is-core somewhat

--- source/state_machines_macros/src/vstd_path.rs
+++ source/state_machines_macros/src/vstd_path.rs
@@ -4,16 +4,23 @@ use quote::{quote_spanned, ToTokens};
 use std::sync::atomic::{AtomicBool, Ordering};
 
 pub static IS_VSTD: AtomicBool = AtomicBool::new(false);
+pub static IS_CORE: AtomicBool = AtomicBool::new(false);
 
 pub fn set_is_vstd(b: bool) {
     IS_VSTD.store(b, Ordering::Relaxed);
+}
+
+pub fn set_is_core(b: bool) {
+    IS_CORE.store(b, Ordering::Relaxed);
 }
 
 pub struct Vstd(pub Span);
 
 impl ToTokens for Vstd {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        if IS_VSTD.load(Ordering::Relaxed) {
+        if IS_CORE.load(Ordering::Relaxed) {
+            tokens.extend(quote_spanned! { self.0 => crate::vstd });
+        } else if IS_VSTD.load(Ordering::Relaxed) {
             tokens.extend(quote_spanned! { self.0 => crate });
         } else {
             tokens.extend(quote_spanned! { self.0 => ::vstd });
