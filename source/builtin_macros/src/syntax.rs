6bc0dfc6992b53b28a41f258008b76b56dba664f
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2024-10-28 17:35:56 -0700

    Add allow(non_snake_case) to __VERUS_CONST_BODY__

--- source/builtin_macros/src/syntax.rs
+++ source/builtin_macros/src/syntax.rs
@@ -662,7 +662,7 @@ impl Visitor {
                 self.inside_ghost += 1;
                 self.visit_expr_mut(&mut expr);
                 self.inside_ghost -= 1;
-                stmts.push(Stmt::Expr(Expr::Verbatim(quote_spanned!(con_span => #[verus::internal(verus_macro)] #[verus::internal(const_body)] fn __VERUS_CONST_BODY__() -> #con_ty { #expr } ))));
+                stmts.push(Stmt::Expr(Expr::Verbatim(quote_spanned!(con_span => #[allow(non_snake_case)]#[verus::internal(verus_macro)] #[verus::internal(const_body)] fn __VERUS_CONST_BODY__() -> #con_ty { #expr } ))));
                 stmts.push(Stmt::Expr(Expr::Verbatim(
                     quote_spanned!(con_span => unsafe { core::mem::zeroed() }),
                 )));
