48a0ea85772d30434198fb46f97f99bc9da50180
Travis Hance <tjhance7@gmail.com>
2024-11-28 13:44:28 -0500

    map

--- source/vstd/tokens.rs
+++ source/vstd/tokens.rs
@@ -1,26 +1,33 @@
 //// ValueToken - for variable, option, persistent_option
 
-pub trait ValueToken<Instance, Value> {
-    spec fn instance() -> Instance;
-    spec fn value() -> Value;
+use vstd::prelude::*;
+use core::marker::PhantomData;
+
+verus!{
+
+ghost struct InstanceId(pub int);
+
+pub trait ValueToken<Value> {
+    spec fn instance(&self) -> InstanceId;
+    spec fn value(&self) -> Value;
 
     proof fn agree(tracked &self, tracked other: &Self)
         requires self.instance() == other.instance(),
         ensures self.value() == other.value();
 }
 
-pub trait UniqueValueToken<Instance, Value> : ValueToken<Instance, Value> {
+pub trait UniqueValueToken<Value> : ValueToken<Value> {
     proof fn unique(tracked &mut self, tracked other: &Self)
         ensures self.instance() != other.instance();
 }
 
-pub trait PersistentValueToken<Instance, Value> : ValueToken<Instance, Value> + Copy {
-}
+//pub trait PersistentValueToken<Value> : ValueToken<Value> + Copy {
+//}
 
-pub trait KeyValueToken<Instance, Key, Value> {
-    spec fn instance() -> Instance;
-    spec fn key() -> Key;
-    spec fn value() -> Value;
+pub trait KeyValueToken<Key, Value> {
+    spec fn instance(&self) -> InstanceId;
+    spec fn key(&self) -> Key;
+    spec fn value(&self) -> Value;
 
     proof fn agree(tracked &self, tracked other: &Self)
         requires self.instance() == other.instance(),
@@ -28,44 +35,118 @@ pub trait KeyValueToken<Instance, Key, Value> {
         ensures self.value() == other.value();
 }
 
-pub trait UniqueKeyValueToken<Instance, Key, Value> : KeyValueToken<Instance, Key, Value> {
+pub trait UniqueKeyValueToken<Key, Value> : KeyValueToken<Key, Value> {
     proof fn unique(tracked &mut self, tracked other: &Self)
         ensures self.instance() != other.instance()
             || self.key() != other.key();
 }
 
-pub trait PersistentKeyValueToken<Instance, Key, Value> : KeyValueToken<Instance, Key, Value> + Copy {
+//pub trait PersistentKeyValueToken<Key, Value> : KeyValueToken<Key, Value> + Copy {
+//}
+
+pub trait CountToken {
+    spec fn instance() -> InstanceId;
+    spec fn count() -> nat;
+
+    proof fn join(tracked &self, tracked other: Self);
 }
 
-tracked struct MapToken<Instance, K, V, Token> {
-    ghost inst: Instance,
-    tracked m: Map<K, Token>,
+#[verifier::reject_recursive_types(Key)]
+tracked struct MapToken<Key, Value, Token>
+    where Token: KeyValueToken<Key, Value>
+{
+    ghost _v: PhantomData<Value>,
+    ghost inst: InstanceId,
+    tracked m: Map<Key, Token>,
 }
 
-impl<Instance, Key, Value, Token> MapToken<Instance, K, V, Token>
-    where Token: KeyValueToken<Instance, Key, Value>
+impl<Key, Value, Token> MapToken<Key, Value, Token>
+    where Token: KeyValueToken<Key, Value>
 {
     #[verifier::type_invariant]
     spec fn wf(self) -> bool {
-        forall |k| self.m.dom().contains(k) ==> self.m[k].key() == k
+        forall |k| #[trigger] self.m.dom().contains(k) ==> self.m[k].key() == k
+            && self.m[k].instance() == self.inst
     }
 
-    pub closed spec fn instance(self) -> Instance {
+    pub closed spec fn instance(self) -> InstanceId {
         self.inst
     }
 
-    pub closed spec fn map(self) -> Map<K, V> {
+    pub closed spec fn map(self) -> Map<Key, Value> {
         Map::new(
             |k: Key| self.m.dom().contains(k),
             |k: Key| self.m[k].value(),
         )
     }
 
-    fn empty(instance: Instance) -> (s: Self)
+    proof fn empty(instance: InstanceId) -> (tracked s: Self)
         ensures
             s.instance() == instance,
             s.map() === Map::empty(),
     {
-        Self { inst: instance, m: Map::empty() }
+        let tracked s = Self { inst: instance, m: Map::tracked_empty(), _v: PhantomData };
+        assert(s.map() =~= Map::empty());
+        return s;
     }
+
+    proof fn insert(tracked &mut self, tracked token: Token)
+        requires
+            old(self).instance() == token.instance(),
+        ensures
+            self.instance() == old(self).instance(),
+            self.map() == old(self).map().insert(token.key(), token.value()),
+    {
+        use_type_invariant(&*self);
+        self.m.tracked_insert(token.key(), token);
+        assert(self.map() =~= old(self).map().insert(token.key(), token.value()));
+    }
+
+    proof fn remove(tracked &mut self, key: Key) -> (tracked token: Token)
+        requires
+            old(self).map().dom().contains(key)
+        ensures
+            self.instance() == old(self).instance(),
+            self.map() == old(self).map().remove(key),
+            token.instance() == self.instance(),
+            token.key() == key,
+            token.value() == old(self).map()[key]
+    {
+        use_type_invariant(&*self);
+        let tracked t = self.m.tracked_remove(key);
+        assert(self.map() =~= old(self).map().remove(key));
+        t
+    }
+
+    proof fn into_map(tracked self) -> (tracked map: Map<Key, Token>)
+        ensures
+            map.dom() == self.map().dom(),
+            forall |key| #[trigger] map.dom().contains(key)
+                ==> map[key].instance() == self.instance()
+                 && map[key].key() == key
+                 && map[key].value() == self.map()[key]
+    {
+        use_type_invariant(&self);
+        let tracked MapToken { inst, m, _v } = self;
+        assert(m.dom() =~= self.map().dom());
+        return m;
+    }
+
+    proof fn from_map(instance: InstanceId, tracked map: Map<Key, Token>) -> (s: Self)
+        requires
+            forall |key| map.dom().contains(key) ==> map[key].instance() == instance,
+            forall |key| map.dom().contains(key) ==> map[key].key() == key,
+        ensures
+            map.dom() == s.map().dom(),
+            forall |key| #[trigger] map.dom().contains(key)
+                ==> map[key].instance() == s.instance()
+                 && map[key].key() == key
+                 && map[key].value() == s.map()[key]
+    {
+        let tracked s = MapToken { inst: instance, m: map, _v: PhantomData };
+        assert(map.dom() == s.map().dom());
+        s
+    }
+}
+
 }
