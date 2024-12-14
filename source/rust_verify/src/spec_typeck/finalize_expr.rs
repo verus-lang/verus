9567ef4760495c7ed666ab8f70ebf9ca156f3fbf
Travis Hance <tjhance7@gmail.com>
2024-12-13 20:12:21 -0500

    maybe try fulfillment ctxt

--- source/rust_verify/src/spec_typeck/finalize_expr.rs
+++ source/rust_verify/src/spec_typeck/finalize_expr.rs
@@ -19,29 +19,52 @@ impl State<'_, '_> {
         })
     }
 
-    fn finalize_one_expr(&mut self, e: &Expr) -> Result<Expr, VirErr> {
-        match &e.x {
+    fn finalize_one_expr(&mut self, expr: &Expr) -> Result<Expr, VirErr> {
+        match &expr.x {
             ExprX::Call(call_target, _args) => {
                 match call_target {
                     CallTarget::Fun(_kind, fun, typs, _, _usage) => {
                         let def_id = crate::rust_to_vir_base::def_id_of_vir_path(&fun.path);
-                        self.check_trait_obligations(def_id, typs);
-                        Ok(e.clone())
+                        self.check_trait_obligations(&expr.span, def_id, typs);
+                        Ok(expr.clone())
                     }
-                    CallTarget::FnSpec(_) => Ok(e.clone()),
+                    CallTarget::FnSpec(_) => Ok(expr.clone()),
                     _ => todo!(),
                 }
             }
-            _ => Ok(e.clone())
+            _ => Ok(expr.clone())
         }
     }
 
-    fn check_trait_obligations(&mut self, def_id: DefId, typs: &Typs) {
+    fn check_trait_obligations(&mut self, span: &vir::messages::Span, def_id: DefId, typs: &Typs) {
+        let span = self.bctx.ctxt.spans.from_air_span(span, None).unwrap();
+
         use crate::rustc_trait_selection::solve::InferCtxtEvalExt;
         use crate::rustc_infer::infer::TyCtxtInferExt;
+        use crate::rustc_trait_selection::traits::TraitEngineExt;
 
         let infcx = self.tcx.infer_ctxt().ignoring_regions().build();
+        let mut fulfillment_cx = <dyn rustc_trait_selection::traits::TraitEngine<'_>>::new(&infcx);
 
+        let args = self.finalized_vir_typs_to_generic_args(typs);
+        let clauses = self.tcx.predicates_of(def_id).instantiate(self.tcx, args).predicates;
+        for clause in clauses.into_iter() {
+            let cause = rustc_trait_selection::traits::ObligationCause::new(
+                span,
+                self.bctx.fun_id.expect_local(),
+                rustc_trait_selection::traits::ObligationCauseCode::ItemObligation(def_id),
+            );
+            let obligation = rustc_trait_selection::traits::Obligation::new(
+                self.tcx,
+                cause,
+                self.tcx.param_env(self.bctx.fun_id),
+                clause);
+            fulfillment_cx.register_predicate_obligation(&infcx, obligation);
+        }
+
+        let errors = fulfillment_cx. 
+
+        /*
         let args = self.finalized_vir_typs_to_generic_args(typs);
         let clauses = self.tcx.predicates_of(def_id).instantiate(self.tcx, args).predicates;
         for clause in clauses.into_iter() {
@@ -58,5 +81,6 @@ impl State<'_, '_> {
                 Err(_e) => todo!(),
             }
         }
+        */
     }
 }
