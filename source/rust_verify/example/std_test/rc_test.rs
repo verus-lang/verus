31a1b48575c7b168657ba371004b31f17c6e7fe8
Jay Lorch <lorch@microsoft.com>
2025-01-26 19:28:14 -0800

    Specs for Rc::try_unwrap and ::into_inner

---
+++ source/rust_verify/example/std_test/rc_test.rs
@@ -0,0 +1,44 @@
+use std::rc::Rc;
+
+use vstd::pervasive::runtime_assert;
+use vstd::prelude::*;
+
+verus! {
+
+fn try_unwrap_test() {
+    let r1 = Rc::new(5);
+    let r2 = r1.clone();
+    let result1 = Rc::try_unwrap(r1);
+    assert(result1 is Err ==> result1.unwrap_err() == 5);
+    match result1 {
+        Err(r3) => { assert(r3 == 5); },
+        Ok(five) => { assert(five == 5); }, // won't happen
+    };
+    let result2 = Rc::try_unwrap(r2);
+    assert(result2 is Ok ==> result2.unwrap() == 5);
+    match result2 {
+        Err(r4) => { assert(r4 == 5); }, // won't happen
+        Ok(five) => { assert(five == 5); },
+    }
+
+}
+
+fn into_inner_test() {
+    let r1 = Rc::new(5);
+    let r2 = r1.clone();
+    let result1 = Rc::into_inner(r1);
+    match result1 {
+        Some(five) => {
+            assert(five == 5); // won't happen
+        },
+        None => {
+            let result2 = Rc::into_inner(r2);
+            match result2 {
+                Some(five) => { runtime_assert(five == 5) },
+                None => {},
+            }
+        },
+    }
+}
+
+} // verus!
