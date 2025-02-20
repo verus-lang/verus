16c34dd353ea4ee45c9d1e7b01f65fb4aa18e356
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-02-19 22:53:29 -0800

    Add SpecPartialEqOp for raw_ptr
    fix

--- source/rust_verify_test/tests/raw_ptrs.rs
+++ source/rust_verify_test/tests/raw_ptrs.rs
@@ -222,8 +222,10 @@ test_verify_one_file! {
 
 test_verify_one_file! {
     #[test] pointer_exec_eq_is_not_spec_eq verus_code! {
+        use vstd::prelude::*;
         fn test_const_eq(x: *const u8, y: *const u8) {
             if x == y {
+                assert(x@.addr == y@.addr);
                 assert(x == y); // FAILS
             }
         }
@@ -233,7 +235,7 @@ test_verify_one_file! {
                 assert(x == y); // FAILS
             }
         }
-    } => Err(err) => assert_vir_error_msg(err, "The verifier does not yet support the following Rust feature: ==/!= for non smt equality types")
+    } => Err(err) => assert_fails(err, 2)
 }
 
 test_verify_one_file! {
