951f80f8480e6adf25c7b705d1e0a8b7d5273c49
Andrea Lattuada <andrea@mpi-sws.org>
2025-02-25 09:23:09 +0100

    [wip] uninterp marker on uniterpreted spec fns

--- dependencies/syn/src/lib.rs
+++ dependencies/syn/src/lib.rs
@@ -559,7 +559,7 @@ pub use crate::verus::{
     InvariantNameSetNone, ItemBroadcastGroup, MatchesOpExpr, MatchesOpToken, Mode, ModeExec,
     ModeGhost, ModeProof, ModeSpec, ModeSpecChecked, ModeTracked, Open, OpenRestricted, Prover,
     Publish, Recommends, Requires, Returns, RevealHide, SignatureDecreases, SignatureInvariants,
-    SignatureSpec, SignatureSpecAttr, SignatureUnwind, Specification, TypeFnSpec, View,
+    SignatureSpec, SignatureSpecAttr, SignatureUnwind, Specification, TypeFnSpec, Uninterp, View,
 };
 
 #[rustfmt::skip] // https://github.com/rust-lang/rustfmt/issues/6176
