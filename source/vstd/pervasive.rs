8b82cc4d5d1b0ab2c80ac990cb224f2f7d4ea01c
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-03-04 15:13:56 -0800

    Add a feature flag allow-panic in vstd to indicate whether the code may panic && Rename to __call_panic to indicate that the function should not be used directly.

--- source/vstd/pervasive.rs
+++ source/vstd/pervasive.rs
@@ -414,29 +414,48 @@ pub open spec fn cloned<T: Clone>(a: T, b: T) -> bool {
 } // verus!
 
 verus! {
-
-#[verifier(external_body)]
-pub fn call_vpanic(_out: &[&str]) -> ! {
-    unimplemented!()
+/// The default behavior of the vstd library enforces writing panic-free code.
+/// While developers may still use panic, verification should ensure that any
+/// panic is provably unreachable.
+/// cfg!(feature = "allow_panic") explicily allows code to panic.
+pub open spec fn allow_panic() -> bool {
+    cfg!(feature = "allow_panic")
 }
 
+#[doc(hidden)]
 #[verifier(external_body)]
-pub fn vpanic_fmt<T: core::fmt::Debug>(_v: &T) -> &str {
-    unimplemented!()
+pub fn __call_panic(out: &[&str]) -> !
+requires
+    allow_panic()
+{
+    core::panic!("__call_panic {:?}", out);
+}
+
+// rt::Argument is a private type and we cannot add specification directly
+#[cfg(feature = "alloc")]
+#[doc(hidden)]
+#[verifier(external_body)]
+pub fn __new_argument<T: core::fmt::Debug>(v: &T) -> alloc::string::String {
+    alloc::format!("{:?}", v)
 }
 
 } // verus!
+
+/// Replace panic macro with vpanic when needed.
+/// panic!{} may call panic_fmt with private rt::Argument, which could not
+/// be supported in verus.
 #[macro_export]
 macro_rules! vpanic {
     // Case: Format string with arguments
     ($fmt:expr $(,$val:expr)*) => {
-        vstd::pervasive::call_vpanic(
-            &[$(
-                vstd::pervasive::vpanic_fmt(&$val),
+        vstd::pervasive::__call_panic(
+            &[vstd::pervasive::__new_argument(&$fmt).as_str(),
+            $(
+                vstd::pervasive::__new_argument(&$val).as_str(),
             )*]
         );
     };
     () => {
-        vstd::pervasive::call_vpanic([]);
+        vstd::pervasive::__call_panic(&[]);
     };
 }
