cacac5e14a762cef16f82e30ddb0df4fc2fb08d9
Jay Lorch <lorch@microsoft.com>
2025-03-06 16:16:05 -0800

    Remove unnecessary ExGlobal

--- source/vstd/std_specs/vecdeque.rs
+++ source/vstd/std_specs/vecdeque.rs
@@ -18,12 +18,6 @@ verus! {
 #[verifier::reject_recursive_types(A)]
 pub struct ExVecDeque<T, A: Allocator>(VecDeque<T, A>);
 
-// this is a bit of a hack; verus treats Global specially already,
-// but putting this here helps Verus pick up all the trait impls for Global
-#[verifier::external_type_specification]
-#[verifier::external_body]
-pub struct ExGlobal(alloc::alloc::Global);
-
 impl<T, A: Allocator> View for VecDeque<T, A> {
     type V = Seq<T>;
 
