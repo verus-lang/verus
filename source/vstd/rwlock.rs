f0094d18dc00658c8f022cc53aeabbaa99e2b1c1
Travis Hance <tjhance7@gmail.com>
2025-03-19 16:03:54 +0100

    fix vstd rwlock

--- source/vstd/rwlock.rs
+++ source/vstd/rwlock.rs
@@ -10,10 +10,8 @@ use super::multiset::*;
 use super::prelude::*;
 use super::set::*;
 use core::marker::PhantomData;
-#[cfg(verus_keep_ghost)]
 use state_machines_macros::tokenized_state_machine_vstd;
 
-#[cfg(verus_keep_ghost)]
 tokenized_state_machine_vstd!(
 RwLockToks<K, V, Pred: InvariantPredicate<K, V>> {
     fields {
