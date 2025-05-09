f85ef04954df6eb6e03dc0f314ceb2c513851343
ziqiaozhou <ziqiaozhou@microsoft.com>
2025-05-09 13:34:44 -0700

    Do not wrap in proof block if stmt is macro inside proof_decl macro (#1623)

--- source/rust_verify_test/tests/syntax_attr.rs
+++ source/rust_verify_test/tests/syntax_attr.rs
@@ -670,3 +670,25 @@ test_verify_one_file! {
 
     } => Err(e) => assert_rust_error_msg(e, "method `spec_f` is not a member of trait `X`")
 }
+
+test_verify_one_file! {
+    #[test] test_macro_inside_proof_decl code!{
+        macro_rules! inline_proof {
+            ($val: expr => $x: ident, $y: ident) => {
+                proof_decl!{
+                    let tracked $x: int = $val;
+                    let ghost $y: int = $val;
+                    assert($x == $y);
+                }
+             }
+        }
+
+        fn test_inline_proof_via_macro() {
+            proof_decl!{
+                inline_proof!(0 => x, y);
+                assert(x == 0);
+            }
+        }
+
+    } => Ok(())
+}
