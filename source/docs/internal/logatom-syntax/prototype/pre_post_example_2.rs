e9409eebee70a881ef5d73391468cab2e03af179
Andrea Lattuada <andrea@mpi-sws.org>
2025-05-23 14:50:39 +0200

    fix atomic requires cfg

--- source/docs/internal/logatom-syntax/prototype/pre_post_example_2.rs
+++ source/docs/internal/logatom-syntax/prototype/pre_post_example_2.rs
@@ -4,15 +4,16 @@ use vstd::atomic::{PermissionU64, PAtomicU64};
 verus! {
 
 fn add_1(p: PAtomicU64, Tracked(perm): Tracked<PermissionU64>) -> ()
+    /// requires perm.view().patomic == p.id()
     // atomic_update: {
     //   (perm: PermissionU64) -> ()
     //   requires perm.view().patomic == p.id()
     //   ensures <expr>
     // }
 {
-    #[cfg(verus_keep_ghost)]
+    #[cfg(verus_keep_ghost_body)]
     /* will be syntax */ ::builtin::atomic_requires([
-    /* will be syntax */    perm.view().patomic == p.id()
+    /* will be syntax */    ::builtin::spec_eq(perm.view().patomic, p.id())
     /* will be syntax */ ]);
 
 }
