7133e9d50262155774e5a0d21d9043f24c23e2ae
Daniel Schoepe <schoeped@amazon.com>
2025-06-11 17:16:51 -0700

    Add `cfg(feature = "alloc")` for no_std build

--- source/vstd/std_specs/alloc.rs
+++ source/vstd/std_specs/alloc.rs
@@ -4,12 +4,14 @@ verus! {
 
 // this is a bit of a hack; verus treats Global specially already,
 // but putting this here helps Verus pick up all the trait impls for Global
+#[cfg(feature = "alloc")]
 #[verifier::external_type_specification]
 #[verifier::external_body]
 pub struct ExGlobal(alloc::alloc::Global);
 
+#[cfg(feature = "alloc")]
 #[feature(liballoc_internals)]
-pub assume_specification<T>[ alloc::boxed::box_new ](x: T) -> (result: Box<T>)
+pub assume_specification<T>[ alloc::boxed::box_new ](x: T) -> (result: alloc::boxed::Box<T>)
     ensures
         *result == x,
 ;
