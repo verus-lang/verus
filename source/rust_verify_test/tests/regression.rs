005dd4c4ed430eb4d0e8102d4516527fbaaaf0dc
Chris Hawblitzel (Microsoft) <Chris.Hawblitzel@microsoft.com>
2025-06-20 10:14:35 -0700

    Treat fresh variables separate from free variables in subst_exp (#1751)
    (fixes https://github.com/verus-lang/verus/issues/1511 )

--- source/rust_verify_test/tests/regression.rs
+++ source/rust_verify_test/tests/regression.rs
@@ -1443,3 +1443,35 @@ test_verify_one_file! {
         }
     } => Ok(())
 }
+
+test_verify_one_file! {
+    #[test] subst_in_traits verus_code! {
+        // https://github.com/verus-lang/verus/issues/1511
+        use vstd::prelude::*;
+
+        struct A {
+            a: bool,
+        }
+
+        struct B;
+
+        trait TraitA {
+            proof fn prop(chain: Seq<Option<A>>, i: int)
+                ensures
+                    0 <= i < chain.len() ==> {
+                        let c = chain[i];
+                        if c is Some {
+                            let MISSING_KEY = c->0;
+                            MISSING_KEY.a
+                        } else {
+                            false
+                        }
+                    }
+                    ;
+        }
+
+        impl TraitA for B {
+            proof fn prop(chain: Seq<Option<A>>, i: int) { assume(false); }
+        }
+    } => Ok(())
+}
