verus-lang/verus

commit 096393dda29f27c59a5622bcec45ea87e2766d5b

guide: clean up the new ownership and borrowing section

examples/guide/references.rs
@@ -8,18 +8,24 @@ use vstd::prelude::*;
 verus! {
 
 
-fn immut()
-{
 // ANCHOR: immut
-let x: u32 = 0;
-let immut_ref_x = &x;
-assert(x == 0);
-assert(*immut_ref_x == 0);
-// ANCHOR_END: immut
+fn immutable_references_example() {
+    let x: u32 = 0;
+    let y: u32 = 0;
+
+    let immut_ref_x = &x;
+    let immut_ref_y = &y;
+
+    assert(x == 0);
+    assert(*immut_ref_x == 0);
+
+    // These point to different stack variables, but they compare equal.
+    assert(immut_ref_x == immut_ref_y);
 }
+// ANCHOR_END: immut
 
 // ANCHOR: mut
-fn modify_y(a: &mut u32) 
+fn modify_y(a: &mut u32)
     ensures *a == 2
 {
     *a = 2;
@@ -35,17 +41,18 @@ fn mutable_example()
 // ANCHOR_END: mut
 
 // ANCHOR: requires
-fn check_and_inc(a: &mut u32) 
-    requires *old(a) == 0
-    ensures *a == 1
+fn increment(a: &mut u32)
+    requires *old(a) < u32::MAX,
+    ensures *a == *old(a) + 1,
 {
     *a = *a + 1;
 }
 
-fn function_requires_ensures()
+fn caller()
 {
     let mut z: u32 = 0;
     check_and_inc(&mut z);
+    assert(z == 1);
 }
 // ANCHOR_END: requires
 

commit 086c345ea67013d458400995ba25f2c055993152

fix CI breakage from 096393dda29f27c59a5622bcec45ea87e2766d5b

examples/guide/references.rs
@@ -51,7 +51,7 @@ fn increment(a: &mut u32)
 fn caller()
 {
     let mut z: u32 = 0;
-    check_and_inc(&mut z);
+    increment(&mut z);
     assert(z == 1);
 }
 // ANCHOR_END: requires