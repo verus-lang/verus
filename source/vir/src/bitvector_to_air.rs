f318fe1447487f52432bfdff22bab9c8faa2bf69
Travis Hance <tjhance7@gmail.com>
2025-07-12 00:57:01 +0200

    building blocks for general mut ref support

--- source/vir/src/bitvector_to_air.rs
+++ source/vir/src/bitvector_to_air.rs
@@ -356,6 +356,9 @@ fn bv_exp_to_expr(ctx: &Ctx, state: &mut State, exp: &Exp) -> Result<BvExpr, Vir
             UnaryOp::CastToInteger => {
                 panic!("internal error: unexpected CastToInteger")
             }
+            UnaryOp::MutRefCurrent | UnaryOp::MutRefFuture => {
+                panic!("mut-ref operation not allowed in bitvector query")
+            }
         },
         ExpX::UnaryOpr(UnaryOpr::Box(_) | UnaryOpr::Unbox(_), exp) => {
             bv_exp_to_expr(ctx, state, exp)
--- source/vir/src/sst_util.rs
+++ source/vir/src/sst_util.rs
@@ -450,6 +450,12 @@ impl ExpX {
                 UnaryOp::CastToInteger => {
                     (format!("{} as int", exp.x.to_user_string(global)), precedence)
                 }
+                UnaryOp::MutRefCurrent => {
+                    (format!("mut_ref_current({})", exp.x.to_string_prec(global, 99)), 0)
+                }
+                UnaryOp::MutRefFuture => {
+                    (format!("mut_ref_future({})", exp.x.to_string_prec(global, 99)), 0)
+                }
             },
             UnaryOpr(op, exp) => {
                 use crate::ast::UnaryOpr::*;
@@ -759,6 +765,24 @@ pub fn sst_equal(span: &Span, e1: &Exp, e2: &Exp) -> Exp {
     SpannedTyped::new(span, &Arc::new(TypX::Bool), ExpX::Binary(op, e1.clone(), e2.clone()))
 }
 
+pub fn sst_mut_ref_current(span: &Span, e1: &Exp) -> Exp {
+    let t = match &*e1.typ {
+        TypX::MutRef(t) => t,
+        _ => panic!("sst_mut_ref_current expected MutRef type"),
+    };
+    let op = UnaryOp::MutRefCurrent;
+    SpannedTyped::new(span, &t, ExpX::Unary(op, e1.clone()))
+}
+
+pub fn sst_mut_ref_future(span: &Span, e1: &Exp) -> Exp {
+    let t = match &*e1.typ {
+        TypX::MutRef(t) => t,
+        _ => panic!("sst_mut_ref_future expected MutRef type"),
+    };
+    let op = UnaryOp::MutRefFuture;
+    SpannedTyped::new(span, &t, ExpX::Unary(op, e1.clone()))
+}
+
 pub fn sst_equal_ext(span: &Span, e1: &Exp, e2: &Exp, ext: Option<bool>) -> Exp {
     match ext {
         None => sst_equal(span, e1, e2),
@@ -815,6 +839,7 @@ impl LocalDeclKind {
             LocalDeclKind::ExecClosureParam => false,
             LocalDeclKind::ExecClosureRet => false,
             LocalDeclKind::Nondeterministic => false,
+            LocalDeclKind::BorrowMut => false,
         }
     }
 }
