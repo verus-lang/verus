828c630a7f3f01a245718dc76d94f223dd143a07
Bryan Parno <parno@users.noreply.github.com>
2025-08-22 16:19:04 -0400

    Automated versioning for Verus crates (#1864)
    * A new script for coordinating new version numbers for Verus's various crates.
    * Use a unified set of version numbers consistent with our plan moving forward.
    * Update our .lock file to reflect the new version numbers.
    * Rename a vstd file that identifies the inspirations of some of the vstd code.

--- source/cargo-verus/src/main.rs
+++ source/cargo-verus/src/main.rs
@@ -102,19 +102,12 @@ version = "0.1.0"
 edition = "2021"
 
 [dependencies]
-vstd = { git = "https://github.com/verus-lang/verus", rev = "BUILDREV" }
+vstd = "=0.0.0-2025-08-12-1837"
 
 [package.metadata.verus]
 verify = true
 "#
-    .replace("NAME", name)
-    .replace(
-        "BUILDREV",
-        match option_env!("VARGO_BUILD_SHA") {
-            None => unimplemented!(),
-            Some(rev) => rev,
-        },
-    );
+    .replace("NAME", name);
 
     let project_dir = PathBuf::from(name);
     if project_dir.exists() {
