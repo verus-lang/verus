28f0dd1ac30d7e87f00a59326a044989637b30d5
Michael McLoughlin <mcloughlin@cmu.edu>
2025-09-09 09:58:36 -0400

    air: support `bvs{div,rem}` (#1886)

--- source/air/src/typecheck.rs
+++ source/air/src/typecheck.rs
@@ -345,8 +345,8 @@ fn check_expr(typing: &mut Typing, expr: &Expr) -> Result<Typ, TypeError> {
         ExprX::Binary(BinaryOp::BitXor, e1, e2) => {
             check_bv_exprs(typing, BinaryOp::BitXor, "^", &[e1.clone(), e2.clone()])
         }
-        ExprX::Binary(BinaryOp::BitUMod, e1, e2) => {
-            check_bv_exprs(typing, BinaryOp::BitUMod, "bvmod", &[e1.clone(), e2.clone()])
+        ExprX::Binary(BinaryOp::BitURem, e1, e2) => {
+            check_bv_exprs(typing, BinaryOp::BitURem, "bvurem", &[e1.clone(), e2.clone()])
         }
         ExprX::Binary(BinaryOp::BitAnd, e1, e2) => {
             check_bv_exprs(typing, BinaryOp::BitAnd, "&", &[e1.clone(), e2.clone()])
@@ -364,7 +364,13 @@ fn check_expr(typing: &mut Typing, expr: &Expr) -> Result<Typ, TypeError> {
             check_bv_exprs(typing, BinaryOp::BitMul, "bvmul", &[e1.clone(), e2.clone()])
         }
         ExprX::Binary(BinaryOp::BitUDiv, e1, e2) => {
-            check_bv_exprs(typing, BinaryOp::BitUDiv, "bvdiv", &[e1.clone(), e2.clone()])
+            check_bv_exprs(typing, BinaryOp::BitUDiv, "bvudiv", &[e1.clone(), e2.clone()])
+        }
+        ExprX::Binary(BinaryOp::BitSDiv, e1, e2) => {
+            check_bv_exprs(typing, BinaryOp::BitSDiv, "bvsdiv", &[e1.clone(), e2.clone()])
+        }
+        ExprX::Binary(BinaryOp::BitSRem, e1, e2) => {
+            check_bv_exprs(typing, BinaryOp::BitSRem, "bvsrem", &[e1.clone(), e2.clone()])
         }
         ExprX::Binary(BinaryOp::LShr, e1, e2) => {
             check_bv_exprs(typing, BinaryOp::LShr, ">> (lshr)", &[e1.clone(), e2.clone()])
