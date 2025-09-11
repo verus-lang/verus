85742343d19fc408381a304d5e5dc86919f15e81
Jon Howell <howell@vmware.com>
2025-09-11 16:08:57 -0700

    fmt

--- source/vstd/map_lib.rs
+++ source/vstd/map_lib.rs
@@ -566,7 +566,11 @@ pub broadcast group group_map_union {
 }
 
 /// submap_of (<=) is transitive.
-pub broadcast proof fn lemma_submap_of_trans<K, V, FINITE: Finiteness>(m1: GMap<K, V, FINITE>, m2: GMap<K, V, FINITE>, m3: GMap<K, V, FINITE>)
+pub broadcast proof fn lemma_submap_of_trans<K, V, FINITE: Finiteness>(
+    m1: GMap<K, V, FINITE>,
+    m2: GMap<K, V, FINITE>,
+    m3: GMap<K, V, FINITE>,
+)
     requires
         #[trigger] m1.submap_of(m2),
         #[trigger] m2.submap_of(m3),
