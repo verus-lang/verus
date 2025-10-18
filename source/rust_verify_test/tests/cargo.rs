7b8c9bfe976c4f1360b2d59141c5150fad54fe2f
Bryan Parno <parno@cmu.edu>
2025-10-18 16:04:43 -0400

    Extend the test harness so that it automatically calls `cargo verus` on all of the crates in `cargo-tests/verified`, and it calls `cargo` on all of the crates in `cargo-tests/unverified`.

--- source/rust_verify_test/tests/cargo.rs
+++ source/rust_verify_test/tests/cargo.rs
@@ -3,6 +3,41 @@
 mod common;
 use common::*;
 use tempfile::tempdir;
+use rust_verify_test_macros::cargo_examples;
+
+fn run_cargo_verus_for_dir(dir: &str) {
+    let current_exe = std::env::current_exe().unwrap();
+    let test_dir = current_exe.parent().unwrap().parent().unwrap().parent().unwrap().parent().unwrap().join(dir);
+
+    let args = vec!["clean"];
+    let run = run_cargo_verus(&args, &test_dir.as_path());
+    assert!(run.status.success());
+
+    let args = vec!["verify"];
+    let run = run_cargo_verus(&args, &test_dir.as_path());
+    assert!(run.status.success());
+
+    let args = vec!["build"];
+    let run = run_cargo_verus(&args, &test_dir.as_path());
+    assert!(run.status.success());
+}
+
+fn run_vanilla_cargo_for_dir(dir: &str) {
+    let current_exe = std::env::current_exe().unwrap();
+    let test_dir = current_exe.parent().unwrap().parent().unwrap().parent().unwrap().parent().unwrap().join(dir);
+
+    let args = vec!["clean"];
+    let run = run_cargo(&args, &test_dir.as_path());
+    assert!(run.status.success());
+
+    let args = vec!["check"];
+    let run = run_cargo(&args, &test_dir.as_path());
+    assert!(run.status.success());
+
+    let args = vec!["build"];
+    let run = run_cargo(&args, &test_dir.as_path());
+    assert!(run.status.success());
+}
 
 #[test] 
 fn cargo_new_verifies() {
@@ -28,13 +63,8 @@ fn cargo_new_builds() {
     assert!(run.status.success());
 }
 
-#[test] 
-#[ignore]
-fn vstd_macros() {
-    let current_exe = std::env::current_exe().unwrap();
-    let test_dir = current_exe.parent().unwrap().parent().unwrap().parent().unwrap().parent().unwrap().join("rust_verify_test/tests/cargo-tests");
-    let test_dir = test_dir.join("vstd-macros");
-    let args = vec!["verify"];
-    let run = run_cargo_verus(&args, &test_dir.as_path());
-    assert!(run.status.success());
-}
+// Tests that run `cargo verus {verify, build}` on each crate in the cargo-tests/verified directory
+cargo_examples!(true);
+
+// Tests that run `cargo {check, build}` on each crate in the cargo-tests/verified directory
+cargo_examples!(false);
