4d99143eaeccf005aa807a908793b3f4c4d18dbd
Elanor Tang <elanor@umich.edu>
2025-10-20 12:47:09 -0400

    debug type invariant panic

--- source/vir/src/user_defined_type_invariants.rs
+++ source/vir/src/user_defined_type_invariants.rs
@@ -37,6 +37,7 @@ pub(crate) fn annotate_user_defined_invariants(
         &|expr: &Expr| {
             match &expr.x {
                 ExprX::Ctor(Dt::Path(_), ..) => {
+                    dbg!(expr);
                     if info.ctor_needs_check[&expr.span.id]
                         && typ_has_user_defined_type_invariant(datatypes, &expr.typ)
                     {
