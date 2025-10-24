ea096f275cc4c328c7fad5dd780bcf7a65d08f13
Bryan Parno <parno@users.noreply.github.com>
2025-10-24 14:44:54 -0400

    Update the existing GitHub release workflow to also publish any updated crates to crates.io (#1881)
    * Add ability to invoke `cargo publish`
    * Update default features for verus_syn, so that a bare `cargo publish`
    succeeds.  Update verus_prettyplease to specify the full set of features
    it needs from verus_syn, so that a bare `cargo publish` succeeds with it
    as well.
    * Update the release workflow to run the version bumper and publish any
    updated Verus crates
    * Offer a --publish instead of --dry-run option, as per @jaybosamiya-ms's
    suggestion.
    * Update the version bumper to bump versions for all directly modified
    crates *and* the crates that transitively depend on them.
    * Swap to a subcommand style, so that we can externally perform
    a git commit between the update and publish steps.
    * Update the release workflow to update crate versions, git commit the
    changes, and publish to crates.io.  Everything is still in practice
    mode, aside from the git changes.

--- source/tools/bump_crate_versions/src/main.rs
+++ source/tools/bump_crate_versions/src/main.rs
@@ -1,16 +1,35 @@
-use std::{fs, path::Path, process::Stdio, sync::LazyLock};
+use std::{collections::{HashMap, HashSet}, fs, path::Path, process::Stdio, sync::LazyLock};
 use toml_edit::DocumentMut;
 use regex::Regex;
+use clap::Parser as ClapParser;
 
-/*
- * This tool scans for modified crates in the Verus repository and updates the version numbers
- * in their respective Cargo.toml files. In cases where one crate depends on another, we also
- * update the version of the dependency in the dependent crate's Cargo.toml.  Finally, when vstd
- * is modified, we also update the version in the cargo-verus template.  The code is optimized
- * for readability and maintainability, rather than performance.
- *
- * Usage: Run this tool from the root of the Verus repository.
- */
+
+/// This tool scans for modified crates in the Verus repository and updates the version numbers
+/// in their respective Cargo.toml files. In cases where one crate depends on another, we also
+/// update the version of the dependency in the dependent crate's Cargo.toml.  Finally, when vstd
+/// is modified, we also update the version in the cargo-verus template.  The code is optimized
+/// for readability and maintainability, rather than performance.
+///
+/// Usage: Run this tool from the root of the Verus repository.
+
+#[derive(clap::Subcommand)]
+enum Command {
+    /// Update version numbers
+    Update,
+    /// Publish any updated crates to crates.io
+    Publish {
+        /// Operate in dry-run mode, instead of actually publishing
+        #[arg(long = "dry-run")]
+        dry_run: bool,
+    },
+}
+
+#[derive(ClapParser)]
+#[command(version, about)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
 
 // Path to cargo-verus's main file, where we have a static string
 // indicating which version of vstd to use
@@ -32,12 +51,42 @@ static NEW_VERSION: LazyLock<String> = LazyLock::new(|| {
     )
 });
 
-#[derive(Clone)]
+
+#[derive(Clone,Debug,PartialEq,Eq,Hash,PartialOrd,Ord)]
 struct Crate {
     // Crate's official name
     name: String,
     // Path to the crate's directory, relative to the repository root
     path: String,
+}
+
+// For each crate, identify the other crates (in `crates`) that depend on it
+fn compute_immediate_deps(crates: &Vec<Crate>) -> HashMap<Crate, Vec<Crate>> {
+    let mut dep_map: HashMap<Crate, Vec<Crate>> = HashMap::new();
+    for krate in crates {
+        let cargo_toml_path = Path::new(&krate.path).join("Cargo.toml");
+
+        // Read the Cargo.toml file
+        let content = fs::read_to_string(&cargo_toml_path).expect(format!("Failed to read {}", cargo_toml_path.display()).as_str());
+        let doc = content.parse::<DocumentMut>().expect("Failed to parse Cargo.toml");
+
+        for maybe_dep in crates {
+            if doc.contains_key("dependencies") && doc["dependencies"].get(&maybe_dep.name).is_some() {
+                // krate depends on maybe_dep, so add an edge: maybe_dep -> krate,
+                // so if maybe_dep is updated, we know that krate needs to be updated too
+                dep_map.entry(maybe_dep.clone()).and_modify(|v: &mut Vec<Crate>| v.push(krate.clone())).or_insert(vec![krate.clone()]);
+            }
+        }
+    }
+    dep_map
+}
+
+fn display_dep_map(dep_map: &HashMap<Crate, Vec<Crate>>, tab_depth: usize) {
+    for (krate, dependents) in dep_map {
+        print!("{}{}: ", "\t".repeat(tab_depth), krate.name);
+        let names = dependents.iter().map(|c| c.name.clone()).collect::<Vec<String>>();
+        println!("{}", names.join(", "));
+    }
 }
 
 // Given a path to a directory, run git to check for the most recent change to the Cargo.toml file
@@ -96,7 +145,7 @@ fn update_toml_version(dir: &Path) {
     fs::write(&cargo_toml_path, content).expect("Failed to write Cargo.toml");
 }
 
-fn update_toml_dependencies(dir: &Path, dependencies: &Vec<Crate>) {
+fn update_toml_dependencies(dir: &Path, dependencies: &Vec<&Crate>) {
     let cargo_toml_path = dir.join("Cargo.toml");
 
     // Read the Cargo.toml file
@@ -115,6 +164,27 @@ fn update_toml_dependencies(dir: &Path, dependencies: &Vec<Crate>) {
     fs::write(&cargo_toml_path, content).expect("Failed to write Cargo.toml");
 }
 
+fn publish(dir: &Path, dry_run: bool) {
+    use std::process::Command;
+
+    let mut cmd = Command::new("cargo");
+    cmd.arg("publish");
+    if dry_run {
+        cmd.arg("--dry-run");
+    }
+    let status = cmd
+        .current_dir(dir)
+        .status()
+        .expect("Failed to execute cargo publish");
+
+    if !status.success() {
+        panic!("cargo publish{} failed for {}", 
+            if dry_run { " --dry-run" } else { "" },
+            dir.display()
+        );
+    }
+}
+
 fn update_cargo_verus_template() {
     let main = Path::new(CARGO_VERUS_MAIN);
     let content = fs::read_to_string(main).expect("Failed to read cargo-verus main.rs");
@@ -126,14 +196,91 @@ fn update_cargo_verus_template() {
         panic!("Expected to find exactly one occurence of 'vstd = ' in {}.  Found {}.", CARGO_VERUS_MAIN, count);
     }
     let updated_content = re.replace(&content, format!("vstd = \"={}\"", *NEW_VERSION).as_str());
-    println!("Updated cargo-verus main.rs:\n{}", updated_content);
+    //println!("Updated cargo-verus main.rs:\n{}", updated_content);
+    println!("Updated cargo-verus main.rs\n");
 
     // Write the updated content back to the file
     fs::write(main, updated_content.to_string()).expect("Failed to write cargo-verus main.rs");
 }
 
-fn main() {
-    println!("Scanning for modified crates...");
+fn update_crates(crates: Vec<Crate>) {
+    // Compute directly modified crates
+    println!("\nScanning for crates with modified source code...");
+    let mut modified_crates: HashSet<&Crate> = HashSet::new();
+    for krate in &crates {
+        if let Some(commit) = last_commit(&Path::new(&krate.path)) {
+            if src_modified(&Path::new(&krate.path), &commit) {
+                println!("\t{}:\n\t\tHAS been modified since commit {}.\n", krate.name, commit);
+                modified_crates.insert(&krate);
+            } else {
+                println!("\t{}:\n\t\t has NOT been modified since commit {}.\n", krate.name, commit);
+            }
+        } else {
+            println!("{}: Could not find last commit for {}", krate.name, krate.path);
+        }
+    }
+
+    // Compute crates that (transitively) depend on modified crates, and hence themselves need a version update
+    println!("\nScanning for crates that depend on modified crates...");
+    let dep_map = compute_immediate_deps(&crates);
+    println!("\tworking from the following map A: [B1, ..., BN], where each Bi depends on A");
+    display_dep_map(&dep_map, 2);
+    println!("\tidentifying transitively affected crates...");
+    loop {
+        let mut new_modifications = HashSet::new();
+        for krate in &modified_crates {
+            if dep_map.contains_key(krate) {
+                for dependent in &dep_map[krate] {
+                    // For each dependent that relies on a modified krate,
+                    // if it hasn't already been marked for modification, mark it now.
+                    if !modified_crates.contains(&dependent) {
+                        new_modifications.insert(dependent);
+                        println!("\t\t{}: depends on modified crate {}", dependent.name, krate.name);
+                    }
+                }
+            }
+        }
+
+        if new_modifications.len() == 0 {
+            break;
+        } else {
+            modified_crates.extend(new_modifications.iter());
+        }
+    }
+
+    // Do the modifications
+    if modified_crates.len() > 0 {
+        println!("\nModifying each of the following crates to version {} and updating their dependencies ...", *NEW_VERSION);
+        let mut modified_crates: Vec<&Crate> = modified_crates.into_iter().collect();
+        modified_crates.sort();
+        for krate in &modified_crates {
+            println!("\t{}", krate.name);
+            update_toml_version(&Path::new(&krate.path));
+            update_toml_dependencies(&Path::new(&krate.path), &modified_crates);
+
+            if krate.name == "vstd" {
+                update_cargo_verus_template();
+            }
+        }
+    }
+
+}
+
+fn publish_crates(crates: Vec<Crate>, dry_run: bool) {
+    for krate in crates {
+        if dry_run {
+            println!("Performing a dry-run publish of modified crate {}", krate.name);
+        } else {
+            println!("Publishing modified crate {}", krate.name);
+        }
+        publish(&Path::new(&krate.path), dry_run);
+    }
+
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let  args = Args::parse();
+
     let crates = vec![
         Crate {
             name: "vstd".to_string(),
@@ -161,28 +308,15 @@ fn main() {
         },
     ];
 
-    let mut modified_crates = Vec::new();
-    for krate in &crates {
-        if let Some(commit) = last_commit(&Path::new(&krate.path)) {
-            if src_modified(&Path::new(&krate.path), &commit) {
-                println!("\t{}:\n\t\thas been modified since commit {}.\n\t\tUpdating version to {}", krate.name, commit, *NEW_VERSION);
-                update_toml_version(&Path::new(&krate.path));
-                modified_crates.push(krate.clone());
-                if krate.name == "vstd" {
-                    update_cargo_verus_template();
-                }
-            } else {
-                println!("\t{}:\n\t\t has not been modified since commit {}", krate.name, commit);
-            }
-        } else {
-            println!("{}: Could not find last commit for {}", krate.name, krate.path);
-        }
+    let test_path = Path::new(&crates[0].path);
+    if !Path::exists(test_path) {
+        return Err(format!("Failed to find path: {}.  Hint: This tool expects to run in the root of the Verus repo", test_path.display()).into());
     }
 
-    if !modified_crates.is_empty() {
-        for krate in &crates{
-            println!("Updating dependencies for {}", krate.name);
-            update_toml_dependencies(&Path::new(&krate.path), &modified_crates);
-        }
+    match &args.command {
+        Command::Update => update_crates(crates),
+        Command::Publish { dry_run } => publish_crates(crates, *dry_run),
     }
+
+    Ok(())
 }
