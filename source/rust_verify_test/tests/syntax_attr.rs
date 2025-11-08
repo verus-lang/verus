10c88d3f1c2a91ce894fcaf27477af4564cbd1d0
Hiroki <haobin.chen@certik.com>
2025-11-08 14:25:02 -0500

    Fix trailing commas for `with`

--- source/rust_verify_test/tests/syntax_attr.rs
+++ source/rust_verify_test/tests/syntax_attr.rs
@@ -987,3 +987,73 @@ test_verify_one_file! {
         }
     } => Err(e) => assert_any_vir_error_msg(e, "cannot read const with mode exec")
 }
+
+test_verify_one_file! {
+    #[test] test_verus_spec_with_trailing_comma code!{
+        use vstd::prelude::*;
+
+        #[verus_spec(ret =>
+            with
+                Tracked(y): Tracked<&mut u32>,
+                Ghost(w): Ghost<u32>,
+                    -> z: Ghost<u32>,
+            requires
+                x < 100,
+            ensures
+                ret == x + 1,
+                z@ == x,
+            )]
+        fn foo(x: u32) -> u32 {
+            proof_with!(|= Ghost(x));
+            (x + 1)
+        }
+
+        #[verus_spec(ret =>
+            with
+                Tracked(y): Tracked<&mut u32>,
+                Ghost(w): Ghost<u32>
+                    -> z: Ghost<u32>,
+            requires
+                x < 100,
+            ensures
+                ret == x + 1,
+                z@ == x,
+            )]
+        fn bar(x: u32) -> u32 {
+            proof_with!(|= Ghost(x));
+            (x + 1)
+        }
+
+        #[verus_spec(ret =>
+            with
+                Tracked(y): Tracked<&mut u32>,
+                Ghost(w): Ghost<u32>,
+                    -> z: Ghost<u32>
+            requires
+                x < 100,
+            ensures
+                ret == x + 1,
+                z@ == x,
+            )]
+        fn baz(x: u32) -> u32 {
+            proof_with!(|= Ghost(x));
+            (x + 1)
+        }
+
+        #[verus_spec(ret =>
+            with
+                Tracked(y): Tracked<&mut u32>,
+                Ghost(w): Ghost<u32>
+                    -> z: Ghost<u32>
+            requires
+                x < 100,
+            ensures
+                ret == x + 1,
+                z@ == x,
+            )]
+        fn qux(x: u32) -> u32 {
+            proof_with!(|= Ghost(x));
+            (x + 1)
+        }
+    } => Ok(())
+}
