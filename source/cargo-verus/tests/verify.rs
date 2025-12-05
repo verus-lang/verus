aa43b2c5729e80589fdff7bd3819b4c7cc22d5a5
Mate Kovacs <mkovaxx@gmail.com>
2025-12-05 19:55:16 +0900

    use good old .expect() in tests instead of Result

--- source/cargo-verus/tests/verify.rs
+++ source/cargo-verus/tests/verify.rs
@@ -5,28 +5,28 @@ compile_error!("enable the `integration-tests` feature to run these tests");
 mod utils;
 
 #[test]
-fn verify_single_crate() -> anyhow::Result<()> {
-    let project_dir = utils::clone_fixture(utils::SINGLE_CRATE)?;
+fn verify_single_crate() {
+    let project_dir = utils::clone_fixture(utils::SINGLE_CRATE).expect("clone fixture");
 
     let (output, captured) = utils::run_cargo_verus(|cmd| {
         cmd.current_dir(&project_dir).arg("verify");
-    })?;
+    })
+    .expect("receive output");
 
     assert!(output.status.success());
     assert_eq!(captured.args, vec!["build"]);
-
-    Ok(())
 }
 
 #[test]
 fn verify_single_crate_explicit_manifest() -> anyhow::Result<()> {
-    let project_dir = utils::clone_fixture(utils::SINGLE_CRATE)?;
+    let project_dir = utils::clone_fixture(utils::SINGLE_CRATE).expect("clone fixture");
     let manifest_path = project_dir.join("Cargo.toml");
 
     let (output, captured) = utils::run_cargo_verus(|cmd| {
         cmd.arg("verify");
         cmd.arg("--manifest-path").arg(&manifest_path);
-    })?;
+    })
+    .expect("receive output");
 
     assert!(output.status.success());
     assert_eq!(
