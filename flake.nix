ca8a6a884e7d9dc475697ebd355f4ca11aa54ce8
Stephen Huan <stephen.huan@cgdct.moe>
2025-12-22 06:29:06 -0500

    add verusfmt

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
           inherit system overlays;
         };
         formatter = pkgs.nixpkgs-fmt;
-        linters = [ pkgs.statix ];
+        linters = [ pkgs.statix vargo verusfmt ];
         rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
         # Inline rustup
@@ -104,6 +104,38 @@
           meta = meta // { mainProgram = "verus"; };
         };
 
+        verusfmt = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "verusfmt";
+          version = "0.6.1";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "verus-lang";
+            repo = "verusfmt";
+            tag = "v${finalAttrs.version}";
+            hash = "sha256-+NHI2dvCxEGVIUF9zO2aVvVbPSLRtsHFCIHU4cfRzUY=";
+          };
+
+          cargoHash = "sha256-8r8PzBrYZWibeFDh2nENctEEkigUzQeD9uD0Jl/Nv5U=";
+
+          nativeCheckInputs = [ pkgs.cargo pkgs.rustfmt ];
+
+          doCheck = true;
+
+          meta = {
+            homepage = "https://github.com/verus-lang/verusfmt";
+            description = "An Opinionated Formatter for Verus";
+            license = lib.licenses.mit;
+            mainProgram = "verusfmt";
+            maintainers = with lib.maintainers; [ stephen-huan ];
+            platforms = [
+              "x86_64-linux"
+              "x86_64-darwin"
+              "aarch64-darwin"
+              "x86_64-windows"
+            ];
+          };
+        });
+
         # EXPECTED_Z3_VERSION in tools/common/consts.rs
         z3 = pkgs.z3.overrideAttrs (finalAttrs: previousAttrs: {
           version = "4.12.5";
@@ -131,9 +163,16 @@
       {
         packages.${system} = rec {
           default = verus;
-          inherit rust-bin rustup vargo verus z3 cvc5;
+          inherit rust-bin rustup vargo verus verusfmt z3 cvc5;
         };
-        formatter.${system} = formatter;
+        formatter.${system} = pkgs.writeShellApplication {
+          name = "formatter";
+          runtimeInputs = [ verusfmt ] ++ lib.singleton formatter;
+          text = ''
+            nixpkgs-fmt "$@"
+            find vstd -name \*.rs -print0 | xargs -0 -n1 verusfmt
+          '';
+        };
         checks.${system}.lint = pkgs.stdenvNoCC.mkDerivation {
           name = "lint";
           src = ./.;
@@ -167,6 +206,7 @@
             rustup
             vargo
             verus
+            verusfmt
             z3
             cvc5
           ];
