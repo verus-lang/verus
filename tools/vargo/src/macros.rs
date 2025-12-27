5c5f0eb65358ab14f53f94694f96d6f072f741ef
Baltasar Dinis <bsdinis@mit.edu>
2025-12-27 13:40:11 -0500

    vargo: create logging macros
    This simplifies their call site
    Also moved to yansi 1.0

---
+++ tools/vargo/src/macros.rs
@@ -0,0 +1,26 @@
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        use yansi::Paint;
+        let vargo_nest = *VARGO_NEST.read().unwrap();
+        eprintln!(
+            "{}{}",
+            format!("vargo info [{vargo_nest}]: ").blue().bold(),
+            format!($($arg)*).blue()
+        )
+    }};
+}
+
+macro_rules! warning {
+    ($($arg:tt)*) => {{
+        use yansi::Paint;
+        let vargo_nest = *VARGO_NEST.read().unwrap();
+        eprintln!(
+            "{}{}",
+            format!("vargo warn [{vargo_nest}]: ").yellow().bold(),
+            format!($($arg)*).yellow()
+        )
+    }};
+}
+
+pub(crate) use info;
+pub(crate) use warning;
