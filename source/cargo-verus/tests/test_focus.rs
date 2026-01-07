59f18633d61aba36d4968079a33ea192390fb33e
Mate Kovacs <mkovaxx@gmail.com>
2026-01-07 10:02:04 +0900

    WIP

--- source/cargo-verus/tests/test_focus.rs
+++ source/cargo-verus/tests/test_focus.rs
@@ -32,7 +32,7 @@ fn crate_optin_manifest() {
 }
 
 #[test]
-fn workspace_manifest_package_hasdeps() {
+fn workspace_manifest() {
     let optin = "optin";
     let optout = "optout";
     let unset = "unset";
@@ -55,19 +55,12 @@ fn workspace_manifest_package_hasdeps() {
     let (status, data) = run_cargo_verus(|cmd| {
         cmd.arg("focus");
         cmd.arg("--manifest-path").arg(&manifest_path);
-        cmd.arg("--package").arg(hasdeps);
     });
 
     assert!(status.success());
     assert_eq!(
         data.args,
-        vec![
-            "build",
-            "--manifest-path",
-            manifest_path.to_str().expect("manifest path to string"),
-            "--package",
-            hasdeps,
-        ]
+        vec!["build", "--manifest-path", manifest_path.to_str().expect("manifest path to string"),]
     );
 
     data.assert_env_has("RUSTC_WRAPPER");
@@ -75,6 +68,53 @@ fn workspace_manifest_package_hasdeps() {
     data.assert_env_sets("__VERUS_DRIVER_VIA_CARGO__", "1");
 
     data.assert_env_sets_key_prefix(&verify_hasdeps_prefix, "1");
+    let verify_hasdeps_args = data
+        .parse_driver_args_for_key_prefix(&format!(" __VERUS_DRIVER_ARGS_FOR_{hasdeps}-0.1.0-"));
+    assert!(verify_hasdeps_args.contains(&"--no-verify"));
+
+    data.assert_env_sets_key_prefix(&verify_optin_prefix, "1");
+    let verify_optin_args =
+        data.parse_driver_args_for_key_prefix(&format!(" __VERUS_DRIVER_ARGS_FOR_{optin}-0.1.0-"));
+    assert!(verify_optin_args.contains(&"--no-verify"));
+
+    data.assert_env_has_no_key_prefix(&verify_optout_prefix);
+    data.assert_env_has_no_key_prefix(&verify_unset_prefix);
+}
+
+#[test]
+fn workspace_package_hasdeps() {
+    let optin = "optin";
+    let optout = "optout";
+    let unset = "unset";
+    let hasdeps = "hasdeps";
+
+    let workspace_dir = MockWorkspace::new()
+        .member(MockPackage::new(optin).lib().verify(true))
+        .member(MockPackage::new(optout).lib().verify(false))
+        .member(MockPackage::new(unset).lib())
+        .member(MockPackage::new(hasdeps).lib().dep(optin).verify(true))
+        .materialize();
+
+    let verify_optin_prefix = format!("__VERUS_DRIVER_VERIFY_{optin}-0.1.0-");
+    let verify_optout_prefix = format!("__VERUS_DRIVER_VERIFY_{optout}-0.1.0-");
+    let verify_unset_prefix = format!("__VERUS_DRIVER_VERIFY_{unset}-0.1.0-");
+    let verify_hasdeps_prefix = format!("__VERUS_DRIVER_VERIFY_{hasdeps}-0.1.0-");
+
+    let (status, data) = run_cargo_verus(|cmd| {
+        cmd.current_dir(&workspace_dir).arg("focus");
+    });
+
+    assert!(status.success());
+    assert_eq!(data.args, vec!["build"]);
+
+    data.assert_env_has("RUSTC_WRAPPER");
+    data.assert_env_sets("__CARGO_DEFAULT_LIB_METADATA", "verus");
+    data.assert_env_sets("__VERUS_DRIVER_VIA_CARGO__", "1");
+
+    data.assert_env_sets_key_prefix(&verify_hasdeps_prefix, "1");
+    let verify_hasdeps_args = data
+        .parse_driver_args_for_key_prefix(&format!(" __VERUS_DRIVER_ARGS_FOR_{hasdeps}-0.1.0-"));
+    assert!(!verify_hasdeps_args.contains(&"--no-verify"));
 
     data.assert_env_sets_key_prefix(&verify_optin_prefix, "1");
     let verify_optin_args =
