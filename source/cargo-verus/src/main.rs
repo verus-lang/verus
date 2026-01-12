ed8ac9440bda1f77b878e32fe996373331e4c6fd
Mate Kovacs <mkovaxx@gmail.com>
2026-01-12 13:01:43 +0900

    fix earlier mistake

--- source/cargo-verus/src/main.rs
+++ source/cargo-verus/src/main.rs
@@ -19,67 +19,56 @@ mod subcommands;
 #[cfg(any(test, feature = "integration-tests"))]
 pub mod test_utils;
 
-use crate::cli::{CargoVerusCli, VerusSubcommand};
+use crate::{
+    cli::{CargoVerusCli, VerusSubcommand},
+    subcommands::CargoRunConfig,
+};
 
 pub fn main() -> Result<ExitCode> {
     let normalized_args: Vec<_> = normalize_args(env::args()).collect();
     let parsed_cli =
         CargoVerusCli::parse_from(normalized_args.iter().cloned()).clap_trailing_args_hotfix();
 
-    match parsed_cli.command {
+    let cfg = match parsed_cli.command {
         VerusSubcommand::New(new_cmd) => {
             match (new_cmd.bin, new_cmd.lib) {
                 (Some(name), None) => subcommands::create_new_project(&name, true)?,
                 (None, Some(name)) => subcommands::create_new_project(&name, false)?,
                 _ => unreachable!("clap enforces exactly one of --bin/--lib"),
             }
-            Ok(ExitCode::SUCCESS)
+            return Ok(ExitCode::SUCCESS);
         }
-        VerusSubcommand::Verify(cmd) => {
-            let warn_if_nothing_verified = true;
-            let verify_deps = true;
-            subcommands::run_cargo(
-                "build",
-                verify_deps,
-                &cmd.cargo_opts,
-                &cmd.verus_args,
-                warn_if_nothing_verified,
-            )
-        }
-        VerusSubcommand::Focus(cmd) => {
-            let warn_if_nothing_verified = true;
-            let verify_deps = false;
-            subcommands::run_cargo(
-                "build",
-                verify_deps,
-                &cmd.cargo_opts,
-                &cmd.verus_args,
-                warn_if_nothing_verified,
-            )
-        }
-        VerusSubcommand::Build(cmd) => {
-            let warn_if_nothing_verified = false;
-            let verify_deps = true;
-            subcommands::run_cargo(
-                "build",
-                verify_deps,
-                &cmd.cargo_opts,
-                &cmd.verus_args,
-                warn_if_nothing_verified,
-            )
-        }
-        VerusSubcommand::Check(cmd) => {
-            let warn_if_nothing_verified = true;
-            let verify_deps = true;
-            subcommands::run_cargo(
-                "check",
-                verify_deps,
-                &cmd.cargo_opts,
-                &cmd.verus_args,
-                warn_if_nothing_verified,
-            )
-        }
-    }
+        VerusSubcommand::Verify(options) => CargoRunConfig {
+            subcommand: "build",
+            options,
+            compile_primary: false,
+            verify_deps: true,
+            warn_if_nothing_verified: true,
+        },
+        VerusSubcommand::Focus(options) => CargoRunConfig {
+            subcommand: "build",
+            options,
+            compile_primary: false,
+            verify_deps: false,
+            warn_if_nothing_verified: true,
+        },
+        VerusSubcommand::Build(options) => CargoRunConfig {
+            subcommand: "build",
+            options,
+            compile_primary: true,
+            verify_deps: true,
+            warn_if_nothing_verified: false,
+        },
+        VerusSubcommand::Check(options) => CargoRunConfig {
+            subcommand: "check",
+            options,
+            compile_primary: false,
+            verify_deps: true,
+            warn_if_nothing_verified: true,
+        },
+    };
+
+    subcommands::run_cargo(cfg)
 }
 
 fn normalize_args(args: impl Iterator<Item = String>) -> impl Iterator<Item = String> {
--- source/cargo-verus/src/subcommands.rs
+++ source/cargo-verus/src/subcommands.rs
@@ -7,7 +7,7 @@ use anyhow::{anyhow, bail, Context, Result};
 use cargo_metadata::PackageId;
 use colored::Colorize;
 
-use crate::cli::CargoOptions;
+use crate::cli::{CargoOptions, VerifyCommand};
 use crate::metadata::{fetch_metadata, make_package_id, MetadataIndex};
 
 pub const VERUS_DRIVER_ARGS: &str = " __VERUS_DRIVER_ARGS__";
@@ -89,32 +89,34 @@ verify = true
     Ok(())
 }
 
-pub fn run_cargo(
-    subcommand: &str,
-    verify_deps: bool,
-    cargo_options: &CargoOptions,
-    verus_args: &[String],
-    warn_if_nothing_verified: bool,
-) -> Result<ExitCode> {
+pub struct CargoRunConfig {
+    pub subcommand: &'static str,
+    pub options: VerifyCommand,
+    pub compile_primary: bool,
+    pub verify_deps: bool,
+    pub warn_if_nothing_verified: bool,
+}
+
+pub fn run_cargo(cfg: CargoRunConfig) -> Result<ExitCode> {
     //////////////////////////////////////////////////
     // Phase 1: fetch metadata via `cargo metadata` //
     //////////////////////////////////////////////////
     let metadata_args = {
         let for_cargo_metadata = true;
-        make_cargo_args(cargo_options, for_cargo_metadata)
+        make_cargo_args(&cfg.options.cargo_opts, for_cargo_metadata)
     };
     let metadata = fetch_metadata(&metadata_args)?;
     let metadata_index = MetadataIndex::new(&metadata)?;
 
     let (included_packages, _excluded_packages) =
-        cargo_options.workspace.partition_packages(&metadata);
+        cfg.options.cargo_opts.workspace.partition_packages(&metadata);
 
     let root_packages: Set<PackageId> =
         included_packages.iter().map(|package| package.id.clone()).collect();
     let all_packages = metadata_index.get_transitive_closure(root_packages.clone());
 
     let packages_to_process = &all_packages;
-    let packages_to_verify = if verify_deps { &all_packages } else { &root_packages };
+    let packages_to_verify = if cfg.verify_deps { &all_packages } else { &root_packages };
 
     /////////////////////////////////////////////////
     // Phase 2: run Verus via `cargo {subcommand}` //
@@ -122,21 +124,22 @@ pub fn run_cargo(
 
     let cargo_args = {
         let for_cargo_metadata = false;
-        make_cargo_args(cargo_options, for_cargo_metadata)
+        make_cargo_args(&cfg.options.cargo_opts, for_cargo_metadata)
     };
 
-    let mut common_verus_driver_args: Vec<String> = vec![];
+    let mut common_verus_driver_args: Vec<String> =
+        vec!["--VIA-CARGO".to_owned(), "compile-when-not-primary-package".to_owned()];
 
-    if !warn_if_nothing_verified {
+    if cfg.compile_primary {
         common_verus_driver_args.extend_from_slice(&[
             "--VIA-CARGO".to_owned(),
             "compile-when-primary-package".to_owned(),
         ]);
     }
 
-    common_verus_driver_args.extend(verus_args.iter().cloned());
+    common_verus_driver_args.extend(cfg.options.verus_args.iter().cloned());
     let (mut command, verified_something) = make_cargo_command(
-        subcommand,
+        cfg.subcommand,
         &cargo_args,
         common_verus_driver_args,
         &metadata_index,
@@ -150,7 +153,7 @@ pub fn run_cargo(
         .wait()
         .context("Failed to wait for cargo")?;
 
-    if warn_if_nothing_verified && !verified_something {
+    if cfg.warn_if_nothing_verified && !verified_something {
         eprint!(
             "{}",
             "\
