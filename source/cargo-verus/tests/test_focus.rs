41600c6e26da3b072df878a4397b43ed9768789f
Mate Kovacs <mkovaxx@gmail.com>
2026-01-12 14:58:57 +0900

    fix tests

--- source/cargo-verus/tests/test_focus.rs
+++ source/cargo-verus/tests/test_focus.rs
@@ -10,7 +10,10 @@ fn crate_optin_manifest() {
     let verify_for_crate_prefix = format!(" __VERUS_DRIVER_ARGS_FOR_{crate_name}-0.1.0-");
     let package_dir = MockPackage::new(crate_name).lib().verify(true).materialize();
 
-    let manifest_path = package_dir.path().join("Cargo.toml");
+    // Canonicalize the path to avoid weirdness on e.g. macOS
+    let package_dir = package_dir.path().canonicalize().expect("canonical path");
+
+    let manifest_path = package_dir.join("Cargo.toml");
 
     let (status, data) = run_cargo_verus(|cmd| {
         cmd.arg("focus");
@@ -19,7 +22,7 @@ fn crate_optin_manifest() {
 
     assert!(status.success());
 
-    let target_dir = package_dir.path().join("target").join("verus-partial");
+    let target_dir = package_dir.join("target").join("verus-partial");
 
     assert_eq!(
         data.args,
@@ -55,12 +58,15 @@ fn workspace_manifest() {
         ])
         .materialize();
 
+    // Canonicalize the path to avoid weirdness on e.g. macOS
+    let workspace_dir = workspace_dir.path().canonicalize().expect("canonical path");
+
     let verify_optin_prefix = format!("__VERUS_DRIVER_VERIFY_{optin}-0.1.0-");
     let verify_optout_prefix = format!("__VERUS_DRIVER_VERIFY_{optout}-0.1.0-");
     let verify_unset_prefix = format!("__VERUS_DRIVER_VERIFY_{unset}-0.1.0-");
     let verify_hasdeps_prefix = format!("__VERUS_DRIVER_VERIFY_{hasdeps}-0.1.0-");
 
-    let manifest_path = workspace_dir.path().join("Cargo.toml");
+    let manifest_path = workspace_dir.join("Cargo.toml");
 
     let (status, data) = run_cargo_verus(|cmd| {
         cmd.arg("focus");
@@ -69,7 +75,7 @@ fn workspace_manifest() {
 
     assert!(status.success());
 
-    let target_dir = workspace_dir.path().join("target").join("verus-partial");
+    let target_dir = workspace_dir.join("target").join("verus-partial");
 
     assert_eq!(
         data.args,
@@ -116,6 +122,9 @@ fn workspace_package_hasdeps() {
         ])
         .materialize();
 
+    // Canonicalize the path to avoid weirdness on e.g. macOS
+    let workspace_dir = workspace_dir.path().canonicalize().expect("canonical path");
+
     let verify_optin_prefix = format!("__VERUS_DRIVER_VERIFY_{optin}-0.1.0-");
     let verify_optout_prefix = format!("__VERUS_DRIVER_VERIFY_{optout}-0.1.0-");
     let verify_unset_prefix = format!("__VERUS_DRIVER_VERIFY_{unset}-0.1.0-");
@@ -129,7 +138,7 @@ fn workspace_package_hasdeps() {
 
     assert!(status.success());
 
-    let target_dir = workspace_dir.path().join("target").join("verus-partial");
+    let target_dir = workspace_dir.join("target").join("verus-partial");
 
     assert_eq!(
         data.args,
