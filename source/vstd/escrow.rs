a842c569a48a426e8a9dee886a22d230aa6b131a
zero-to-nat <nneamtu@andrew.cmu.edu>
2026-01-14 17:01:57 -0500

    start working on axiomatized escrow api

---
+++ source/vstd/escrow.rs
@@ -0,0 +1,128 @@
+
+use super::prelude::*;
+use super::pcm::*;
+use crate::simple_pptr::*;
+
+verus! {
+
+/// Token PCM
+enum TokenCarrier {
+    Tok,
+    Empty,
+    Invalid,
+}
+
+impl PCM for TokenCarrier {
+    closed spec fn valid(self) -> bool {
+        match self {
+            TokenCarrier::Invalid => false,
+            TokenCarrier::Empty | TokenCarrier::Tok => true,
+        }
+    }
+
+    closed spec fn op(self, other: Self) -> Self {
+        match self {
+            TokenCarrier::Invalid => TokenCarrier::Invalid,
+            TokenCarrier::Empty => other,
+            TokenCarrier::Tok => match other {
+                TokenCarrier::Invalid | TokenCarrier::Tok => TokenCarrier::Invalid,
+                TokenCarrier::Empty => self,
+            },
+        }
+    }
+
+    closed spec fn unit() -> Self {
+        TokenCarrier::Empty
+    }
+
+    proof fn closed_under_incl(a: Self, b: Self) {
+    }
+
+    proof fn commutative(a: Self, b: Self) {
+    }
+
+    proof fn associative(a: Self, b: Self, c: Self) {
+    }
+
+    proof fn op_unit(a: Self) {
+    }
+
+    proof fn unit_valid() {
+    }
+}
+
+pub struct Token {
+    t: Resource<TokenCarrier>
+}
+
+impl Token {
+    #[verifier::type_invariant]
+    closed spec fn inv(&self) -> bool {
+        self.resource().value() matches TokenCarrier::Tok
+    }
+
+    closed spec fn resource(&self) -> Resource<TokenCarrier> {
+        self.t
+    }
+}
+
+/// Escrow, in the style of https://plv.mpi-sws.org/igps/igps-full.pdf
+
+pub trait Guard : Sized {
+    spec fn id(&self) -> int;
+
+    proof fn excl(tracked self, tracked other: Self) -> (tracked out: Self)
+        ensures
+            self.id() == other.id() ==> false,
+            out == self
+        ;
+}
+
+impl Guard for Token {
+    closed spec fn id(&self) -> int {
+        self.resource().loc()
+    }
+
+    proof fn excl(tracked self, tracked other: Self) -> (tracked out: Self) {
+        if (self.id() == other.id()) {
+            use_type_invariant(&self);
+            use_type_invariant(&other);
+
+            let tracked joined = self.t.join(other.t);
+            joined.validate();
+            assert(false);
+            proof_from_false()
+        } else {
+            self
+        }
+    }
+}
+
+pub trait Escrow<T, P: Guard> : Sized {
+    // payload type (Q) will always be PointsTo for now. this avoids complication in footnote 7
+
+    spec fn guard(&self) -> P;
+
+    spec fn payload(&self) -> PointsTo<T>;
+
+    proof fn intro(tracked payload: PointsTo<T>, guard_data: P) -> (tracked out: Self)
+        ensures
+            out.guard() == guard_data,
+            out.payload() == payload
+        ;
+
+    proof fn elim(tracked &self, tracked guard: P) -> (tracked out: PointsTo<T>)
+        requires
+            self.guard() == guard
+        ensures
+            self.payload() == out
+        ;
+
+    proof fn persistent(tracked &self) -> (tracked out: Self)
+        ensures
+            self.guard() == out.guard(),
+            self.payload() == out.payload()
+        ;
+}
+
+}
