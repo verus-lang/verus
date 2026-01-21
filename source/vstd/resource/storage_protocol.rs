2144986c6a064aafb7a5fd12d526a47087285f53
Baltasar Dinis <bsdinis@mit.edu>
2026-01-21 04:35:05 -0500

    resource: move implemented resource algebras to resource dirs

--- source/vstd/resource/storage_protocol.rs
+++ source/vstd/resource/storage_protocol.rs
@@ -1,13 +1,13 @@
-use super::prelude::*;
-use super::resource::Loc;
+use super::super::prelude::*;
+use super::Loc;
 
 verus! {
 
-broadcast use {super::set::group_set_axioms, super::map::group_map_axioms};
+broadcast use {super::super::set::group_set_axioms, super::super::map::group_map_axioms};
 
 /// Interface for "storage protocol" ghost state.
 /// This is an extension-slash-variant on the more well-known concept
-/// of "PCM" ghost state, which we also have an interface for [here](crate::resource::Resource).
+/// of "PCM" ghost state, which we also have an interface for [here](crate::resource::pcm::Resource).
 /// The unique feature of a storage protocol is the ability to use [`guard`](StorageResource::guard)
 /// to manipulate _shared_ references of ghost state.
 ///
@@ -32,7 +32,7 @@ broadcast use {super::set::group_set_axioms, super::map::group_map_axioms};
 #[verifier::accept_recursive_types(V)]
 pub tracked struct StorageResource<K, V, P> {
     _p: core::marker::PhantomData<(K, V, P)>,
-    _send_sync: super::state_machine_internal::SyncSendIfSyncSend<Map<K, V>>,
+    _send_sync: super::super::state_machine_internal::SyncSendIfSyncSend<Map<K, V>>,
 }
 
 /// See [`StorageResource`] for more information.
@@ -241,7 +241,7 @@ impl<K, V, P: Protocol<K, V>> StorageResource<K, V, P> {
         P::op_unit(p.value());
         let tracked (selff, unit) = p.split(p.value(), P::unit());
         let new_values0 = set_op(new_values, P::unit());
-        super::set_lib::assert_sets_equal!(new_values0, new_values, v => {
+        super::super::set_lib::assert_sets_equal!(new_values0, new_values, v => {
             P::op_unit(v.0);
             if new_values.contains(v) {
                 assert(new_values0.contains(v));
