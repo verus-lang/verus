5587aae7d964175fd76f68265ac828e6fe60051e
Bryan Parno <parno@cmu.edu>
2026-02-01 17:12:05 -0500

    Add a test case that succeeds and a test case that fails.

--- examples/iter_notes/iters/testcase.rs
+++ examples/iter_notes/iters/testcase.rs
@@ -18,6 +18,63 @@ fn test_basic() {
     assert(w@ == v@);
 }
 
+/*
+fn all_positive(v: &Vec<u8>) -> (b: bool)
+    ensures
+        b <==> (forall|i: int| 0 <= i < v.len() ==> v[i] > 0),
+{
+    let mut b: bool = true;
+
+    for x in iter: v
+        invariant
+            b <==> (forall|i: int| 0 <= i < iter.index@ ==> v[i] > 0),
+    {
+        b = b && *x > 0;
+    }
+    b
+}
+*/
+
+fn test() {
+    let mut v1: Vec<u32> = Vec::new();
+    let mut v2: Vec<u32> = Vec::new();
+    v1.push(3);
+    v1.push(4);
+    assert(v1@ == seq![3u32, 4u32]);
+
+    v2.push(5);
+    assert(v2.len() == 1);
+    v2.push(7);
+    assert(v2@.len() == 2);
+    v2.insert(1, 6);
+    assert(v2@ == seq![5u32, 6u32, 7u32]);
+
+    v1.append(&mut v2);
+    assert(v2@.len() == 0);
+    assert(v1@.len() == 5);
+    assert(v1@ == seq![3u32, 4u32, 5u32, 6u32, 7u32]);
+    v1.remove(2);
+    assert(v1@ == seq![3u32, 4u32, 6u32, 7u32]);
+
+    v1.push(8u32);
+    v1.push(9u32);
+    assert(v1@ == seq![3u32, 4u32, 6u32, 7u32, 8u32, 9u32]);
+
+    v1.swap_remove(5);
+    assert(v1@ == seq![3u32, 4u32, 6u32, 7u32, 8u32]);
+
+    let mut i: usize = 0;
+    for x in it: v1
+        invariant
+            i == it.index@,
+            it.seq() == seq![3u32, 4u32, 6u32, 7u32, 8u32],
+    {
+        assert(x > 2);
+        assert(x < 10);
+        i = i + 1;
+    }
+}
+
 spec fn sum_u8(s: Seq<u8>) -> nat 
     decreases s.len(),
 {
