5322f42255f8ac4fa7d9f12cc1c5de5f52d2680b
Jay Lorch <jaylorch@gmail.com>
2026-02-02 10:49:12 -0800

    Fix specifications as recommended by AI (#2126)
    * Move unsigned saturating ops into modname_u
    * Implement checked div and rem for all integers, not just 32-bit
    * Fix typo in axiom name

--- source/vstd/std_specs/num.rs
+++ source/vstd/std_specs/num.rs
@@ -171,6 +171,48 @@ macro_rules! num_specs {
                     result == checked_div(lhs, rhs);
 
             #[verifier::allow_in_spec]
+            pub assume_specification[<$uN>::checked_rem](lhs: $uN, rhs: $uN) -> Option<$uN>
+                returns (
+                    if rhs == 0 {
+                        None
+                    }
+                    else {
+                        Some((lhs % rhs) as $uN)
+                    }
+                );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$uN>::checked_rem_euclid](lhs: $uN, rhs: $uN) -> Option<$uN>
+                returns (
+                    if rhs == 0 {
+                        None
+                    }
+                    else {
+                        Some((lhs % rhs) as $uN)
+                    }
+                );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$uN>::saturating_add](x: $uN, y: $uN) -> $uN
+                returns (
+                    if x + y > <$uN>::MAX {
+                        <$uN>::MAX
+                    } else {
+                        (x + y) as $uN
+                    }
+                );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$uN>::saturating_sub](x: $uN, y: $uN) -> $uN
+                returns (
+                    if x - y < <$uN>::MIN {
+                        <$uN>::MIN
+                    } else {
+                        (x - y) as $uN
+                    }
+                );
+
+            #[verifier::allow_in_spec]
             pub assume_specification[<$uN>::is_multiple_of](x: $uN, y: $uN) -> bool
                 returns (
                     if y == 0 { x == 0 } else { x % y == 0 }
@@ -310,26 +352,6 @@ macro_rules! num_specs {
                 );
 
             #[verifier::allow_in_spec]
-            pub assume_specification[<$uN>::saturating_add](x: $uN, y: $uN) -> $uN
-                returns (
-                    if x + y > <$uN>::MAX {
-                        <$uN>::MAX
-                    } else {
-                        (x + y) as $uN
-                    }
-                );
-
-            #[verifier::allow_in_spec]
-            pub assume_specification[<$uN>::saturating_sub](x: $uN, y: $uN) -> $uN
-                returns (
-                    if x - y < <$uN>::MIN {
-                        <$uN>::MIN
-                    } else {
-                        (x - y) as $uN
-                    }
-                );
-
-            #[verifier::allow_in_spec]
             pub assume_specification[<$iN>::checked_sub](x: $iN, y: $iN) -> Option<$iN>
                 returns (
                     if x - y > <$iN>::MAX || x - y < <$iN>::MIN {
@@ -358,6 +380,90 @@ macro_rules! num_specs {
                         Some((x * y) as $iN)
                     }
                 );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$iN>::checked_div](lhs: $iN, rhs: $iN) -> Option<$iN>
+                returns (
+                    if rhs == 0 {
+                        None
+                    }
+                    else {
+                        let x = lhs as int;
+                        let d = rhs as int;
+                        let output = if x == 0 {
+                            0
+                        } else if x > 0 && d > 0 {
+                            x / d
+                        } else if x < 0 && d < 0 {
+                            ((x * -1) / (d * -1))
+                        } else if x < 0 {
+                            ((x * -1) / d) * -1
+                        } else {  // d < 0
+                            (x / (d * -1)) * -1
+                        };
+                        if output < <$iN>::MIN || output > <$iN>::MAX {
+                            None
+                        } else {
+                            Some(output as $iN)
+                        }
+                    }
+                );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$iN>::checked_div_euclid](lhs: $iN, rhs: $iN) -> Option<$iN>
+                returns (
+                    if rhs == 0 {
+                        None
+                    }
+                    else if <$iN>::MIN <= lhs / rhs <= <$iN>::MAX {
+                        Some((lhs / rhs) as $iN)
+                    }
+                    else {
+                        None
+                    }
+                );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$iN>::checked_rem](lhs: $iN, rhs: $iN) -> Option<$iN>
+                returns (
+                    if rhs == 0 {
+                        None
+                    }
+                    else {
+                        let x = lhs as int;
+                        let d = rhs as int;
+                        let output = if x == 0 {
+                            0
+                        } else if x > 0 && d > 0 {
+                            x % d
+                        } else if x < 0 && d < 0 {
+                            ((x * -1) % (d * -1)) * -1
+                        } else if x < 0 {
+                            ((x * -1) % d) * -1
+                        } else {  // d < 0
+                            x % (d * -1)
+                        };
+                        if output < <$iN>::MIN || output > <$iN>::MAX {
+                            None
+                        } else {
+                            Some(output as $iN)
+                        }
+                    }
+                );
+
+            #[verifier::allow_in_spec]
+            pub assume_specification[<$iN>::checked_rem_euclid](lhs: $iN, rhs: $iN) -> Option<$iN>
+                returns (
+                    if rhs == 0 {
+                        None
+                    }
+                    else if <$iN>::MIN <= lhs % rhs <= <$iN>::MAX {
+                        Some((lhs % rhs) as $iN)
+                    }
+                    else {
+                        None
+                    }
+                );
         }
 
         }
@@ -370,86 +476,3 @@ num_specs!(u32, i32, u32_specs, i32_specs, 0x1_0000_0000);
 num_specs!(u64, i64, u64_specs, i64_specs, 0x1_0000_0000_0000_0000);
 num_specs!(u128, i128, u128_specs, i128_specs, 0x1_0000_0000_0000_0000_0000_0000_0000_0000);
 num_specs!(usize, isize, usize_specs, isize_specs, (usize::MAX - usize::MIN + 1));
-
-verus! {
-
-// TODO move all these into the num_specs! macro to handle them for other integer widths
-// == u32 methods ==
-pub assume_specification[ u32::checked_rem ](lhs: u32, rhs: u32) -> (result: Option<u32>)
-    ensures
-        rhs == 0 ==> result is None,
-        rhs != 0 ==> result == Some((lhs % rhs) as u32),
-;
-
-pub assume_specification[ u32::checked_rem_euclid ](lhs: u32, rhs: u32) -> (result: Option<u32>)
-    ensures
-        rhs == 0 ==> result is None,
-        rhs != 0 ==> result == Some((lhs % rhs) as u32),
-;
-
-// == i32 methods ==
-pub assume_specification[ i32::checked_div ](lhs: i32, rhs: i32) -> (result: Option<i32>)
-    ensures
-        rhs == 0 ==> result is None,
-        ({
-            let x = lhs as int;
-            let d = rhs as int;
-            let output = if x == 0 {
-                0
-            } else if x > 0 && d > 0 {
-                x / d
-            } else if x < 0 && d < 0 {
-                ((x * -1) / (d * -1))
-            } else if x < 0 {
-                ((x * -1) / d) * -1
-            } else {  // d < 0
-                (x / (d * -1)) * -1
-            };
-            if output < i32::MIN || output > i32::MAX {
-                result is None
-            } else {
-                result == Some(output as i32)
-            }
-        }),
-;
-
-pub assume_specification[ i32::checked_div_euclid ](lhs: i32, rhs: i32) -> (result: Option<i32>)
-    ensures
-        rhs == 0 ==> result is None,
-        lhs / rhs < i32::MIN || lhs / rhs > i32::MAX ==> result is None,
-        i32::MIN <= lhs / rhs <= i32::MAX ==> result == Some((lhs / rhs) as i32),
-;
-
-pub assume_specification[ i32::checked_rem ](lhs: i32, rhs: i32) -> (result: Option<i32>)
-    ensures
-        rhs == 0 ==> result is None,
-        ({
-            let x = lhs as int;
-            let d = rhs as int;
-            let output = if x == 0 {
-                0
-            } else if x > 0 && d > 0 {
-                x % d
-            } else if x < 0 && d < 0 {
-                ((x * -1) % (d * -1)) * -1
-            } else if x < 0 {
-                ((x * -1) % d) * -1
-            } else {  // d < 0
-                x % (d * -1)
-            };
-            if output < i32::MIN || output > i32::MAX {
-                result is None
-            } else {
-                result == Some(output as i32)
-            }
-        }),
-;
-
-pub assume_specification[ i32::checked_rem_euclid ](lhs: i32, rhs: i32) -> (result: Option<i32>)
-    ensures
-        rhs == 0 ==> result is None,
-        lhs % rhs < i32::MIN || lhs % rhs > i32::MAX ==> result is None,
-        i32::MIN <= lhs % rhs <= i32::MAX ==> result == Some((lhs % rhs) as i32),
-;
-
-} // verus!
