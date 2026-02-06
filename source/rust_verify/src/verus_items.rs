9f236d7fc7c43ddbd8f9af6115e00b67d37193b8
Elanor Tang <elanor@umich.edu>
2026-02-06 14:21:26 -0500

    remove `RustItem::Allocator`

--- source/rust_verify/src/verus_items.rs
+++ source/rust_verify/src/verus_items.rs
@@ -711,7 +711,6 @@ pub(crate) enum RustItem {
     CloneClone,
     CloneFrom,
     IntIntrinsic(RustIntIntrinsicItem),
-    Allocator,
     TryTraitBranch,
     ResidualTraitFromResidual,
     IntoIterFn,
@@ -824,9 +823,6 @@ pub(crate) fn get_rust_item_str(rust_path: Option<&str>) -> Option<RustItem> {
         return Some(RustItem::CloneFrom);
     }
 
-    if rust_path == Some("core::alloc::Allocator") {
-        return Some(RustItem::Allocator);
-    }
     if rust_path == Some("core::slice::index::private_slice_index::Sealed") {
         return Some(RustItem::SliceSealed);
     }
