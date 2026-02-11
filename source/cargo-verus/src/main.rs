5732a4103b9bee4ad130fb05b237f5e88a23a383
Jay Lorch <lorch@microsoft.com>
2026-02-10 16:21:05 -0800

    Modularize implementation better

--- source/cargo-verus/src/main.rs
+++ source/cargo-verus/src/main.rs
@@ -20,58 +20,18 @@ mod subcommands;
 pub mod test_utils;
 
 use crate::{
-    cli::{CargoOptions, CargoVerusCli, VerusSubcommand},
+    cli::{CargoVerusCli, VerusSubcommand},
     subcommands::CargoRunConfig,
 };
-
-fn has_late_verus_arg(opts: &CargoOptions) -> bool {
-    for arg in opts.cargo_args.iter() {
-        if arg.starts_with("-Z") && arg.len() > 2 {
-            eprintln!(
-                "Break the command-line argument {0} into two, by using a space after -Z, so that Verus sees it.",
-                arg
-            );
-            return true;
-        }
-    }
-    for arg in opts.cargo_args.iter().skip(1) {
-        if arg.starts_with("-p")
-            || arg == "--package"
-            || arg.starts_with("--package=")
-            || arg == "--workspace"
-            || arg == "--all"
-            || arg == "--exclude"
-            || arg.starts_with("--exclude=")
-            || arg == "--manifest-path"
-            || arg.starts_with("--manifest-path=")
-            || arg == "--all-features"
-            || arg == "--no-default-features"
-            || arg == "--features"
-            || arg.starts_with("--features=")
-            || arg == "--frozen"
-            || arg == "--locked"
-            || arg == "--offline"
-            || arg == "--target-dir"
-            || arg.starts_with("--target-dir=")
-            || arg == "--config"
-            || arg.starts_with("--config=")
-            || arg.starts_with("-Z")
-        {
-            eprintln!(
-                "The Verus-relevant command-line argument {0} can't follow the Verus-irrelevant argument {1} because that will cause the Verus-relevant argument to be ignored. To fix this, place Verus-relevant cargo options (like --package, --features, --manifest-path) before any Verus-irrelevant ones.",
-                arg, opts.cargo_args[0]
-            );
-            return true;
-        }
-    }
-
-    false
-}
 
 pub fn main() -> Result<ExitCode> {
     let normalized_args: Vec<_> = normalize_args(env::args()).collect();
     let parsed_cli =
         CargoVerusCli::parse_from(normalized_args.iter().cloned()).clap_trailing_args_hotfix();
+
+    if parsed_cli.has_late_verus_argument() {
+        return Ok(ExitCode::from(2));
+    }
 
     let cfg = match parsed_cli.command {
         VerusSubcommand::New(new_cmd) => {
@@ -82,54 +42,34 @@ pub fn main() -> Result<ExitCode> {
             }
             return Ok(ExitCode::SUCCESS);
         }
-        VerusSubcommand::Verify(options) => {
-            if has_late_verus_arg(&options.cargo_opts) {
-                return Ok(ExitCode::from(2));
-            }
-            CargoRunConfig {
-                subcommand: "build",
-                options,
-                compile_primary: false,
-                verify_deps: true,
-                warn_if_nothing_verified: true,
-            }
-        }
-        VerusSubcommand::Focus(options) => {
-            if has_late_verus_arg(&options.cargo_opts) {
-                return Ok(ExitCode::from(2));
-            }
-            CargoRunConfig {
-                subcommand: "build",
-                options,
-                compile_primary: false,
-                verify_deps: false,
-                warn_if_nothing_verified: true,
-            }
-        }
-        VerusSubcommand::Build(options) => {
-            if has_late_verus_arg(&options.cargo_opts) {
-                return Ok(ExitCode::from(2));
-            }
-            CargoRunConfig {
-                subcommand: "build",
-                options,
-                compile_primary: true,
-                verify_deps: true,
-                warn_if_nothing_verified: false,
-            }
-        }
-        VerusSubcommand::Check(options) => {
-            if has_late_verus_arg(&options.cargo_opts) {
-                return Ok(ExitCode::from(2));
-            }
-            CargoRunConfig {
-                subcommand: "check",
-                options,
-                compile_primary: false,
-                verify_deps: true,
-                warn_if_nothing_verified: true,
-            }
-        }
+        VerusSubcommand::Verify(options) => CargoRunConfig {
+            subcommand: "build",
+            options,
+            compile_primary: false,
+            verify_deps: true,
+            warn_if_nothing_verified: true,
+        },
+        VerusSubcommand::Focus(options) => CargoRunConfig {
+            subcommand: "build",
+            options,
+            compile_primary: false,
+            verify_deps: false,
+            warn_if_nothing_verified: true,
+        },
+        VerusSubcommand::Build(options) => CargoRunConfig {
+            subcommand: "build",
+            options,
+            compile_primary: true,
+            verify_deps: true,
+            warn_if_nothing_verified: false,
+        },
+        VerusSubcommand::Check(options) => CargoRunConfig {
+            subcommand: "check",
+            options,
+            compile_primary: false,
+            verify_deps: true,
+            warn_if_nothing_verified: true,
+        },
     };
 
     subcommands::run_cargo(cfg)
