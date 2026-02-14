a0df61a055bb1685ae9548309aff48f68a213b59
Mate Kovacs <mkovaxx@gmail.com>
2026-02-15 00:47:06 +0900

    extend tests to check the JSON output

--- source/rust_verify_test/tests/output_json.rs
+++ source/rust_verify_test/tests/output_json.rs
@@ -17,7 +17,12 @@ test_verify_one_file_with_options! {
         fn caller() {
             let _ = example(1, 2); // precondition fails
         }
-    } => Err(err) => assert_help_error_msg(err, "note: Property 732")
+    } => Err(err) => {
+        assert_help_error_msg(err.clone(), "note: Property 732");
+        assert_json_func_details(&err, "crate::caller", |details| {
+            details.failed_proof_notes.contains("Property 732")
+        });
+    }
 }
 
 test_verify_one_file_with_options! {
@@ -34,7 +39,12 @@ test_verify_one_file_with_options! {
         fn caller() {
             let _ = example(1, 2); // postcondition fails
         }
-    } => Err(err) => assert_help_error_msg(err, "note: Property 732")
+    } => Err(err) => {
+        assert_help_error_msg(err.clone(), "note: Property 732");
+        assert_json_func_details(&err, "crate::example", |details| {
+            details.failed_proof_notes.contains("Property 732")
+        });
+    }
 }
 
 test_verify_one_file_with_options! {
@@ -46,7 +56,12 @@ test_verify_one_file_with_options! {
                 (1 > 2)
             ); // assertion fails
         }
-    } => Err(err) => assert_help_error_msg(err, "note: Statement known to be false")
+    } => Err(err) => {
+        assert_help_error_msg(err.clone(), "note: Statement known to be false");
+        assert_json_func_details(&err, "crate::caller", |details| {
+            details.failed_proof_notes.contains("Statement known to be false")
+        });
+    }
 }
 
 test_verify_one_file_with_options! {
@@ -59,5 +74,35 @@ test_verify_one_file_with_options! {
                 (1 > 2)
             ); // assumption fails
         }
-    } => Err(err) => assert_help_error_msg(err, "note: Statement known to be false")
+    } => Err(err) => {
+        assert_help_error_msg(err.clone(), "note: Statement known to be false");
+        // TODO: This doesn't work, because `--no-cheating` mode prevents JSON output.
+        // assert_json_func_details(&err, "crate::caller", |details| {
+        //     details.failed_proof_notes.contains("Statement known to be false")
+        // });
+    }
+}
+
+fn assert_json_func_details(err: &TestErr, func: &str, predicate: impl Fn(&FuncDetails) -> bool) {
+    let json = err.json_output.as_ref().expect("expected JSON summary output");
+
+    dbg!(json);
+
+    let func_details = json
+        .get("func-details")
+        .and_then(|v| v.as_object())
+        .expect("`func-details` missing or not an object");
+
+    let details_json =
+        func_details.get(func).expect(&format!("func-details has no entry for `{func}`"));
+
+    let details =
+        serde_json::from_value(details_json.clone()).expect("failed to deserialize FuncDetails");
+
+    assert!(predicate(&details));
+}
+
+#[derive(serde::Deserialize)]
+pub struct FuncDetails {
+    pub failed_proof_notes: std::collections::HashSet<String>,
 }
