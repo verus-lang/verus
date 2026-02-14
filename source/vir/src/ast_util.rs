d46f89d771e41ec4eb870a1165b96df6745fdec7
Mate Kovacs <mkovaxx@gmail.com>
2026-02-14 15:46:51 +0900

    improve comment

--- source/vir/src/ast_util.rs
+++ source/vir/src/ast_util.rs
@@ -905,7 +905,7 @@ pub fn wrap_in_trigger(expr: &Expr) -> Expr {
 
 pub(crate) fn ast_expr_get_proof_note(expr: &Expr) -> Option<Arc<String>> {
     match &expr.x {
-        // NOTE: `UnaryOpr::Box` and `Unbox` are not used here; only in `ast_to_sst`.
+        // NOTE: `UnaryOpr::Box` and `Unbox` not relevant; only introduced later in `ast_to_sst`.
         ExprX::UnaryOpr(UnaryOpr::CustomErr(_), e) => ast_expr_get_proof_note(e),
         ExprX::UnaryOpr(UnaryOpr::ProofNote(s), _) => Some(s.clone()),
         _ => None,
