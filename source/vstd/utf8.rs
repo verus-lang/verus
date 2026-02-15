f4e05df237cc9d7bb7688187c40c4b7c1dcd497c
zero-to-nat <nneamtu@andrew.cmu.edu>
2026-02-15 11:51:30 -0500

    complete proofs that decode_utf8 and encode_utf8 are inverses

--- source/vstd/utf8.rs
+++ source/vstd/utf8.rs
@@ -3,7 +3,7 @@ use super::seq::*;
 
 verus! {
 
-broadcast use crate::group_vstd_default;
+broadcast use super::seq::group_seq_axioms;
 
 /// byte has the form 0xxxxxxx
 pub open spec fn is_leading_1(byte: u8) -> bool {
@@ -13,7 +13,7 @@ pub open spec fn is_leading_1(byte: u8) -> bool {
 /// byte has the form 110xxxxx
 /// 0xe0 = 1110 0000
 /// 0xc0 = 1100 0000
-/// excluding 0xc0 and 0xc1 is taken care of by valid_scalar check
+/// excluding 0xc0 and 0xc1 is taken care of by valid_scalar_of_len check
 pub open spec fn is_leading_2(byte: u8) -> bool {
     byte & 0xe0 == 0xc0
 }
@@ -28,7 +28,7 @@ pub open spec fn is_leading_3(byte: u8) -> bool {
 /// byte has the form 11110xxx
 /// 0xf8 = 1111 1000
 /// 0xf0 = 1111 0000
-/// excluding 0xf5, 0xf6, 0xf7 is taken care of by valid_scalar check
+/// excluding 0xf5, 0xf6, 0xf7 is taken care of by valid_scalar_of_len check
 pub open spec fn is_leading_4(byte: u8) -> bool {
     byte & 0xf8 == 0xf0
 }
@@ -202,7 +202,8 @@ pub open spec fn first_codepoint(bytes: Seq<u8>) -> Seq<u8> {
 }
 
 /// Is the scalar value allowed for the given byte-length?
-pub open spec fn valid_scalar(scalar: u32, len: int) -> bool {
+// todo -- should this have a check for len 1?
+pub open spec fn valid_scalar_of_len(scalar: u32, len: int) -> bool {
     // Require that it's not an 'overlong encoding'
     (len == 2 ==> scalar >= 0x80) && (len == 3 ==> scalar >= 0x800) && (len == 4 ==> scalar
         >= 0x10000 && scalar
@@ -214,7 +215,7 @@ pub open spec fn valid_scalar(scalar: u32, len: int) -> bool {
 /// Is the first codepoint valid?
 /// Requires both a valid bit-encoding and a valid scalar value.
 pub open spec fn valid_first_codepoint(bytes: Seq<u8>) -> bool {
-    valid_first_bit_encoding(bytes) && valid_scalar(
+    valid_first_bit_encoding(bytes) && valid_scalar_of_len(
         scalar_of_first_codepoint(bytes),
         length_of_first_codepoint(bytes),
     )
@@ -241,27 +242,32 @@ pub open spec fn decode_utf8(bytes: Seq<u8>) -> Seq<char>
     }
 }
 
-/// scalar has the form 0xxxxxxx (where the leading bits are zeros)
+/// True when the given `u32` represents a UTF-8 codepoint that is 1 byte long.
+/// The value must have the form 0xxxxxxx (where the leading bytes are zeros).
+/// todo - redo these comments maybe?
 pub open spec fn is_1_byte_codepoint(c: u32) -> bool {
     0 <= c <= 0x7F
 }
 
-/// scalar has the form 00000xxx xxxxxxxx (where the leading bits are zeros)
+/// True when the given `u32` represents a UTF-8 codepoint that is 2 bytes long.
+/// The value must have the form 00000xxx xxxxxxxx (where the leading bytes are zeros).
 pub open spec fn is_2_byte_codepoint(c: u32) -> bool {
     0x80 <= c <= 0x7FF
 }
 
-/// scalar has the form xxxxxxxx xxxxxxxx (where the leading bits are zeros)
+/// True when the given `u32` represents a UTF-8 codepoint that is 3 bytes long.
+/// The value must have the form xxxxxxxx xxxxxxxx (where the leading bytes are zeros) and not be in the range 0xD800 to 0xDFFF.
 pub open spec fn is_3_byte_codepoint(c: u32) -> bool {
     0x800 <= c <= 0xFFFF && !(0xD800 <= c <= 0xDFFF)
 }
 
-/// scalar has the form 000x0000 xxxxxxxx xxxxxxxx (where the leading bits are zeros)
-// TODO
+/// True when the given `u32` represents a UTF-8 codepoint that is 4 bytes long.
+/// The value must have the form 000x0000 xxxxxxxx xxxxxxxx (where the leading bytes are zeros).
 pub open spec fn is_4_byte_codepoint(c: u32) -> bool {
     0x10000 <= c <= 0x10FFFF
 }
 
+/// Converts a `u32` which represents a 1-byte UTF-8 codepoint to the byte representation of that codepoint.
 pub open spec fn first_byte_1_byte_codepoint(c: u32) -> u8 
     recommends
         is_1_byte_codepoint(c)
@@ -269,6 +275,7 @@ pub open spec fn first_byte_1_byte_codepoint(c: u32) -> u8
     (c & 0x7F) as u8
 }
 
+/// Converts a `u32` which represents a 2-byte UTF-8 codepoint to the first byte of that codepoint.
 pub open spec fn first_byte_2_byte_codepoint(c: u32) -> u8 
     recommends
         is_2_byte_codepoint(c)
@@ -276,6 +283,7 @@ pub open spec fn first_byte_2_byte_codepoint(c: u32) -> u8
     0xC0 | ((c >> 6) & 0x1F) as u8
 }
 
+/// Converts a `u32` which represents a 3-byte UTF-8 codepoint to the first byte of that codepoint.
 pub open spec fn first_byte_3_byte_codepoint(c: u32) -> u8 
     recommends
         is_3_byte_codepoint(c)
@@ -283,6 +291,7 @@ pub open spec fn first_byte_3_byte_codepoint(c: u32) -> u8
     0xE0 | ((c >> 12) & 0x0F) as u8
 }
 
+/// Converts a `u32` which represents a 4-byte UTF-8 codepoint to the first byte of that codepoint.
 pub open spec fn first_byte_4_byte_codepoint(c: u32) -> u8 
     recommends
         is_4_byte_codepoint(c)
@@ -290,6 +299,7 @@ pub open spec fn first_byte_4_byte_codepoint(c: u32) -> u8
     0xF0 | ((c >> 18) & 0x7) as u8
 }
 
+/// Converts a `u32` which represents a 2-byte, 3-byte, or 4-byte UTF-8 codepoint to the last continuation byte of that codepoint.
 pub open spec fn last_continuation_byte(c: u32) -> u8 
     recommends
         is_2_byte_codepoint(c) || is_3_byte_codepoint(c) || is_4_byte_codepoint(c)
@@ -297,6 +307,7 @@ pub open spec fn last_continuation_byte(c: u32) -> u8
     0x80 | (c & 0x3F) as u8
 }
 
+/// Converts a `u32` which represents a 3-byte or 4-byte UTF-8 codepoint to the second-last continuation byte of that codepoint.
 pub open spec fn second_last_continuation_byte(c: u32) -> u8 
     recommends
         is_3_byte_codepoint(c) || is_4_byte_codepoint(c)
@@ -304,6 +315,7 @@ pub open spec fn second_last_continuation_byte(c: u32) -> u8
     0x80 | ((c >> 6) & 0x3F) as u8
 }
 
+/// Converts a `u32` which represents a 4-byte UTF-8 codepoint to the third-last continuation byte of that codepoint.
 pub open spec fn third_last_continuation_byte(c: u32) -> u8 
     recommends
         is_4_byte_codepoint(c)
@@ -311,13 +323,15 @@ pub open spec fn third_last_continuation_byte(c: u32) -> u8
     0x80 | ((c >> 12) & 0x3F) as u8
 }
 
-pub open spec fn valid_codepoint(c: u32) -> bool {
-    0 < c <= 0x10ffff && !(0xD800 <= c <= 0xDFFF)
+/// True when the given `u32` can be encoded as UTF-8.
+pub open spec fn valid_scalar(c: u32) -> bool {
+    0 <= c <= 0x10ffff && !(0xD800 <= c <= 0xDFFF)
 }
 
+/// Converts a `u32` to the codepoint of its UTF-8 representation.
 pub open spec fn codepoint_of_scalar(c: u32) -> Seq<u8> 
     recommends
-        valid_codepoint(c)
+        valid_scalar(c)
 {
     if is_1_byte_codepoint(c) {
         seq![first_byte_1_byte_codepoint(c)]
@@ -330,6 +344,7 @@ pub open spec fn codepoint_of_scalar(c: u32) -> Seq<u8>
     }
 }
 
+/// Converts a sequence of `char`s to its UTF-8 representation.
 pub open spec fn encode_utf8(chars: Seq<char>) -> Seq<u8> 
     decreases chars.len()
 {
@@ -356,9 +371,9 @@ pub open spec fn is_char_boundary(bytes: Seq<u8>, index: int) -> bool
     }
 }
 
-// ============================================================================
-// Lemmas for proving decode_utf8 and encode_utf8 are inverses
-// ============================================================================
+/* encode_utf8 and decode_utf8 correspondence */
+
+// codepoint_of_scalar((scalar_of_codepoint_1(bytes[0]) as char) as u32)[0] == bytes[0]
 
 proof fn codepoint_of_scalar_scalar_of_codepoint_1_byte(c: u32) by (bit_vector)
     requires
@@ -368,6 +383,19 @@ proof fn codepoint_of_scalar_scalar_of_codepoint_1_byte(c: u32) by (bit_vector)
             let b1 = first_byte_1_byte_codepoint(c);
             &&& is_leading_1(b1)
             &&& scalar_of_codepoint_1(b1) == c
+        })
+{}
+
+proof fn scalar_of_codepoint_codepoint_of_scalar_1_byte(b1: u8) by (bit_vector)
+    requires
+        is_leading_1(b1),
+        valid_scalar_of_len(scalar_of_codepoint_1(b1), 1)
+    ensures
+        ({
+            let c = scalar_of_codepoint_1(b1);
+            &&& valid_scalar(c)
+            &&& is_1_byte_codepoint(c)
+            &&& first_byte_1_byte_codepoint(c) == b1
         })
 {}
 
@@ -385,6 +413,21 @@ proof fn codepoint_of_scalar_scalar_of_codepoint_2_byte(c: u32) by (bit_vector)
         })
 {}
 
+proof fn scalar_of_codepoint_codepoint_of_scalar_2_byte(b1: u8, b2: u8) by (bit_vector)
+    requires
+        is_leading_2(b1),
+        is_continuation(b2),
+        valid_scalar_of_len(scalar_of_codepoint_2(b1, b2), 2)
+    ensures
+        ({
+            let c = scalar_of_codepoint_2(b1, b2);
+            &&& valid_scalar(c)
+            &&& is_2_byte_codepoint(c)
+            &&& first_byte_2_byte_codepoint(c) == b1
+            &&& last_continuation_byte(c) == b2
+        })
+{}
+
 proof fn codepoint_of_scalar_scalar_of_codepoint_3_byte(c: u32) by (bit_vector)
     requires
         is_3_byte_codepoint(c)
@@ -399,6 +442,23 @@ proof fn codepoint_of_scalar_scalar_of_codepoint_3_byte(c: u32) by (bit_vector)
             &&& is_continuation(b2)
             &&& is_continuation(b3)
             &&& scalar_of_codepoint_3(b1, b2, b3) == c
+        })
+{}
+
+proof fn scalar_of_codepoint_codepoint_of_scalar_3_byte(b1: u8, b2: u8, b3: u8) by (bit_vector)
+    requires
+        is_leading_3(b1),
+        is_continuation(b2),
+        is_continuation(b3),
+        valid_scalar_of_len(scalar_of_codepoint_3(b1, b2, b3), 3)
+    ensures
+        ({
+            let c = scalar_of_codepoint_3(b1, b2, b3);
+            &&& valid_scalar(c)
+            &&& is_3_byte_codepoint(c)
+            &&& first_byte_3_byte_codepoint(c) == b1
+            &&& second_last_continuation_byte(c) == b2
+            &&& last_continuation_byte(c) == b3
         })
 {}
 
@@ -422,31 +482,39 @@ proof fn codepoint_of_scalar_scalar_of_codepoint_4_byte(c: u32) by (bit_vector)
         })
 {}
 
-proof fn lemma_codepoint_scalar_roundtrip(c: u32)
+proof fn scalar_of_codepoint_codepoint_of_scalar_4_byte(b1: u8, b2: u8, b3: u8, b4: u8) by (bit_vector)
     requires
-        valid_codepoint(c)
+        is_leading_4(b1),
+        is_continuation(b2),
+        is_continuation(b3),
+        is_continuation(b4),
+        valid_scalar_of_len(scalar_of_codepoint_4(b1, b2, b3, b4), 4)
     ensures
-        scalar_of_first_codepoint(codepoint_of_scalar(c)) == c,
-        length_of_first_codepoint(codepoint_of_scalar(c)) == codepoint_of_scalar(c).len(),
-        valid_first_bit_encoding(codepoint_of_scalar(c)),
-        valid_scalar(c, codepoint_of_scalar(c).len() as int)
-{
-    if is_1_byte_codepoint(c) {
-        codepoint_of_scalar_scalar_of_codepoint_1_byte(c);
-    } else if is_2_byte_codepoint(c) {
-        codepoint_of_scalar_scalar_of_codepoint_2_byte(c);
-    } else if is_3_byte_codepoint(c) {
-        codepoint_of_scalar_scalar_of_codepoint_3_byte(c);
-    } else {
-        codepoint_of_scalar_scalar_of_codepoint_4_byte(c);
-    }
-}
+        ({
+            let c = scalar_of_codepoint_4(b1, b2, b3, b4);
+            &&& valid_scalar(c)
+            &&& is_4_byte_codepoint(c)
+            &&& first_byte_4_byte_codepoint(c) == b1
+            &&& third_last_continuation_byte(c) == b2
+            &&& second_last_continuation_byte(c) == b3
+            &&& last_continuation_byte(c) == b4
+        })
+{}
 
-pub broadcast axiom fn char_is_valid_codepoint(c: char)
+pub broadcast proof fn char_is_valid_scalar(c: char)
     ensures
-        #[trigger] valid_codepoint(c as u32)
-    ;
+        #[trigger] valid_scalar(c as u32)
+{}
 
+pub broadcast proof fn char_u32_cast(c: char, u: u32)  
+    requires
+        u == c as u32
+    ensures
+        #![trigger c as u32, u as char]
+        u as char == c
+{}
+
+/// Properties of the first codepoint of the result of `encode_utf8`.
 pub proof fn encode_utf8_first_codepoint(chars: Seq<char>)
     requires
         chars.len() > 0
@@ -454,13 +522,21 @@ pub proof fn encode_utf8_first_codepoint(chars: Seq<char>)
         scalar_of_first_codepoint(encode_utf8(chars)) == chars[0] as u32,
         length_of_first_codepoint(encode_utf8(chars)) == codepoint_of_scalar(chars[0] as u32).len(),
         valid_first_codepoint(encode_utf8(chars)),
-        pop_first_codepoint(encode_utf8(chars)) =~= encode_utf8(chars.drop_first())
 {
-    let bytes = encode_utf8(chars);
-    char_is_valid_codepoint(chars[0]);
-    lemma_codepoint_scalar_roundtrip(chars[0] as u32);
+    char_is_valid_scalar(chars[0]);
+    let s = chars[0] as u32;
+    if is_1_byte_codepoint(s) {
+        codepoint_of_scalar_scalar_of_codepoint_1_byte(s);
+    } else if is_2_byte_codepoint(s) {
+        codepoint_of_scalar_scalar_of_codepoint_2_byte(s);
+    } else if is_3_byte_codepoint(s) {
+        codepoint_of_scalar_scalar_of_codepoint_3_byte(s);
+    } else {
+        codepoint_of_scalar_scalar_of_codepoint_4_byte(s);
+    }
 }
 
+/// Ensures the result of `encode_utf8` always satisfies `valid_utf8`.
 pub broadcast proof fn encode_utf8_valid_utf8(chars: Seq<char>)
     ensures valid_utf8(#[trigger] encode_utf8(chars))
     decreases chars.len()
@@ -474,8 +550,8 @@ pub broadcast proof fn encode_utf8_valid_utf8(chars: Seq<char>)
     }
 }
 
-/// Main theorem: decode_utf8(encode_utf8(chars)) == chars
-pub proof fn theorem_decode_encode_inverse(chars: Seq<char>)
+/// Ensures that performing `encode_utf8` followed by `decode_utf8` results in the original `char` sequence.
+pub proof fn encode_utf8_decode_utf8(chars: Seq<char>)
     ensures
         decode_utf8(encode_utf8(chars)) == chars,
     decreases chars.len(),
@@ -485,10 +561,78 @@ pub proof fn theorem_decode_encode_inverse(chars: Seq<char>)
     } else {
         let bytes = encode_utf8(chars);
         encode_utf8_first_codepoint(chars);
-        assume(scalar_of_first_codepoint(bytes) as char == chars[0]); // todo
+        char_u32_cast(chars[0], scalar_of_first_codepoint(bytes));
 
+        assert(pop_first_codepoint(bytes) =~= encode_utf8(chars.drop_first()));
         let rest = chars.drop_first();
-        theorem_decode_encode_inverse(chars.drop_first());
+        encode_utf8_decode_utf8(rest);
+    }
+}
+
+pub proof fn decode_utf8_first_codepoint(bytes: Seq<u8>)
+    requires
+        valid_utf8(bytes),
+        bytes.len() > 0
+    ensures
+        codepoint_of_scalar((scalar_of_first_codepoint(bytes) as char) as u32) == first_codepoint(bytes)
+{
+    if is_leading_1(bytes[0]) {
+        scalar_of_codepoint_codepoint_of_scalar_1_byte(bytes[0]);
+    } else if is_leading_2(bytes[0]) {
+        scalar_of_codepoint_codepoint_of_scalar_2_byte(bytes[0], bytes[1]);
+    } else if is_leading_3(bytes[0]) {
+        scalar_of_codepoint_codepoint_of_scalar_3_byte(bytes[0], bytes[1], bytes[2]);
+    } else {
+        scalar_of_codepoint_codepoint_of_scalar_4_byte(bytes[0], bytes[1], bytes[2], bytes[3]);
+    }
+}
+
+pub proof fn decode_utf8_encode_utf8(bytes: Seq<u8>)
+    requires
+        valid_utf8(bytes)
+    ensures
+        encode_utf8(decode_utf8(bytes)) == bytes
+    decreases bytes.len(),
+{
+    broadcast use encode_utf8_valid_utf8;
+    if bytes.len() == 0 {
+    } else {
+        let chars = decode_utf8(bytes);
+        let first = scalar_of_first_codepoint(bytes) as char;
+        let rest = pop_first_codepoint(bytes);
+        
+        char_is_valid_scalar(first);
+        assert(codepoint_of_scalar(first as u32) == first_codepoint(bytes)) by {
+            decode_utf8_first_codepoint(bytes);
+        }
+        
+        assert(chars.drop_first() =~= decode_utf8(rest));
+        decode_utf8_encode_utf8(rest);
+    }
+}
+
+/* ascii */
+
+pub open spec fn is_ascii_chars(chars: Seq<char>) -> bool {
+    forall |i| 0 <= i < chars.len() ==> '\0' <= #[trigger] chars[i] <= '\x7f'
+}
+
+pub proof fn is_ascii_chars_encode_utf8(chars: Seq<char>)
+    requires
+        is_ascii_chars(chars)
+    ensures
+        chars.len() == encode_utf8(chars).len(),
+        forall |i| #![trigger chars[i]] #![trigger encode_utf8(chars)[i]] 0 <= i < chars.len() ==> chars[i] as u8 == encode_utf8(chars)[i]
+    decreases
+        chars.len()
+{
+    if chars.len() == 0 {
+    } else {
+        let c0 = chars[0] as u32;
+        assert(c0 as u8 == first_byte_1_byte_codepoint(c0)) by (bit_vector)
+            requires
+                is_1_byte_codepoint(c0);
+        is_ascii_chars_encode_utf8(chars.drop_first());
     }
 }
 
