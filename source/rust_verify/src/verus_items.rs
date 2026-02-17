1e64555f00f4f7caa3eea2ad092e02cbada33436
elanortang <77796517+elanortang@users.noreply.github.com>
2026-02-17 11:32:38 -0500

    Verus internals updates (#2118)
    * fix VstdKind ImportedViaCore option
    
    * override the attributes query to make rustc think there are stability attributes where necessary
    
    * add cfg verus_verify_core
    
    * erase assume_specification innards when erasure is set
    
    this fixes an error with --compile where rustc panics
    due to improper MIR resulting from improper use of intrinsics
    
    * add more cfg verus_verify_core
    
    * add SharedReference
    
    * remove cfg verus_verify_core
    
    * fix errors and erase assume_spec fn
    
    * verusfmt
    
    * add documentation and remove commented-out code
    
    ---------
    
    Co-authored-by: Travis Hance <tjhance7@gmail.com>

--- source/rust_verify/src/verus_items.rs
+++ source/rust_verify/src/verus_items.rs
@@ -319,6 +319,7 @@ pub(crate) enum VstdItem {
     RefMutArrayUnsizingCoercion,
     VecIndex,
     VecIndexMut,
+    SharedReference,
 }
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
@@ -589,6 +590,7 @@ fn verus_items_map() -> Vec<(&'static str, VerusItem)> {
         ("verus::vstd::raw_ptr::cast_slice_ptr_to_str_ptr", VerusItem::Vstd(VstdItem::CastSlicePtrToStrPtr, Some(Arc::new("raw_ptr::cast_slice_ptr_to_str_ptr".to_owned())))),
         ("verus::vstd::raw_ptr::cast_str_ptr_to_slice_ptr", VerusItem::Vstd(VstdItem::CastStrPtrToSlicePtr, Some(Arc::new("raw_ptr::cast_str_ptr_to_slice_ptr".to_owned())))),
         ("verus::vstd::raw_ptr::cast_ptr_to_usize", VerusItem::Vstd(VstdItem::CastPtrToUsize, Some(Arc::new("raw_ptr::cast_ptr_to_usize".to_owned())))),
+        ("verus::vstd::raw_ptr::SharedReference", VerusItem::Vstd(VstdItem::SharedReference, Some(Arc::new("raw_ptr::SharedReference".to_owned())))),
             // SeqFn(vir::interpreter::SeqFn::Last    ))),
 
         ("verus::verus_builtin::Structural",              VerusItem::Marker(MarkerItem::Structural)),
