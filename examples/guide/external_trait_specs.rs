dc28d2066c951e054620f9fa5ec43f7db8f86198
Bryan Parno <parno@users.noreply.github.com>
2026-02-23 15:43:52 -0500

    Add documentation for trait extensions (#2173)

---
+++ examples/guide/external_trait_specs.rs
@@ -0,0 +1,88 @@
+#[allow(unused_imports)]
+use verus_builtin::*;
+#[allow(unused_imports)]
+use verus_builtin_macros::*;
+
+// ANCHOR: basic_trait
+#[verifier::external]
+trait Encoder {
+    fn encode_value(&self, x: u64) -> u64;
+}
+// ANCHOR_END: basic_trait
+
+verus! {
+
+// ANCHOR: basic_spec
+#[verifier::external_trait_specification]
+trait ExEncoder {
+    // This associated type names the trait being specified:
+    type ExternalTraitSpecificationFor: Encoder;
+
+    fn encode_value(&self, x: u64) -> (result: u64)
+        ensures
+            result >= x;
+}
+// ANCHOR_END: basic_spec
+
+// ANCHOR: basic_use
+fn use_encoder<E: Encoder>(f: &E, val: u64) -> (result: u64)
+    ensures
+        result >= val,
+{
+    f.encode_value(val)
+}
+// ANCHOR_END: basic_use
+
+} // verus!
+
+verus! {
+
+#[verifier::external]
+trait Summarizer {
+    fn summary(&self) -> (result: u64);
+}
+
+// ANCHOR: extension_spec
+#[verifier::external_trait_specification]
+#[verifier::external_trait_extension(SummarizerSpec via SummarizerSpecImpl)]
+trait ExSummarizer {
+    type ExternalTraitSpecificationFor: Summarizer;
+
+    // A spec helper function (not part of the original trait):
+    spec fn spec_summary(&self) -> u64;
+
+    fn summary(&self) -> (result: u64)
+        ensures
+            result == self.spec_summary();
+}
+// ANCHOR_END: extension_spec
+
+// ANCHOR: extension_impl
+struct MyCustomStruct { value: u64 }
+
+// Implement the concrete external Summarizer trait
+impl Summarizer for MyCustomStruct {
+    fn summary(&self) -> u64 {
+        // Prove that our overly complicated implementation satisfies the spec_summary
+        let v = self.value;
+        assert(v & 0xffff_ffff_ffff_ffff == v) by (bit_vector);
+        v & 0xffff_ffff_ffff_ffff
+    }
+}
+
+// Implement the additional spec functions
+impl SummarizerSpecImpl for MyCustomStruct {
+    spec fn spec_summary(&self) -> u64 {
+        self.value
+    }
+}
+
+fn test_hasher(h: &MyCustomStruct) {
+    let v = h.summary();
+    assert(v == h.value);
+}
+// ANCHOR_END: extension_impl
+
+} // verus!
+
+fn main() {}
