3c1a75060b70d266dd3feae561481b0f17df7ce2
Travis Hance <tjhance7@gmail.com>
2026-02-26 16:01:36 +0100

    new-mut-ref: add tests and notes related to place-expression side-effects
    this documents some existing issues but doesn't change any behavior

--- source/vir/src/ast.rs
+++ source/vir/src/ast.rs
@@ -1199,6 +1199,52 @@ pub enum ModeWrapperMode {
 /// A `Place` tree is always a sequence of modifiers that terminates in either a Local
 /// or a Temporary. Note that there are no modifier nodes for dereferencing a box or dereferencing
 /// a shared reference. These are implicit.
+///
+/// ### Evaluation order and side-effects (more than you wanted to know)
+///
+/// A Place expression can have arbitrary side-effects, INCLUDING modifying the place that
+/// is being referred to. As a result, we have to be very careful about evaluation order.
+/// In general, you evaluate the "place" first, then read/write whatever from the place.
+/// That means if you write something like `a[{ a = b; i }]` this is equivalent to `a = b; a[i]`.
+///
+/// Things get more complicated when dealing with bounds-checks.
+/// Consider: `a[i][{ a = b; j }]`. In this expression, we bounds-check `a[i]` first, then
+/// evaluate the second index. Does that mean the `a = b` assignment could invalidate the
+/// bounds-check that was performed previously?
+///  - If `a` is an array then no, because the length of the array is fixed
+///  - If `a` is a slice then yes, so rustc checks for this case and disallows it.
+///    (See [https://doc.rust-lang.org/1.93.0/nightly-rustc/rustc_middle/mir/enum.FakeReadCause.html#variant.ForIndex])
+/// Either way this is fine for us; either we don't have to worry about it or the bounds-check
+/// is unaffected by the assignment.
+///
+/// What about unions? In Verus, we naturally have a precondition that a union access, but
+/// in Rust there's no actual check (except in the Abstract Machine state to determine UB).
+/// Anyway, despite the fact that union fields have a "precondition",
+/// there's no analogous check like there is for slice indexing.
+/// Thus, Rust's borrow-checker accepts this: `a.union_field[{a = b}]`
+/// and reading this is non-UB provided `b.union_field` is a valid union field read.
+/// Counter-intuitively, it doesn't even matter if `a.union_field` is valid _before_ the assignment,
+/// since that memory location isn't actually read from.
+/// Note, though, that the ordering of the bounds-check and the ordering of the field access
+/// depends on whether the index is for an array or slice (since for the slice, we need to do
+/// the field access to get the length).
+///
+/// To summarize, the evaluation order for a place expression with only array accesses:
+///   local.foo[e1].bar[e2]
+/// Would be:
+///   - evaluate e1
+///   - bounds-check e1
+///   - evaluate e2
+///   - bounds-check e2
+///   - check all field accesses are safe
+/// However, if any of the indices are _slice_ indices, then the relevant field safety checks
+/// need to be moved earlier. (Though, again, index checks actually block subsequent mutations,
+/// so we wouldn't need to do any field safety check more than once.)
+/// Also, the order of the checks doesn't really matter to Verus, as long as both are hard
+/// requirements, but it might matter if we model unwinding.
+///
+/// TODO(new_mut_ref): fix or error on these cases and then document what we actually support
+
 pub type Place = Arc<SpannedTyped<PlaceX>>;
 pub type Places = Arc<Vec<Place>>;
 #[derive(Debug, Serialize, Deserialize, ToDebugSNode, Clone)]
@@ -1224,8 +1270,10 @@ pub enum PlaceX {
     /// Therefore, to properly handle programs with are admitted due to these special
     /// cases, we also need to treat array indexing and slice indexing via a Place node.
     /// That's why `PlaceX::Index` exists.
-    /// It is _not_ necessary to handle other types (like Vec indexing) in this way
-    /// (although it wouldn't hurt, either).
+    ///
+    /// There are also subtle differences involving evaluation order (see above);
+    /// so Array and Slice need to be handled as Place nodes, while Vec indexing
+    /// (and others) needs to be handled as method calls.
     Index(Place, Expr, ArrayKind, BoundsCheck),
     /// Named local variable.
     Local(VarIdent),
