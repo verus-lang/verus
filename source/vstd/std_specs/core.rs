47dbd555702c7b05967ae68af68da6904fd45f8d
Elanor Tang <elanor@umich.edu>
2026-03-03 18:20:14 -0500

    add cfg

--- source/vstd/std_specs/core.rs
+++ source/vstd/std_specs/core.rs
@@ -35,6 +35,7 @@ pub trait ExSpecOrd<Rhs> {
     type ExternalTraitSpecificationFor: SpecOrd<Rhs>;
 }
 
+#[cfg(not(verus_verify_core))]
 #[verifier::external_trait_specification]
 pub trait ExAllocator {
     type ExternalTraitSpecificationFor: core::alloc::Allocator;
