72482bd89035a3a9a20c48acc1a14162717e34b8
Travis Hance <tjhance7@gmail.com>
2026-03-06 14:39:30 +0100

    make progress

--- source/rustc_mir_build_additional_files/verus_expr.rs
+++ source/rustc_mir_build_additional_files/verus_expr.rs
@@ -4,7 +4,7 @@ use crate::verus::{
     erase_node, erase_node_unadjusted, erase_tree_kind, erased_ghost_value, handle_call,
     is_node_with_single_arg_erased,
     is_binary_node_with_any_arg_erased,
-    preferred_erasure_ty,
+    preferred_erasure_ty, VerusErasureCtxt,
 };
 use rustc_hir::{Expr, ExprKind, UnOp};
 use rustc_middle::ty::adjustment::{Adjust, Adjustment, AutoBorrow};
@@ -99,11 +99,13 @@ pub(crate) fn mirror_expr_post<'tcx>(
             }
         }
         ExprKind::Struct(..) => {
+            // structs also go into the 'call' map
             let call_erasure = handle_call(&cx.verus_ctxt, expr);
             if call_erasure.should_erase() {
                 return erase_node_unadjusted(cx, expr, kind);
             } else {
-                kind
+                let kind = fixup_struct_types(cx, erasure_ctxt, expr, kind);
+                return (kind, cx.typeck_results.expr_ty_adjusted(expr));
             }
         }
         ExprKind::Field(..) | ExprKind::AddrOf(..) => {
@@ -135,10 +137,37 @@ pub(crate) fn mirror_expr_post<'tcx>(
     (kind, ty)
 }
 
+/// If we have a non-erased call which takes erased args, the types might be wrong.
+/// e.g.:
+/// proof fn f(ghost x: X)
+/// Then a call `f(X{})` would get turned into `f(erased_ghost_value::<()>())`.
+/// This would fail to type check because the arg has type ().
+/// We can't just erase `f`, since it's a proof function,
+/// so we "fix up" the function type instead.
 fn fixup_call_types<'tcx>(
     cx: &mut ThirBuildCx<'tcx>,
     expr: &'tcx Expr<'tcx>,
     kind: rustc_middle::thir::ExprKind<'tcx>,
 ) -> rustc_middle::thir::ExprKind<'tcx> {
+    // TODO: reorg code so it doesn't look like this function is only time-travel-related
     crate::verus_time_travel_prevention::call_post(cx, expr, kind)
+}
+
+/// For structs, we turn the erased arguments back to their original expected types.
+/// This needs a different solution to fix inhabitness issues.
+fn fixup_struct_types<'tcx>(
+    cx: &mut ThirBuildCx<'tcx>,
+    erasure_ctxt: &VerusErasureCtxt,
+    expr: &'tcx Expr<'tcx>,
+    kind: rustc_middle::thir::ExprKind<'tcx>,
+) -> rustc_middle::thir::ExprKind<'tcx> {
+    let mut kind = kind;
+    for field in kind.fields.iter_mut() {
+        if is_erased(&cx.thir.exprs[field.expr].kind) {
+            let expected_ty = kind.adt_def.variants()[kind.variant_idx].fields[field.name].ty(cx.tcx, kind.args);
+            let e = erased_ghost_value(cx, erasure_ctxt, expr.hir_id, expr.span, expected_ty);
+            field.expr = e;
+        }
+    }
+    kind
 }
