893e3404dba5bf03bc3c033d4592697bf5ad8040
Chris Hawblitzel <Chris.Hawblitzel@microsoft.com>
2026-03-06 16:27:03 -0800

    Split assume_ieee_float off into a separate pull request

--- source/vstd/contrib/mod.rs
+++ source/vstd/contrib/mod.rs
@@ -1,5 +1,4 @@
 pub use verus_builtin_macros::auto_spec;
 pub use verus_builtin_macros::{make_spec_type, self_view};
 pub use verus_builtin_macros::{set_build, set_build_debug};
-pub mod assume_ieee_float;
 pub mod exec_spec;
