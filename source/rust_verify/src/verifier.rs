3989854617cb3d1b96bb0028f3c330f37c841a0a
Daniel Schoepe <schoeped@amazon.com>
2026-03-14 15:26:28 -0700

    Minor fixes

--- source/rust_verify/src/verifier.rs
+++ source/rust_verify/src/verifier.rs
@@ -3097,7 +3097,7 @@ pub(crate) static BODY_HIR_ID_TO_REVEAL_PATH_RES: std::sync::RwLock<
 > = std::sync::RwLock::new(None);
 
 fn hir_crate<'tcx>(tcx: TyCtxt<'tcx>, _: ()) -> rustc_hir::Crate<'tcx> {
-    let mut crate_ = (rustc_interface::DEFAULT_QUERY_PROVIDERS.hir_crate)(tcx, ());
+    let mut crate_ = (rustc_interface::DEFAULT_QUERY_PROVIDERS.queries.hir_crate)(tcx, ());
     crate::hir_hide_reveal_rewrite::hir_hide_reveal_rewrite(&mut crate_, tcx);
     crate_
 }
