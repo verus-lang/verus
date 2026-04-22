8545cd209986b748cd2ef8367ce48955e81b9512
JakeGinesin <jakeginesin@gmail.com>
2026-04-21 21:46:54 -0400

    add wrapping_shl and wrapping_shr

--- source/vstd/wrapping.rs
+++ source/vstd/wrapping.rs
@@ -8,13 +8,11 @@
 use super::prelude::*;
 
 macro_rules! wrapping_specs {
-    ([$(($uN: ty, $iN: ty, $modname_u:ident, $modname_i:ident, $range:expr),)*]) => {
+    ([$(($uN: ty, $iN: ty, $modname_u:ident, $modname_i:ident, $range:expr, $bits:expr),)*]) => {
         $(
             verus! {
-
             pub mod $modname_u {
                 use super::*;
-
                 pub open spec fn wrapping_add(x: $uN, y: $uN) -> $uN {
                     if x + y > <$uN>::MAX {
                         (x + y - $range) as $uN
@@ -44,11 +42,17 @@ macro_rules! wrapping_specs {
                 pub open spec fn wrapping_mul(x: $uN, y: $uN) -> $uN {
                     ((x as nat * y as nat) % $range as nat) as $uN
                 }
-            }
 
+                pub open spec fn wrapping_shl(x: $uN, shift: u32) -> $uN {
+                    x << (shift % $bits)
+                }
+                pub open spec fn wrapping_shr(x: $uN, shift: u32) -> $uN {
+                    x >> (shift % $bits)
+                }
+
+            }
             pub mod $modname_i {
                 use super::*;
-
                 pub open spec fn wrapping_add(x: $iN, y: $iN) -> $iN {
                     if x + y > <$iN>::MAX {
                         (x + y - $range) as $iN
@@ -76,7 +80,6 @@ macro_rules! wrapping_specs {
                         (x - y) as $iN
                     }
                 }
-
                 pub open spec fn signed_crop(x: int) -> $iN {
                     if (x % ($range as int)) > (<$iN>::MAX as int) {
                         ((x % ($range as int)) - $range) as $iN
@@ -88,19 +91,24 @@ macro_rules! wrapping_specs {
                 pub open spec fn wrapping_mul(x: $iN, y: $iN) -> $iN {
                     signed_crop(x * y)
                 }
-            }
+
+                pub open spec fn wrapping_shl(x: $iN, shift: u32) -> $iN {
+                    x << (shift % $bits)
+                }
+                pub open spec fn wrapping_shr(x: $iN, shift: u32) -> $iN {
+                    x >> (shift % $bits)
+                }
 
             }
+        }
             )*
-
     }
 }
-
 wrapping_specs!([
-    (u8, i8, u8_specs, i8_specs, 0x100),
-    (u16, i16, u16_specs, i16_specs, 0x1_0000),
-    (u32, i32, u32_specs, i32_specs, 0x1_0000_0000),
-    (u64, i64, u64_specs, i64_specs, 0x1_0000_0000_0000_0000),
-    (u128, i128, u128_specs, i128_specs, 0x1_0000_0000_0000_0000_0000_0000_0000_0000),
-    (usize, isize, usize_specs, isize_specs, (usize::MAX - usize::MIN + 1)),
+    (u8, i8, u8_specs, i8_specs, 0x100, 8u32),
+    (u16, i16, u16_specs, i16_specs, 0x1_0000, 16u32),
+    (u32, i32, u32_specs, i32_specs, 0x1_0000_0000, 32u32),
+    (u64, i64, u64_specs, i64_specs, 0x1_0000_0000_0000_0000, 64u32),
+    (u128, i128, u128_specs, i128_specs, 0x1_0000_0000_0000_0000_0000_0000_0000_0000, 128u32),
+    (usize, isize, usize_specs, isize_specs, (usize::MAX - usize::MIN + 1), usize::BITS),
 ]);
