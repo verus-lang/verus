631282a99d8cc30b2878084e25d09c027225e8e8
Travis Hance <tjhance7@gmail.com>
2026-05-15 23:21:51 +0200

    fmt

--- source/rust_verify/src/automatic_derive.rs
+++ source/rust_verify/src/automatic_derive.rs
@@ -1,17 +1,17 @@
 use crate::context::Context;
 use crate::verus_items::RustItem;
 use rustc_hir::HirId;
+use rustc_hir::def_id::DefId;
+use rustc_middle::ty::TyKind;
 use rustc_span::Span;
 use std::sync::Arc;
 use vir::ast::{
-    BinaryOp, Expr, ExprX, FunctionX, Mode, Place, PlaceX, SpannedTyped, VirErr, VirErrAs,
-    CallTarget, CallTargetKind, TypX, CallTargetAttrs, AutospecUsage, Arm, UnaryOpr, FieldOpr, Typ, ImplPaths, CrateId, Fun, Visibility, Path, GenericBounds, Idents,
-    Function, ParamX, FunctionKind, BodyVisibility, Opaqueness, ItemKind, FunctionAttrsX,
-    VarIdent,
+    Arm, AutospecUsage, BinaryOp, BodyVisibility, CallTarget, CallTargetAttrs, CallTargetKind,
+    CrateId, Expr, ExprX, FieldOpr, Fun, Function, FunctionAttrsX, FunctionKind, FunctionX,
+    GenericBounds, Idents, ImplPaths, ItemKind, Mode, Opaqueness, ParamX, Path, Place, PlaceX,
+    SpannedTyped, Typ, TypX, UnaryOpr, VarIdent, VirErr, VirErrAs, Visibility,
 };
-use vir::ast_util::{bool_typ};
-use rustc_hir::def_id::DefId;
-use rustc_middle::ty::TyKind;
+use vir::ast_util::bool_typ;
 
 /// Traits with special handling
 #[derive(Clone, Copy, Debug)]
@@ -188,14 +188,20 @@ fn clone_add_post_condition<'tcx>(
         let other_var = SpannedTyped::new(
             &expr.span,
             &functionx.params[0].x.typ,
-            ExprX::Var(other_ident.clone()));
+            ExprX::Var(other_ident.clone()),
+        );
         let rel = match to_relation(expr, &other_var) {
             Ok(rel) => rel,
-            Err(()) => { warn_unexpected(); return Ok(None); }
+            Err(()) => {
+                warn_unexpected();
+                return Ok(None);
+            }
         };
         let rel = cleanup_span_ids(ctxt, span, hir_id, &rel);
-        let (datatype_visibility, datatype_body_visibility) = get_visibilities(&expr.span, ctxt, def_id)?;
-        let function = mk_spec_function(&expr.span,
+        let (datatype_visibility, datatype_body_visibility) =
+            get_visibilities(&expr.span, ctxt, def_id)?;
+        let function = mk_spec_function(
+            &expr.span,
             spec_fn_name,
             datatype_visibility.clone(),
             datatype_body_visibility,
@@ -205,24 +211,38 @@ fn clone_add_post_condition<'tcx>(
             &functionx.params[0].x.typ,
             &functionx.params[0].x.name,
             &other_ident,
-            &rel);
+            &rel,
+        );
 
-        let typ_args: Vec<_> = functionx.typ_params.iter()
-            .map(|param| { Arc::new(TypX::TypParam(param.clone())) })
+        let typ_args: Vec<_> = functionx
+            .typ_params
+            .iter()
+            .map(|param| Arc::new(TypX::TypParam(param.clone())))
             .collect();
-        let ct = CallTarget::Fun(CallTargetKind::Static, function.x.name.clone(),
-            Arc::new(typ_args), Arc::new(vec![]), CallTargetAttrs { autospec: AutospecUsage::Final, const_var: false, assume_external_allowed: false });
+        let ct = CallTarget::Fun(
+            CallTargetKind::Static,
+            function.x.name.clone(),
+            Arc::new(typ_args),
+            Arc::new(vec![]),
+            CallTargetAttrs {
+                autospec: AutospecUsage::Final,
+                const_var: false,
+                assume_external_allowed: false,
+            },
+        );
         let self_var = SpannedTyped::new(
             &expr.span,
             &functionx.params[0].x.typ,
-            ExprX::Var(functionx.params[0].x.name.clone()));
+            ExprX::Var(functionx.params[0].x.name.clone()),
+        );
         let ret_var = SpannedTyped::new(
             &expr.span,
             &functionx.ret.x.typ,
-            ExprX::Var(functionx.ret.x.name.clone()));
+            ExprX::Var(functionx.ret.x.name.clone()),
+        );
         let args = Arc::new(vec![self_var, ret_var]);
         let ens_expr = SpannedTyped::new(&expr.span, &bool_typ(), ExprX::Call(ct, args, None));
-        
+
         let ens_expr = cleanup_span_ids(ctxt, span, hir_id, &ens_expr);
         functionx.ensure.0 = Arc::new(vec![ens_expr]);
         // TODO: Right now trait functions are always public, but the datatype might be private,
@@ -242,47 +262,81 @@ fn to_relation(expr: &Expr, output: &Expr) -> Result<Expr, ()> {
                 armx.x.body = to_relation(&armx.x.body, output)?;
                 new_arms.push(arm);
             }
-            Ok(SpannedTyped::new(&expr.span, &expr.typ, ExprX::Match(scrutinee.clone(), Arc::new(new_arms))))
+            Ok(SpannedTyped::new(
+                &expr.span,
+                &expr.typ,
+                ExprX::Match(scrutinee.clone(), Arc::new(new_arms)),
+            ))
         }
         ExprX::Ctor(dt, variant_ident, binders, None) => {
-            let mut conjuncts = vec![
-                SpannedTyped::new(&expr.span, &bool_typ(), ExprX::UnaryOpr(
+            let mut conjuncts = vec![SpannedTyped::new(
+                &expr.span,
+                &bool_typ(),
+                ExprX::UnaryOpr(
                     UnaryOpr::IsVariant { datatype: dt.clone(), variant: variant_ident.clone() },
-                    output.clone())),
-            ];
+                    output.clone(),
+                ),
+            )];
             for binder in binders.iter() {
                 let field_of_output = SpannedTyped::new(
-                    &expr.span, &binder.a.typ, ExprX::UnaryOpr(UnaryOpr::Field(FieldOpr {
-                        datatype: dt.clone(),
-                        variant: variant_ident.clone(),
-                        field: binder.name.clone(),
-                        get_variant: true,
-                        check: vir::ast::VariantCheck::None,
-                    }), output.clone()));
+                    &expr.span,
+                    &binder.a.typ,
+                    ExprX::UnaryOpr(
+                        UnaryOpr::Field(FieldOpr {
+                            datatype: dt.clone(),
+                            variant: variant_ident.clone(),
+                            field: binder.name.clone(),
+                            get_variant: true,
+                            check: vir::ast::VariantCheck::None,
+                        }),
+                        output.clone(),
+                    ),
+                );
                 let (clonee, impl_paths, clone_typ_arg) = match &binder.a.x {
-                    ExprX::Call(CallTarget::Fun(
-                        CallTargetKind::DynamicResolved { .. } | CallTargetKind::Dynamic,
-                        fun, typs, impl_paths, _call_target_attrs
-                    ), args, None) if fun == &vir::fun!(CrateId::Core => "clone", "Clone", "clone") =>
-                    {
+                    ExprX::Call(
+                        CallTarget::Fun(
+                            CallTargetKind::DynamicResolved { .. } | CallTargetKind::Dynamic,
+                            fun,
+                            typs,
+                            impl_paths,
+                            _call_target_attrs,
+                        ),
+                        args,
+                        None,
+                    ) if fun == &vir::fun!(CrateId::Core => "clone", "Clone", "clone") => {
                         assert!(args.len() == 1);
                         (args[0].clone(), impl_paths.clone(), typs[0].clone())
                     }
-                    _ => { return Err(()); }
+                    _ => {
+                        return Err(());
+                    }
                 };
                 conjuncts.push(strictly_cloned_call(
-                    &clonee, &field_of_output, clone_typ_arg, impl_paths));
+                    &clonee,
+                    &field_of_output,
+                    clone_typ_arg,
+                    impl_paths,
+                ));
             }
             Ok(vir::ast_util::conjoin(&expr.span, &conjuncts))
         }
-        _ => { Err(()) }
+        _ => Err(()),
     }
 }
 
 fn strictly_cloned_call(e1: &Expr, e2: &Expr, typ: Typ, impl_paths: ImplPaths) -> Expr {
     let fun = vir::fun!(CrateId::Vstd => "pervasive", "strictly_cloned");
-    let ct = CallTarget::Fun(CallTargetKind::Static, fun, Arc::new(vec![typ]), impl_paths,
-        CallTargetAttrs { autospec: AutospecUsage::Final, const_var: false, assume_external_allowed: false });
+    let ct = CallTarget::Fun(
+        CallTargetKind::Static,
+        fun,
+        Arc::new(vec![typ]),
+        impl_paths,
+        CallTargetAttrs {
+            autospec: AutospecUsage::Final,
+            const_var: false,
+            assume_external_allowed: false,
+        },
+    );
     let args = Arc::new(vec![e1.clone(), e2.clone()]);
     SpannedTyped::new(&e1.span, &bool_typ(), ExprX::Call(ct, args, None))
 }
@@ -330,77 +384,92 @@ fn mk_spec_function(
             user_mut: false,
         },
     );
-    vir::def::Spanned::new(span.clone(), FunctionX {
-        name: spec_fn_name,
-        proxy: None,
-        kind: FunctionKind::Static,
-        visibility: datatype_visibility,
-        body_visibility: BodyVisibility::Visibility(datatype_body_visibility.clone()),
-        opaqueness: Opaqueness::Revealed { visibility: datatype_body_visibility.clone() },
-        owning_module: Some(module.clone()),
-        mode: Mode::Spec,
-        typ_params: typ_params.clone(),
-        typ_bounds: typ_bounds.clone(),
-        params: Arc::new(vec![self_param, other_param]),
-        ret: ret_param,
-        ens_has_return: false,
-        require: Arc::new(vec![]),
-        ensure: (Arc::new(vec![]), Arc::new(vec![])),
-        returns: None,
-        decrease: Arc::new(vec![]),
-        decrease_when: None,
-        decrease_by: None,
-        fndef_axioms: None,
-        mask_spec: None,
-        unwind_spec: None,
-        item_kind: ItemKind::Function,
-        attrs: Arc::new(FunctionAttrsX {
-            uses_ghost_blocks: true,
-            inline: false,
-            hidden: Arc::new(vec![]),
-            broadcast_forall: false,
-            broadcast_forall_only: false,
-            no_auto_trigger: false,
-            auto_ext_equal: vir::ast::AutoExtEqual { assert: false, assert_by: false, ensures: false, invariant: false },
-            autospec: None,
-            bit_vector: false,
-            atomic: false,
-            integer_ring: false,
-            is_decrease_by: false,
-            check_recommends: false,
-            nonlinear: false,
-            spinoff_prover: false,
-            memoize: false,
-            rlimit: None,
-            print_zero_args: false,
-            print_as_method: false,
-            prophecy_dependent: false,
-            size_of_broadcast_proof: false,
-            is_type_invariant_fn: false,
-            is_external_body: false,
-            is_unsafe: false,
-            exec_assume_termination: false,
-            exec_allows_no_decreases_clause: false,
-            tracked_swap: false,
-            tracked_take_option: false,
-            is_async: false,
-        }),
-        body: Some(body.clone()),
-        extra_dependencies: vec![],
-        async_ret: None,
-    })
+    vir::def::Spanned::new(
+        span.clone(),
+        FunctionX {
+            name: spec_fn_name,
+            proxy: None,
+            kind: FunctionKind::Static,
+            visibility: datatype_visibility,
+            body_visibility: BodyVisibility::Visibility(datatype_body_visibility.clone()),
+            opaqueness: Opaqueness::Revealed { visibility: datatype_body_visibility.clone() },
+            owning_module: Some(module.clone()),
+            mode: Mode::Spec,
+            typ_params: typ_params.clone(),
+            typ_bounds: typ_bounds.clone(),
+            params: Arc::new(vec![self_param, other_param]),
+            ret: ret_param,
+            ens_has_return: false,
+            require: Arc::new(vec![]),
+            ensure: (Arc::new(vec![]), Arc::new(vec![])),
+            returns: None,
+            decrease: Arc::new(vec![]),
+            decrease_when: None,
+            decrease_by: None,
+            fndef_axioms: None,
+            mask_spec: None,
+            unwind_spec: None,
+            item_kind: ItemKind::Function,
+            attrs: Arc::new(FunctionAttrsX {
+                uses_ghost_blocks: true,
+                inline: false,
+                hidden: Arc::new(vec![]),
+                broadcast_forall: false,
+                broadcast_forall_only: false,
+                no_auto_trigger: false,
+                auto_ext_equal: vir::ast::AutoExtEqual {
+                    assert: false,
+                    assert_by: false,
+                    ensures: false,
+                    invariant: false,
+                },
+                autospec: None,
+                bit_vector: false,
+                atomic: false,
+                integer_ring: false,
+                is_decrease_by: false,
+                check_recommends: false,
+                nonlinear: false,
+                spinoff_prover: false,
+                memoize: false,
+                rlimit: None,
+                print_zero_args: false,
+                print_as_method: false,
+                prophecy_dependent: false,
+                size_of_broadcast_proof: false,
+                is_type_invariant_fn: false,
+                is_external_body: false,
+                is_unsafe: false,
+                exec_assume_termination: false,
+                exec_allows_no_decreases_clause: false,
+                tracked_swap: false,
+                tracked_take_option: false,
+                is_async: false,
+            }),
+            body: Some(body.clone()),
+            extra_dependencies: vec![],
+            async_ret: None,
+        },
+    )
 }
 
 /// Given the DefId of the clone impl, returns:
 /// (i) visiblity of the datatype
 /// (ii) visiblity of the interior of the datatype
-fn get_visibilities<'tcx>(span: &vir::messages::Span, ctxt: &Context<'tcx>, def_id: DefId) -> Result<(Visibility, Visibility), VirErr> {
+fn get_visibilities<'tcx>(
+    span: &vir::messages::Span,
+    ctxt: &Context<'tcx>,
+    def_id: DefId,
+) -> Result<(Visibility, Visibility), VirErr> {
     let fn_sig = ctxt.tcx.fn_sig(def_id).skip_binder();
     let self_ty = fn_sig.output().skip_binder();
     let adt_def = match self_ty.kind() {
         TyKind::Adt(adt_def, _args) => adt_def,
         _ => {
-            return Err(vir::messages::error(span, "Verus Internal Error: handling auto-derive Clone, expected TyKind::Adt"));
+            return Err(vir::messages::error(
+                span,
+                "Verus Internal Error: handling auto-derive Clone, expected TyKind::Adt",
+            ));
         }
     };
     let datatype_vis = crate::rust_to_vir_base::mk_visibility(ctxt, adt_def.did());
