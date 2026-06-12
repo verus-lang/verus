885cffc0bd5c0bf0c33f1666c2d35c7c18039818
Mate Kovacs <mkovaxx@gmail.com>
2026-06-12 15:32:27 +0900

    format toolchain data as Rust code

--- source/cargo-verus-toolchains/src/lib.rs
+++ source/cargo-verus-toolchains/src/lib.rs
@@ -1,14 +1,16 @@
+use std::fmt::Write;
+
 use serde::{Deserialize, Serialize};
 
 /// A set of Verus components meant to be used together.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Toolchain {
     /// The Verus version; the primary key to identify a toolchain.
-    pub verus: String,
+    pub verus: StrLit,
     /// The vstd version.
     pub vstd: Crate,
     /// The Z3 version.
-    pub z3: String,
+    pub z3: StrLit,
 }
 
 /// Identifies a crate in a registry (i.e. crates.io) or git.
@@ -16,9 +18,93 @@ pub struct Toolchain {
 #[serde(untagged)]
 pub enum Crate {
     /// A version published in a registry.
-    Version(String),
+    Version(StrLit),
     /// A commit in a git repository (e.g. GitHub).
-    GitCommit { git: String, rev: String },
+    GitCommit { git: StrLit, rev: StrLit },
+}
+
+pub fn format_toolchains_code(
+    toolchains: &[Toolchain],
+    i0: Indent,
+    out: &mut impl Write,
+) -> std::fmt::Result {
+    writeln!(out, "{i0}/// An entry for each file in the `toolchain-manifests` directory.")?;
+    writeln!(out, "{i0}pub const TOOLCHAINS: [Toolchain; {}] = [", toolchains.len())?;
+    let i1 = i0.increase();
+    for toolchain in toolchains {
+        toolchain.format_code(i1, out)?;
+        writeln!(out, ",")?;
+    }
+    writeln!(out, "{i0}];")?;
+    Ok(())
+}
+
+/// A string literal.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct StrLit {
+    contents: String,
+}
+
+impl std::fmt::Display for StrLit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // TODO: Escape contents.
+        write!(f, "\"{}\"", self.contents)
+    }
+}
+
+impl From<&str> for StrLit {
+    fn from(value: &str) -> Self {
+        StrLit { contents: value.to_string() }
+    }
+}
+
+impl Toolchain {
+    pub fn format_code(&self, i0: Indent, out: &mut impl Write) -> std::fmt::Result {
+        let i1 = i0.increase();
+        writeln!(out, "{i0}Toolchain {{")?;
+        writeln!(out, "{i1}verus: {},", self.verus)?;
+        writeln!(out, "{i1}vstd: {},", self.vstd)?;
+        writeln!(out, "{i1}z3: {},", self.z3)?;
+        write!(out, "{i0}}}")?;
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for Crate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Crate::Version(version) => write!(f, "Crate::Registry {{ version: {version} }}")?,
+            Crate::GitCommit { git, rev } => {
+                write!(f, "Crate::GitCommit {{ git: {git}, rev: {rev} }}")?
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Indent(usize);
+
+impl Default for Indent {
+    fn default() -> Self {
+        Indent(0)
+    }
+}
+
+impl Indent {
+    pub fn increase(&self) -> Self {
+        Indent(self.0 + 4)
+    }
+}
+
+impl std::fmt::Display for Indent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for _ in 0..self.0 {
+            f.write_char(' ')?;
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
--- source/cargo-verus-toolchains/src/tests.rs
+++ source/cargo-verus-toolchains/src/tests.rs
@@ -1,42 +1,99 @@
-use crate::{Crate, Toolchain};
+use crate::{Crate, Indent, Toolchain, format_toolchains_code};
+use pretty_assertions::assert_eq;
 
-#[test]
-fn parse_toolchain_release() {
-    let toml_str = r#"
-        verus = "0.2026.06.07.cd03505"
-        vstd = "0.0.0-2026-05-31-0205"
-        z3 = "4.12.5"
-    "#;
-
-    let expected = Toolchain {
+fn make_release_manifest() -> Toolchain {
+    Toolchain {
         verus: "0.2026.06.07.cd03505".into(),
         vstd: Crate::Version("0.0.0-2026-05-31-0205".into()),
         z3: "4.12.5".into(),
-    };
-
-    let observed: Toolchain = toml::from_str(toml_str).expect("parse toolchain info");
-
-    assert_eq!(observed, expected);
+    }
 }
 
-#[test]
-fn parse_toolchain_rolling_release() {
-    let toml_str = r#"
-        verus = "0.2026.06.10.e6a6d4f"
-        vstd = { git = "https://github.com/verus-lang/verus.git", rev = "e6a6d4f" }
-        z3 = "4.12.5"
-    "#;
-
-    let expected = Toolchain {
+fn make_rolling_release_manifest() -> Toolchain {
+    Toolchain {
         verus: "0.2026.06.10.e6a6d4f".into(),
         vstd: Crate::GitCommit {
             git: "https://github.com/verus-lang/verus.git".into(),
             rev: "e6a6d4f".into(),
         },
         z3: "4.12.5".into(),
-    };
+    }
+}
+
+#[test]
+fn parse_print_toolchain_release() {
+    let toml_str = r#"
+        verus = "0.2026.06.07.cd03505"
+        vstd = "0.0.0-2026-05-31-0205"
+        z3 = "4.12.5"
+    "#;
+
+    let expected = make_release_manifest();
 
     let observed: Toolchain = toml::from_str(toml_str).expect("parse toolchain info");
 
     assert_eq!(observed, expected);
+
+    let expected_code = r#"Toolchain {
+    verus: "0.2026.06.07.cd03505",
+    vstd: Crate::Registry { version: "0.0.0-2026-05-31-0205" },
+    z3: "4.12.5",
+}"#;
+
+    let mut observed_code = String::new();
+    observed.format_code(Indent::default(), &mut observed_code).expect("format code");
+
+    assert_eq!(observed_code, expected_code);
+}
+
+#[test]
+fn parse_print_toolchain_rolling_release() {
+    let toml_str = r#"
+        verus = "0.2026.06.10.e6a6d4f"
+        vstd = { git = "https://github.com/verus-lang/verus.git", rev = "e6a6d4f" }
+        z3 = "4.12.5"
+    "#;
+
+    let expected = make_rolling_release_manifest();
+
+    let observed: Toolchain = toml::from_str(toml_str).expect("parse toolchain info");
+
+    assert_eq!(observed, expected);
+
+    let expected_code = r#"Toolchain {
+    verus: "0.2026.06.10.e6a6d4f",
+    vstd: Crate::GitCommit { git: "https://github.com/verus-lang/verus.git", rev: "e6a6d4f" },
+    z3: "4.12.5",
+}"#;
+
+    let mut observed_code = String::new();
+    observed.format_code(Indent::default(), &mut observed_code).expect("format code");
+
+    assert_eq!(observed_code, expected_code);
+}
+
+#[test]
+fn print_toolchain_const() {
+    let toolchains = [make_rolling_release_manifest(), make_release_manifest()];
+
+    let expected_code = r#"/// An entry for each file in the `toolchain-manifests` directory.
+pub const TOOLCHAINS: [Toolchain; 2] = [
+    Toolchain {
+        verus: "0.2026.06.10.e6a6d4f",
+        vstd: Crate::GitCommit { git: "https://github.com/verus-lang/verus.git", rev: "e6a6d4f" },
+        z3: "4.12.5",
+    },
+    Toolchain {
+        verus: "0.2026.06.07.cd03505",
+        vstd: Crate::Registry { version: "0.0.0-2026-05-31-0205" },
+        z3: "4.12.5",
+    },
+];
+"#;
+
+    let mut observed_code = String::new();
+    format_toolchains_code(&toolchains, Indent::default(), &mut observed_code)
+        .expect("format code");
+
+    assert_eq!(observed_code, expected_code);
 }
