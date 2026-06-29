9cb33cead3b7fedcc3e406cab1e11187753ce801
Travis Hance <tjhance7@gmail.com>
2026-06-29 17:28:37 -0400

    mut_ref cleanup: rename AssignToPlace to Assign (#2612)

--- source/vir/src/early_exit_cf.rs
+++ source/vir/src/early_exit_cf.rs
@@ -64,7 +64,7 @@ fn expr_get_early_exits_rec(
             | ExprX::Binary(..)
             | ExprX::BinaryOpr(..)
             | ExprX::Multi(..)
-            | ExprX::AssignToPlace { .. }
+            | ExprX::Assign { .. }
             | ExprX::If(..)
             | ExprX::Match(..)
             | ExprX::Ghost { .. }
