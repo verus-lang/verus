559d9d960ef581246bfda99d5fff7158078df945
Baltasar Dinis <bsdinis@mit.edu>
2026-07-13 13:49:54 -0400

    line_count: move to clap

--- source/tools/line_count/src/config.rs
+++ source/tools/line_count/src/config.rs
@@ -1,8 +1,23 @@
+#[derive(Clone, Copy, Debug, clap::Parser)]
 pub struct Config {
+    /// Print all the annotated files
+    #[arg(short, long)]
     pub print_all: bool,
-    pub json: bool,
+
+    /// Do not ignore items outside of `verus!` by default
+    #[arg(long)]
     pub no_external_by_default: bool,
+
+    /// Output as machine-readable json
+    #[arg(long)]
+    pub json: bool,
+
+    /// Consider delimiter-only lines as layout
+    #[arg(long)]
     pub delimiters_are_layout: bool,
+
+    /// Do not apply _trusted_ to proofs
+    #[arg(long)]
     pub proofs_arent_trusted: bool,
 }
 
