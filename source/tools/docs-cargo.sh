bc64477b2b5a8e49dfa7561fc4cd2053c6c6e0af
Mate Kovacs <mkovaxx@gmail.com>
2026-07-27 23:40:36 +0900

    sort out docs.sh stuff

--- source/tools/docs-cargo.sh
+++ source/tools/docs-cargo.sh
@@ -28,7 +28,7 @@ fi
 
 . ../tools/activate
 vargo build -p verusdoc
-vargo build --vstd-no-verify
+vargo build
 
 echo "Running rustdoc..."
 RUSTC_BOOTSTRAP=1 eval ""VERUSDOC=1 VSTD_KIND=IsVstd VERUS_Z3_PATH="$(pwd)/z3"  rustdoc \
@@ -56,6 +56,6 @@ RUSTC_BOOTSTRAP=1 eval ""VERUSDOC=1 VSTD_KIND=IsVstd VERUS_Z3_PATH="$(pwd)/z3"  
   vstd/vstd.rs""
 
 echo "Running post-processor..."
-./target/debug/verusdoc
+./target-verus/debug/verusdoc
 
 echo "Documentation generated at ./doc/vstd/index.html"
