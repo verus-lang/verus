ed8f285407484e50413abd4ab1685945df5c6661
Bryan Parno <parno@cmu.edu>
2026-07-30 09:23:24 -0700

    Remove a duplicated script

--- .github/workflows/get-z3.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-z3_version="4.16.0"
-
-filename=z3-$z3_version-x64-glibc-2.39
-wget https://github.com/Z3Prover/z3/releases/download/z3-$z3_version/$filename.zip
-unzip $filename.zip
-cp $filename/bin/z3 .
-
