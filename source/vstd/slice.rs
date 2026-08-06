f0f2d89a6a3750c092f2c8cc126407eeacf5d7fd
wood-ghost <yizhiy@kth.se>
2026-08-06 15:32:51 +0200

    impl PartialEqSpecImpl for slice == slice

--- source/vstd/slice.rs
+++ source/vstd/slice.rs
@@ -8,12 +8,30 @@ use core::slice::SliceIndex;
 #[cfg(feature = "alloc")]
 pub use super::std_specs::vec::VecAdditionalSpecFns;
 
+#[cfg(verus_keep_ghost)]
+use super::std_specs::cmp::{PartialEqSpec, PartialEqSpecImpl};
+
 verus! {
 
 impl<T> View for [T] {
     type V = Seq<T>;
 
     uninterp spec fn view(&self) -> Seq<T>;
+}
+
+pub assume_specification<T: PartialEq<U>, U>[ <[T] as PartialEq<[U]>>::eq ](left: &[T], right: &[U]) -> bool;
+
+impl<T, U> PartialEqSpecImpl<[U]> for [T]
+    where T: PartialEq<U> + PartialEqSpec<U>,
+{
+    open spec fn obeys_eq_spec() -> bool {
+        <T as PartialEqSpec<U>>::obeys_eq_spec()
+    }
+
+    open spec fn eq_spec(&self, other: &[U]) -> bool {
+        &&& self@.len() == other@.len()
+        &&& forall |i: int| #![auto] 0 <= i < self@.len() ==> <T as PartialEqSpec<U>>::eq_spec(&self@[i], &other@[i])
+    }
 }
 
 impl<T: DeepView> DeepView for [T] {
