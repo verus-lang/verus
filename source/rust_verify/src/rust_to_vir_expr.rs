1814c7d5472e4640371aeb8e976dd88008941b69
Ruize Tang <1466040111@qq.com>
2026-09-18 14:13:17 +0000

    Treat identical VIR casts as identity (#2951)

--- source/rust_verify/src/rust_to_vir_expr.rs
+++ source/rust_verify/src/rust_to_vir_expr.rs
@@ -2541,12 +2541,21 @@ pub(crate) fn expr_to_vir_innermost<'tcx>(
             let source_ty = bctx.types.expr_ty_adjusted(source);
             let source_vir_expr = source_vir.consume(bctx, source_ty);
 
+            let source_vir_ty = &source_vir_expr.typ;
+            let to_vir_ty = expr_typ()?;
+            // If the source and destination types are the same, we don't need to do anything.
+            // This case can show up unexpectedly. For example, in
+            // `fn test(value: &u8) { let value = value as &dyn T; ... }`, the cast appears
+            // non-trivial, but rustc inserts an explicit coercion on the source, making the
+            // explicit `as` coercion trivial.
+            if types_equal(source_vir_ty, &to_vir_ty) {
+                return Ok(ExprOrPlace::Expr(source_vir_expr));
+            }
+
             if let Some(expr) = maybe_do_ptr_cast(bctx, expr, source, &source_vir_expr)? {
                 return Ok(ExprOrPlace::Expr(expr));
             }
 
-            let source_vir_ty = &source_vir_expr.typ;
-            let to_vir_ty = expr_typ()?;
             match (&*undecorate_typ(source_vir_ty), &*undecorate_typ(&to_vir_ty)) {
                 (TypX::Int(_), TypX::Int(_)) => Ok(ExprOrPlace::Expr(mk_ty_clip(
                     bctx,
