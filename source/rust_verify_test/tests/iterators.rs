4539f56eada9533436710a6c4a81f10d9fb5455f
Bryan Parno <parno@cmu.edu>
2026-09-18 16:23:08 -0400

    Sort the iterator tests.

--- source/rust_verify_test/tests/iterators.rs
+++ source/rust_verify_test/tests/iterators.rs
@@ -4,166 +4,6 @@ mod common;
 use common::*;
 
 test_verify_one_file! {
-    #[test] mut_ref_forwarding verus_code! {
-        use vstd::prelude::*;
-        use vstd::std_specs::iter::IteratorSpec;
-
-        pub fn next_test<I: Iterator>(i: &mut I)
-            requires
-                i.obeys_prophetic_iter_laws(),
-                i.will_return_none(),
-            ensures
-                // TODO: The number of operators needed here is unfortunate
-                (&(*final(i))).obeys_prophetic_iter_laws(),
-                (&(*final(i))).will_return_none(),
-        {
-            i.next();
-
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] chars_next_falls_back_to_iterator_spec verus_code! {
-        use vstd::prelude::*;
-        use vstd::std_specs::iter::IteratorSpec;
-
-        fn test(s: &str)
-            requires
-                s@.len() >= 1,
-        {
-            let mut it = s.chars();
-            assert(it.remaining() == s@);
-            let r = it.next();
-            assert(r == Some(s@[0]));
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] range_works verus_code! {
-        use vstd::prelude::*;
-
-        fn test()
-        {
-            let mut v = vec![];
-            for i in iter: 0..4
-            invariant
-                v.len() == iter.index(),
-                iter.index() <= 4,
-            {
-                assert(i < 4);
-                v.push(i);
-            }
-            assert(v.len() == 4);
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] range_inclusive_works verus_code! {
-        use vstd::prelude::*;
-
-        fn test()
-        {
-            let mut v = vec![];
-            for i in iter: 0..=4
-            invariant
-                v.len() == iter.index(),
-                i <= 5,
-            {
-                assert(i <= 4);
-                v.push(i);
-            }
-            assert(v.len() == 5);
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] zip_works verus_code! {
-        use vstd::prelude::*;
-
-        fn zip_works() {
-            let x1 = vec![1u32, 2, 3];
-            let x2 = vec![2u32, 4, 6];
-            let y1 = vec![2u32, 4, 6];
-            let y2 = vec![1u32, 2];
-            let z1 = vec![1u32, 2];
-            let z2 = vec![2u32, 4, 6, 8, 10];
-
-            let x: Vec<(u32, u32)> = x1.into_iter().zip(x2).collect();
-            assert(x@ == seq![(1u32,2u32), (2, 4), (3, 6)]);
-
-            let y: Vec<(u32, u32)> = y1.into_iter().zip(y2).collect();
-            assert(y@ == seq![(2u32,1u32), (4, 2)]);
-
-            let z: Vec<(u32, u32)> = z1.into_iter().zip(z2).collect();
-            assert(z@ == seq![(1u32,2u32), (2, 4)]);
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] collect_works verus_code! {
-        use vstd::prelude::*;
-
-        fn test() {
-            let v: Vec<u32> = vec![1, 2, 3, 4];
-            let w: Vec<u32> = v.into_iter().collect();
-            assert(v@ == w@);
-            let x: Vec<u32> = w.into_iter().rev().collect();
-            assert(x@ == seq![4u32, 3, 2, 1]);
-
-            let y: Vec<u32> = vec![1, 2, 3, 4];
-            let z: Vec<u32> = y.into_iter().rev().rev().collect();
-            assert(z@ == y@);
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] vec_iter_mut_works verus_code! {
-        use vstd::prelude::*;
-
-        fn client_for_loop() {
-            let mut v: Vec<u32> = vec![1, 2, 3, 4];
-            for x in it: v.iter_mut()
-                invariant
-                    forall |i: int| #![auto] 0 <= i < it.index() ==> *final(it.seq()[i]) == 0,
-            {
-                *x = 0;
-            }
-            assert(forall |i: int| 0 <= i < v.len() ==> v[i] == 0);
-            assert(v@ == seq![0, 0, 0, 0]);
-        }
-
-    } => Ok(())
-}
-
-test_verify_one_file! {
-    #[test] find_works verus_code! {
-        use vstd::prelude::*;
-        use vstd::std_specs::iter::IteratorSpec;
-
-        fn test(v: Vec<u32>)
-        {
-            let v_result = v.into_iter().find(
-                |i| -> (ret: bool)
-                ensures ret == (*i < 10)
-                {*i < 10}
-            );
-            if let Some(i) = v_result {
-                assert(i < 10);
-                assert(v@.contains(i));
-            } else {
-                assert(forall |i| 0 <= i < v.len() ==> v[i] >= 10);
-            }
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
     #[test] all_works verus_code! {
         use vstd::prelude::*;
         use vstd::std_specs::iter::IteratorSpec;
@@ -220,6 +60,90 @@ test_verify_one_file! {
 }
 
 test_verify_one_file! {
+    #[test] chars_next_falls_back_to_iterator_spec verus_code! {
+        use vstd::prelude::*;
+        use vstd::std_specs::iter::IteratorSpec;
+
+        fn test(s: &str)
+            requires
+                s@.len() >= 1,
+        {
+            let mut it = s.chars();
+            assert(it.remaining() == s@);
+            let r = it.next();
+            assert(r == Some(s@[0]));
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] collect_works verus_code! {
+        use vstd::prelude::*;
+
+        fn test() {
+            let v: Vec<u32> = vec![1, 2, 3, 4];
+            let w: Vec<u32> = v.into_iter().collect();
+            assert(v@ == w@);
+            let x: Vec<u32> = w.into_iter().rev().collect();
+            assert(x@ == seq![4u32, 3, 2, 1]);
+
+            let y: Vec<u32> = vec![1, 2, 3, 4];
+            let z: Vec<u32> = y.into_iter().rev().rev().collect();
+            assert(z@ == y@);
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] filter_works verus_code! {
+        use vstd::prelude::*;
+        use vstd::std_specs::iter::*;
+
+        fn test() {
+            let p = |x: &u32| -> (b: bool)
+                ensures b == (*x % 2 == 0)
+            { *x % 2 == 0 };
+
+            let v: Vec<u32> = vec![1, 2, 3, 4];
+            let mut w: Vec<u32> = Vec::new();
+
+            for x in it: v.into_iter().filter(p)
+                invariant
+                    w.len() == it.index(),
+                    forall |i| 0 <= i < w.len() ==> w[i] == it.seq()[i],
+            {
+                w.push(x);
+            }
+            assert(w.len() <= 4);
+            assert(forall |i| 0 <= i < w.len() ==> w[i] % 2 == 0);
+            assert(forall |i| #![auto] 0 <= i < w.len() ==> v@.contains(w[i]));
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] find_works verus_code! {
+        use vstd::prelude::*;
+        use vstd::std_specs::iter::IteratorSpec;
+
+        fn test(v: Vec<u32>)
+        {
+            let v_result = v.into_iter().find(
+                |i| -> (ret: bool)
+                ensures ret == (*i < 10)
+                {*i < 10}
+            );
+            if let Some(i) = v_result {
+                assert(i < 10);
+                assert(v@.contains(i));
+            } else {
+                assert(forall |i| 0 <= i < v.len() ==> v[i] >= 10);
+            }
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
     #[test] map_works verus_code! {
         use vstd::prelude::*;
 
@@ -234,6 +158,96 @@ test_verify_one_file! {
                 w.push(x);
             }
             assert(w@ == seq![2u32, 4, 6, 8]);
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] mut_ref_forwarding verus_code! {
+        use vstd::prelude::*;
+        use vstd::std_specs::iter::IteratorSpec;
+
+        pub fn next_test<I: Iterator>(i: &mut I)
+            requires
+                i.obeys_prophetic_iter_laws(),
+                i.will_return_none(),
+            ensures
+                // TODO: The number of operators needed here is unfortunate
+                (&(*final(i))).obeys_prophetic_iter_laws(),
+                (&(*final(i))).will_return_none(),
+        {
+            i.next();
+
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] range_works verus_code! {
+        use vstd::prelude::*;
+
+        fn test()
+        {
+            let mut v = vec![];
+            for i in iter: 0..4
+            invariant
+                v.len() == iter.index(),
+                iter.index() <= 4,
+            {
+                assert(i < 4);
+                v.push(i);
+            }
+            assert(v.len() == 4);
+        }
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] range_inclusive_works verus_code! {
+        use vstd::prelude::*;
+
+        fn test()
+        {
+            let mut v = vec![];
+            for i in iter: 0..=4
+            invariant
+                v.len() == iter.index(),
+                i <= 5,
+            {
+                assert(i <= 4);
+                v.push(i);
+            }
+            assert(v.len() == 5);
+        }
+    } => Ok(())
+}
+
+
+test_verify_one_file! {
+    #[test] skip_works verus_code! {
+        use vstd::prelude::*;
+
+        fn test() {
+            let v: Vec<u32> = vec![1, 2, 3, 4];
+            let w: Vec<u32> = v.into_iter().skip(2).collect();
+            assert(w@ == seq![3, 4]);
+
+
+            let v: Vec<u32> = vec![1, 2, 3, 4];
+            let mut w: Vec<u32> = Vec::new();
+
+            for x in it: v.into_iter().skip(2)
+                invariant
+                    w.len() == it.index(),
+                    forall |i| 0 <= i < w.len() ==> w[i] == it.seq()[i],
+            {
+                w.push(x);
+            }
+            assert(w@ == seq![3, 4]);
+
+            let v: Vec<u32> = vec![1, 2, 3, 4];
+            let w: Vec<u32> = v.into_iter().skip(2).rev().collect();
+            assert(w@ == seq![4u32, 3]);
         }
     } => Ok(())
 }
@@ -268,35 +282,6 @@ test_verify_one_file! {
 }
 
 test_verify_one_file! {
-    #[test] skip_works verus_code! {
-        use vstd::prelude::*;
-
-        fn test() {
-            let v: Vec<u32> = vec![1, 2, 3, 4];
-            let w: Vec<u32> = v.into_iter().skip(2).collect();
-            assert(w@ == seq![3, 4]);
-
-
-            let v: Vec<u32> = vec![1, 2, 3, 4];
-            let mut w: Vec<u32> = Vec::new();
-
-            for x in it: v.into_iter().skip(2)
-                invariant
-                    w.len() == it.index(),
-                    forall |i| 0 <= i < w.len() ==> w[i] == it.seq()[i],
-            {
-                w.push(x);
-            }
-            assert(w@ == seq![3, 4]);
-
-            let v: Vec<u32> = vec![1, 2, 3, 4];
-            let w: Vec<u32> = v.into_iter().skip(2).rev().collect();
-            assert(w@ == seq![4u32, 3]);
-        }
-    } => Ok(())
-}
-
-test_verify_one_file! {
     #[test] take_skip verus_code! {
         use vstd::prelude::*;
         use vstd::std_specs::iter::IteratorSpec;
@@ -317,28 +302,44 @@ test_verify_one_file! {
 }
 
 test_verify_one_file! {
-    #[test] filter_works verus_code! {
+    #[test] vec_iter_mut_works verus_code! {
         use vstd::prelude::*;
-        use vstd::std_specs::iter::*;
 
-        fn test() {
-            let p = |x: &u32| -> (b: bool)
-                ensures b == (*x % 2 == 0)
-            { *x % 2 == 0 };
-
-            let v: Vec<u32> = vec![1, 2, 3, 4];
-            let mut w: Vec<u32> = Vec::new();
-
-            for x in it: v.into_iter().filter(p)
+        fn client_for_loop() {
+            let mut v: Vec<u32> = vec![1, 2, 3, 4];
+            for x in it: v.iter_mut()
                 invariant
-                    w.len() == it.index(),
-                    forall |i| 0 <= i < w.len() ==> w[i] == it.seq()[i],
+                    forall |i: int| #![auto] 0 <= i < it.index() ==> *final(it.seq()[i]) == 0,
             {
-                w.push(x);
+                *x = 0;
             }
-            assert(w.len() <= 4);
-            assert(forall |i| 0 <= i < w.len() ==> w[i] % 2 == 0);
-            assert(forall |i| #![auto] 0 <= i < w.len() ==> v@.contains(w[i]));
+            assert(forall |i: int| 0 <= i < v.len() ==> v[i] == 0);
+            assert(v@ == seq![0, 0, 0, 0]);
+        }
+
+    } => Ok(())
+}
+
+test_verify_one_file! {
+    #[test] zip_works verus_code! {
+        use vstd::prelude::*;
+
+        fn zip_works() {
+            let x1 = vec![1u32, 2, 3];
+            let x2 = vec![2u32, 4, 6];
+            let y1 = vec![2u32, 4, 6];
+            let y2 = vec![1u32, 2];
+            let z1 = vec![1u32, 2];
+            let z2 = vec![2u32, 4, 6, 8, 10];
+
+            let x: Vec<(u32, u32)> = x1.into_iter().zip(x2).collect();
+            assert(x@ == seq![(1u32,2u32), (2, 4), (3, 6)]);
+
+            let y: Vec<(u32, u32)> = y1.into_iter().zip(y2).collect();
+            assert(y@ == seq![(2u32,1u32), (4, 2)]);
+
+            let z: Vec<(u32, u32)> = z1.into_iter().zip(z2).collect();
+            assert(z@ == seq![(1u32,2u32), (2, 4)]);
         }
     } => Ok(())
 }
